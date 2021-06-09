82637de8ea949803dfb261d7f1ba107f46872dd1
Sigmanificient <edhyjox@gmail.com>
2021-06-10 00:05:16 +0200

    better languages stats

--- php/8kyu/return_negative.php
+++ php/8kyu/return_negative.php
@@ -1,4 +1,12 @@
+<?php
+
 function makeNegative(float $num) : float
 {
+    /*
+    https://www.codewars.com/kata/55685cd7ad70877c23000102
+
+    In this simple assignment you are given a number and have to make it negative.
+    But maybe the number is already negative?
+    */
     return -abs($num);
 }
