7de94d0b5cba52feb75a93902fed6fdc042953ce
Sigmanificient <edhyjox@gmail.com>
2021-06-11 00:01:59 +0200

    missing space in docstring for new lines

--- python/6kyu/tribonacci_sequence.py
+++ python/6kyu/tribonacci_sequence.py
@@ -20,7 +20,7 @@ def tribonacci(signature: List[int], n: int) -> List[int]:
 
     [0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
     Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a
-     signature array/list, returns the first n elements - signature included of the so seeded sequence.
+    signature array/list, returns the first n elements - signature included of the so seeded sequence.
 
     Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty
     array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
