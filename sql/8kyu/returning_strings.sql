8dd8f49451c930e34b6adbcedb6edfd5d0c39bb2
Sigmanificient <edhyjox@gmail.com>
2021-06-10 02:49:04 +0200

    returning strings

---
+++ sql/8kyu/returning_strings.sql
@@ -0,0 +1,3 @@
+-- Write a select statement that takes name from person table and return "Hello, <name> how are you doing today?"
+-- results in a column named greeting
+SELECT FORMAT('Hello, %s how are you doing today?', name) as greeting FROM person;
