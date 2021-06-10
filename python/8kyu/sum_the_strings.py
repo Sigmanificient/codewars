ae66d403ab8b2a895710c9656bad4494fff055a0
Sigmanificient <edhyjox@gmail.com>
2021-06-10 03:12:58 +0200

    sum the strings

---
+++ python/8kyu/sum_the_strings.py
@@ -0,0 +1,8 @@
+def sum_str(a: str, b: str) -> str:
+    """
+    https://www.codewars.com/kata/5966e33c4e686b508700002d
+
+    Create a function that takes 2 non negative integers in form of a string as an input, and outputs the sum
+    (also as a string)
+    """
+    return f"{int(f'0{a}') + int(f'0{b}')}"
