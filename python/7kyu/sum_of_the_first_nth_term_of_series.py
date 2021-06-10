e8a88fb0f5fdf2976f744d31f1513b661d91b669
Sigmanificient <edhyjox@gmail.com>
2021-06-10 02:01:38 +0200

    better languages stats

--- python/7kyu/sum_of_the_first_nth_term_of_series.py
+++ python/7kyu/sum_of_the_first_nth_term_of_series.py
@@ -4,13 +4,11 @@ def series_sum(n: int) -> str:
 
     Task:
     Your task is to write a function which returns the sum of following series upto nth term(parameter).
-
     Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+
     Rules:
-    You need to round the answer to 2 decimal places and return it as String.
-
-    If the given value is 0 then it should return 0.00
-
-    You will only be given Natural Numbers as arguments.
+    - You need to round the answer to 2 decimal places and return it as String.
+    - If the given value is 0 then it should return 0.00
+    - You will only be given Natural Numbers as arguments.
     """
     return f"{sum(1/(1 + 3 * x) for x in range(n)):.2f}"
