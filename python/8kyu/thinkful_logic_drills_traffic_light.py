a9464f0a81ff68b7d9330c9e56cdc0af58456143
Sigmanificient <edhyjox@gmail.com>
2021-06-12 00:02:33 +0200

    short docstring test

--- python/8kyu/thinkful_logic_drills_traffic_light.py
+++ python/8kyu/thinkful_logic_drills_traffic_light.py
@@ -2,5 +2,5 @@ lights = ['green', 'yellow', 'red']
 
 
 def update_light(current: str) -> str:
-    """https://www.codewars.com/kata/58649884a1659ed6cb000072"""
+    """This kate url is https://www.codewars.com/kata/58649884a1659ed6cb000072."""
     return lights[(lights.index(current) + 1) % len(lights)]
