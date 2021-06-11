48328234e6544d58a5e400b79425fb1e25a8e378
Sigmanificient <edhyjox@gmail.com>
2021-06-11 23:50:15 +0200

    test

--- python/5kyu/scramble.py
+++ python/5kyu/scramble.py
@@ -1,5 +1,5 @@
 def scramble(s1: str, s2: str) -> bool:
-    """https://www.codewars.com/kata/55c04b4cc56a697bb0000048
+    """https://www.codewars.com/kata/55c04b4cc56a697bb0000048 .
 
     Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to
     match str2, otherwise returns false.
