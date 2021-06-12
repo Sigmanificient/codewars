be32d797dd6c67f6eb04a78f7135a7017cf2327e
Sigmanificient <edhyjox@gmail.com>
2021-06-12 21:32:23 +0200

    primes in numbers

---
+++ python/5kyu/primes_in_numbers.py
@@ -0,0 +1,23 @@
+from math import sqrt
+
+
+def prime_factors(n: int) -> str:
+    result = []
+    for i in range(2, int(sqrt(n)) + 1):
+        while not n % i:
+            result.append(i)
+            n //= i
+
+    if n > 1:
+        result.append(n)
+
+    primes_factorisation = []
+    for prime in sorted(set(result)):
+        power = result.count(prime)
+
+        if power > 1:
+            primes_factorisation.append(f'({prime}**{result.count(prime)})')
+        else:
+            primes_factorisation.append(f'({prime})')
+
+    return ''.join(primes_factorisation)
