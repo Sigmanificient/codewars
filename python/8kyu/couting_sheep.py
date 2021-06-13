fc4d8f8f317940f90ee4a83419f01afd23ac82eb
Sigmanificient <edhyjox@gmail.com>
2021-06-13 22:43:57 +0200

    adding typing to counting sheep

--- python/8kyu/couting_sheep.py
+++ python/8kyu/couting_sheep.py
@@ -1,3 +1,6 @@
-def count_sheeps(sheep):
+from typing import Optional, List
+
+
+def count_sheeps(sheep: List[Optional[bool]]) -> int:
     """Kata url: https://www.codewars.com/kata/54edbc7200b811e956000556."""
     return sum(x for x in sheep if isinstance(x, bool))
