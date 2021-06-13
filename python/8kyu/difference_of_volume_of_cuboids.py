91acb53479a3a5bfd1d0b093489a21c94ddb734f
Sigmanificient <edhyjox@gmail.com>
2021-06-13 22:08:09 +0200

    difference of volume of cuboids

---
+++ python/8kyu/difference_of_volume_of_cuboids.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+def mul(x: List[int]) -> int:
+    """Multiply every element of an array together."""
+    r = 1
+    for k in x:
+        r *= k
+    return r
+
+
+def find_difference(a: List[int], b: List[int]) -> int:
+    """Kata url: https://www.codewars.com/kata/58cb43f4256836ed95000f97."""
+    return abs(mul(a) - mul(b))
