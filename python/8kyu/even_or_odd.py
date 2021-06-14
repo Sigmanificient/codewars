09c7886f17cbd63aedc9ffb5ea1ee166dca7d2cf
Sigmanificient <edhyjox@gmail.com>
2021-06-14 23:34:05 +0200

    [fix] wrong url

--- python/8kyu/even_or_odd.py
+++ python/8kyu/even_or_odd.py
@@ -1,3 +1,3 @@
 def even_or_odd(number: int) -> str:
-    """Kata url: https://www.codewars.com/kata/56dec885c54a926dcd001095."""
+    """Kata url: https://www.codewars.com/kata/53da3dbb4a5168369a0000fe."""
     return 'Odd' if number % 2 else 'Even'
