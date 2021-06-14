9ee8d1b6c7aa185b210d54b91a9406f33e3fc5ab
Sigmanificient <edhyjox@gmail.com>
2021-06-14 23:34:23 +0200

    [+] missing dot

---
+++ python/8kyu/opposite_number.py
@@ -0,0 +1,3 @@
+def opposite(number: float) -> float:
+    """Kata url: https://www.codewars.com/kata/56dec885c54a926dcd001095."""
+    return -number
---
+++ python/8kyu/sum_of_positive.py
@@ -0,0 +1,3 @@
+def positive_sum(arr):
+    """Kata url: https://www.codewars.com/kata/5715eaedb436cf5606000381."""
+    return sum(abs(x) for x in arr)
