86a920cad460bd9885b48795efd79dbcb7a4cbc1
Sigmanificient <edhyjox@gmail.com>
2021-06-14 23:37:47 +0200

    [+] sum of positives

--- python/8kyu/sum_of_positive.py
+++ python/8kyu/sum_of_positive.py
@@ -1,3 +1,6 @@
-def positive_sum(arr):
+from typing import List
+
+
+def positive_sum(arr: List[int]) -> int:
     """Kata url: https://www.codewars.com/kata/5715eaedb436cf5606000381."""
-    return sum(abs(x) for x in arr)
+    return sum(x for x in arr if x > 0)
