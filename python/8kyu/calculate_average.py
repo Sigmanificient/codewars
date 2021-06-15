e7cb491d11db7d6c648fe1894c65ce48abf1f2ec
Sigmanificient <edhyjox@gmail.com>
2021-06-15 23:30:55 +0200

    [+] Calculate average

---
+++ python/8kyu/calculate_average.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+def find_average(numbers: List[int]) -> float:
+    """Kata url: https://www.codewars.com/kata/57a2013acf1fa5bfc4000921."""
+    return sum(numbers) / len(numbers)
