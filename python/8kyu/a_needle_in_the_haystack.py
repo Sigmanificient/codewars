2543e95e43ca321a47a6414cf60e5c3a12a4b73f
Sigmanificient <edhyjox@gmail.com>
2021-06-15 23:08:56 +0200

    [+] A Needle in the Haystack

---
+++ python/8kyu/a_needle_in_the_haystack.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+def find_needle(haystack: List[str]) -> str:
+    """Kata url: https://www.codewars.com/kata/56676e8fabd2d1ff3000000c."""
+    return f"found the needle at position {haystack.index('needle')}"
