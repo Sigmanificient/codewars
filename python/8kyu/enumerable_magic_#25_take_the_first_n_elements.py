3e9a2405d7cf77ccfe62b6560f250cb87a2a02af
Yohann Boniface <edhyjox@gmail.com>
2021-06-16 23:33:16 +0200

    [fix] fixing docstrings

--- python/8kyu/enumerable_magic_#25_take_the_first_n_elements.py
+++ python/8kyu/enumerable_magic_#25_take_the_first_n_elements.py
@@ -2,5 +2,5 @@ from typing import List
 
 
 def take(arr: List[int],n:int) -> List[int]:
-    """Kata url: https://www.codewars.com/kata/545afd0761aa4c3055001386"""
+    """Kata url: https://www.codewars.com/kata/545afd0761aa4c3055001386."""
     return arr[:n]
