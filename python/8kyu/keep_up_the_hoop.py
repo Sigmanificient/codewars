625646cfcb5e91ec74a6febad36266917ec4900e
Yohann Boniface <edhyjox@gmail.com>
2021-06-16 23:37:25 +0200

    [+] Keep up the hoop

---
+++ python/8kyu/keep_up_the_hoop.py
@@ -0,0 +1,3 @@
+def hoop_count(n: int) -> str:
+    """Kata url: https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145."""
+    return "Keep at it until you get it" if n < 10 else "Great, now move on to tricks"
