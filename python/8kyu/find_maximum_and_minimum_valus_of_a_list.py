a6e5ab663e9533a8b43fc3bc7130ead95abdd943
Yohann Boniface <edhyjox@gmail.com>
2021-06-17 23:09:27 +0200

    [+] Find Maximum and Minimum Values of a List

---
+++ python/8kyu/find_maximum_and_minimum_valus_of_a_list.py
@@ -0,0 +1,10 @@
+# Kata url: https://www.codewars.com/kata/577a98a6ae28071780000989.
+from typing import List
+
+
+def minimum(arr: List[int]) -> int:
+    return min(arr)
+
+
+def maximum(arr: List[int]) -> int:
+    return max(arr)
