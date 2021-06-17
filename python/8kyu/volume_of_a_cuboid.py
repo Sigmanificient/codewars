82ac04bde7fb87c5d771a2d517d6ec7ccff4a4b8
Yohann Boniface <edhyjox@gmail.com>
2021-06-17 23:40:41 +0200

    [+] Volume of a Cuboid

---
+++ python/8kyu/volume_of_a_cuboid.py
@@ -0,0 +1,3 @@
+def get_volume_of_cubiod(length: int, width: int, height: int) -> int:
+    """Kata url: https://www.codewars.com/kata/58261acb22be6e2ed800003a."""
+    return length * width * height
