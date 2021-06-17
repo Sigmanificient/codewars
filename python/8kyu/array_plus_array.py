0dea26dca1e541709bd464ff48521600897cc026
Yohann Boniface <edhyjox@gmail.com>
2021-06-17 23:04:44 +0200

    [+] Array plus array

---
+++ python/8kyu/array_plus_array.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+def array_plus_array(arr1: List[int], arr2: List[int]) -> int:
+    """Kata url: https://www.codewars.com/kata/5a2be17aee1aaefe2a000151."""
+    return sum(arr1 + arr2)
