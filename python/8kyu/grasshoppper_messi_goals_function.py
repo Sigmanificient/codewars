d5caa687bdb1c3efe5cd39b013c0da9359976e8a
Yohann Boniface <edhyjox@gmail.com>
2021-06-17 22:57:46 +0200

    [+] Grasshopper - Messi goals function

---
+++ python/8kyu/grasshoppper_messi_goals_function.py
@@ -0,0 +1,3 @@
+def goals(la_liga: int, copa_del_rey: int, champions_league: int) -> int:
+    """Kata url: https://www.codewars.com/kata/55f73be6e12baaa5900000d4."""
+    return la_liga + copa_del_rey + champions_league
