9b4d1271ee84f357adea07f78a02da401b9d0ccf
Yohann Boniface <edhyjox@gmail.com>
2021-06-17 23:27:58 +0200

    [+] Transportation on vacation

---
+++ python/8kyu/transportation_on_vacation.py
@@ -0,0 +1,11 @@
+def rental_car_cost(d: int) -> int:
+    """Kata url: https://www.codewars.com/kata/568d0dd208ee69389d000016."""
+    total: int = 40 * d
+
+    if d >= 7:
+        total -= 50
+
+    if 3 < d < 7:
+        total -= 20
+
+    return total
