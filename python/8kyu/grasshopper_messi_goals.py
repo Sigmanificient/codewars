29094a792caeb5d0a828bb1d3d520290baef00ef
Yohann Boniface <edhyjox@gmail.com>
2021-06-18 23:07:51 +0200

    [+] Grasshopper - Messi Goals

---
+++ python/8kyu/grasshopper_messi_goals.py
@@ -0,0 +1,7 @@
+# Kata url: https://www.codewars.com/kata/55ca77fa094a2af31f00002a.
+
+la_liga_goals: int = 43
+champions_league_goals: int = 10
+copa_del_rey_goals: int = 5
+
+total_goals = la_liga_goals + champions_league_goals + copa_del_rey_goals
