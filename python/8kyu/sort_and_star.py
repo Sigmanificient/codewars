85c5d56b02a630282068f96894899a6e13e4b3e9
Yohann Boniface <edhyjox@gmail.com>
2021-06-19 23:55:34 +0200

    [+] sort and star

---
+++ python/8kyu/sort_and_star.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+def two_sort(array: List[str]) -> str:
+    """Kata url: https://www.codewars.com/kata/57cfdf34902f6ba3d300001e."""
+    return '***'.join(sorted(array)[0])
+
