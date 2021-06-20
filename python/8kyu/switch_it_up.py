7bcc10e93e1c5e68285fa83ba7087faf8918285a
Yohann Boniface <edhyjox@gmail.com>
2021-06-20 23:40:04 +0200

    [+] Switch it up !

---
+++ python/8kyu/switch_it_up.py
@@ -0,0 +1,3 @@
+def switch_it_up(number):
+    """Kata url: https://www.codewars.com/kata/5808dcb8f0ed42ae34000031."""
+    return ("Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine")[number]
