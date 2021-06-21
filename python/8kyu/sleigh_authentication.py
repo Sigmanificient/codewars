11116a716801738732d5694924e8cc638824ee54
Yohann Boniface <edhyjox@gmail.com>
2021-06-21 23:44:49 +0200

    [+] Sleigh Authentication

---
+++ python/8kyu/sleigh_authentication.py
@@ -0,0 +1,6 @@
+class Sleigh:
+    """Kata url: https://www.codewars.com/kata/52adc142b2651f25a8000643."""
+
+    @staticmethod
+    def authenticate(name, password):
+        return name == "Santa Claus" and password == "Ho Ho Ho!"
