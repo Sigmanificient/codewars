4c220e634d43cd39306dfd27c26c476b14690f98
Yohann Boniface <edhyjox@gmail.com>
2021-06-23 23:12:43 +0200

    [+] Reverse List Order

---
+++ python/8kyu/reverse_list_order.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+def reverse_list(collection: List[int]) -> List[int]:
+    """Kata url: https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b."""
+    return collection[::-1]
