5d58ad02410538babe10ab8dcdd8c800fc6ad806
Yohann Boniface <edhyjox@gmail.com>
2021-06-23 23:09:15 +0200

    [+] Well of Ideas - Easy Version

---
+++ python/8kyu/well_of_ideas_easy_version.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+def well(x: List[str]) -> str:
+    """Kata url: https://www.codewars.com/kata/57f222ce69e09c3630000212."""
+    if not x.count('good'):
+        return 'Fail!'
+
+    if x.count('good') <= 2:
+        return 'Publish!'
+
+    return 'I smell a series!'
