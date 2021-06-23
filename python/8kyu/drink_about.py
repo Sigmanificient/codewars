17bf6835a7239a247f0689b2b3e57cdf46e533b9
Yohann Boniface <edhyjox@gmail.com>
2021-06-23 22:18:10 +0200

    [+] Drink about

---
+++ python/8kyu/drink_about.py
@@ -0,0 +1,16 @@
+from typing import Dict
+
+ages: Dict[int, str] = {
+    13: "toddy",
+    17: "coke",
+    18: "beer",
+    20: "beer",
+    30: "whisky"
+}
+
+
+def people_with_age_drink(age: int) -> str:
+    """Kata url: https://www.codewars.com/kata/56170e844da7c6f647000063."""
+    for d_age, drink in ages.items():
+        if age <= d_age:
+            return f"drink {drink}"
