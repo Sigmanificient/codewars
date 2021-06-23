fc5536ef435850164a9a5fbd884f22fe04098cf7
Yohann Boniface <edhyjox@gmail.com>
2021-06-23 23:40:15 +0200

    [+] Find the position!

---
+++ python/8kyu/find_the_position.py
@@ -0,0 +1,3 @@
+def position(alphabet: str) -> str:
+    """Kata url: https://www.codewars.com/kata/5808e2006b65bff35500008f."""
+    return "Position of alphabet: {}".format("abcdefghijklmnopqrstuvwxyz".index(alphabet) + 1)
