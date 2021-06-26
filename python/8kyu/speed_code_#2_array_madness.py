157584a1fb3c1aef48fa5b2b4b168f72f3b485df
Yohann Boniface <edhyjox@gmail.com>
2021-06-26 22:49:56 +0200

    [+] SpeedCode #2 - Array Madness

---
+++ python/8kyu/speed_code_#2_array_madness.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+def array_madness(a: List[int], b: List[int]) -> bool:
+    """Kata url: https://www.codewars.com/kata/56ff6a70e1a63ccdfa0001b1."""
+    return sum(i ** 2 for i in a) > sum(j ** 3 for j in b)
