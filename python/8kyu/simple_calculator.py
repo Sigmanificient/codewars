4d90f7937f70854d69916a93dc4169688def6791
Yohann Boniface <edhyjox@gmail.com>
2021-06-26 23:09:34 +0200

    [+] Simple calculation

---
+++ python/8kyu/simple_calculator.py
@@ -0,0 +1,13 @@
+from typing import Union, Callable, Dict
+
+opts: Dict[str, Callable[[int, int], Union[int, float]]] = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x / y,
+}
+
+
+def calculator(x: Union[int, str], y: Union[int, str], op: str) -> Union[int, str]:
+    op = opts.get(op)
+    return op(x, y) if callable(op) and isinstance(x, int) and isinstance(y, int) else "unknown value"
