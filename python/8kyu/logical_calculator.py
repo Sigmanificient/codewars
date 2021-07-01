580d41d23bee251e46e6dce1977a951f68b96478
Yohann Boniface <edhyjox@gmail.com>
2021-07-01 23:51:55 +0200

    [+] Logical calculator

---
+++ python/8kyu/logical_calculator.py
@@ -0,0 +1,13 @@
+from functools import reduce
+from typing import List
+
+
+def logical_calc(array: List[bool], op: str) -> bool:
+    """Kata url: https://www.codewars.com/kata/57096af70dad013aa200007b."""
+    if op == "AND":
+        return all(array)
+
+    if op == "OR":
+        return any(array)
+
+    return reduce(lambda x, y: x ^ y, array)
