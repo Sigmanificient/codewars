51434c5894ef0a7d9ed8169bb6f76473b4b79465
Yohann Boniface <edhyjox@gmail.com>
2021-07-04 23:29:13 +0200

    [fix] missing docstring

--- python/8kyu/is_there_a_vowel_there.py
+++ python/8kyu/is_there_a_vowel_there.py
@@ -1,2 +1,3 @@
 def is_vow(inp):
+    """Kata url: https://www.codewars.com/kata/57cff961eca260b71900008f."""
     return [chr(x) if (chr(x) if isinstance(x, int) else x) in 'aeiou' else x for x in inp]
