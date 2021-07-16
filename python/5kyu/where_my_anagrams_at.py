28e5789e1bc2fda3ddca2b4036584b0bc2a2a330
Yohann Boniface <edhyjox@gmail.com>
2021-07-16 23:17:13 +0200

    [+] Where my anagrams at?

---
+++ python/5kyu/where_my_anagrams_at.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+def anagrams(word: str, words: List[str]) -> List[str]:
+    """Kata url: https://www.codewars.com/kata/523a86aa4230ebb5420001e1."""
+    sorted_word = sorted(word)
+    return [wrd for wrd in words if sorted(wrd) == sorted_word]
