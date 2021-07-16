8cdeb6cddc9b9b23afb368ac1e363b3327a9562d
Yohann Boniface <edhyjox@gmail.com>
2021-07-16 23:33:33 +0200

    [+] Unique In Order

---
+++ python/6kyu/unique_in_order.py
@@ -0,0 +1,13 @@
+from typing import List, Optional
+
+
+def unique_in_order(iterable: str) -> List[str]:
+    """Kata url: https://www.codewars.com/kata/54e6533c92449cc251001667."""
+    last: Optional[str] = None
+    array: List[str] = []
+    for element in iterable:
+        if last == element:
+            continue
+        array.append(element)
+        last: Optional[str] = element
+    return array
