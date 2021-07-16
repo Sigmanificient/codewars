f19f87e60c17eeb5bfcaa9b3190d4d440a6ff599
Yohann Boniface <edhyjox@gmail.com>
2021-07-16 23:17:55 +0200

    [fix] Typing & blank line

--- python/5kyu/where_my_anagrams_at.py
+++ python/5kyu/where_my_anagrams_at.py
@@ -3,5 +3,5 @@ from typing import List
 
 def anagrams(word: str, words: List[str]) -> List[str]:
     """Kata url: https://www.codewars.com/kata/523a86aa4230ebb5420001e1."""
-    sorted_word = sorted(word)
+    sorted_word: List[str] = sorted(word)
     return [wrd for wrd in words if sorted(wrd) == sorted_word]
