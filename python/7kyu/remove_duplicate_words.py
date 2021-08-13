bd659dc6e60bf20dc3d0d9f19081178f0d00458a
Yohann Boniface <edhyjox@gmail.com>
2021-08-13 23:37:19 +0200

    Remove duplicate words

---
+++ python/7kyu/remove_duplicate_words.py
@@ -0,0 +1,7 @@
+# Kata url: https://www.codewars.com/kata/5b39e3772ae7545f650000fc.
+from typing import List
+
+
+def remove_duplicate_words(s: str) -> str:
+    words: List[str] = s.split()
+    return ' '.join(sorted(set(words), key=words.index))
