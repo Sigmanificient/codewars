2e3c636ff7b5e251c5c75d237ab107e8d6304885
Yohann Boniface <edhyjox@gmail.com>
2021-08-13 23:58:17 +0200

    Counting Duplicates

---
+++ python/6kyu/counting_duplicates.py
@@ -0,0 +1,6 @@
+"""Kata url: https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1."""
+
+
+def duplicate_count(text: str) -> int:
+    text: str = text.lower()
+    return sum(True for x in set(text) if text.count(x) > 1)
