463cba5f563d4908a457a8bfff217297c6592085
Yohann Boniface <edhyjox@gmail.com>
2021-08-15 01:41:15 +0200

    Convert A Hex String To RGB

---
+++ js/5kyu/convert_a_hax_string_to_rgb.js
@@ -0,0 +1,11 @@
+/* Kata url: https://www.codewars.com/kata/5282b48bb70058e4c4000fa7. */
+
+function hexStringToRGB(hexString) {
+  let rgb = hexString.substring(1).match(/.{2}/g)
+
+  return {
+    r: parseInt(rgb[0], 16),
+    g: parseInt(rgb[1], 16),
+    b: parseInt(rgb[2], 16)
+  }
+}
