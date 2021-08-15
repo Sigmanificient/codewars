125adca125c2f5a8d857498468fa5484f8750969
Yohann Boniface <edhyjox@gmail.com>
2021-08-15 23:50:42 +0200

    Grasshopper - Create the rooms

---
+++ python/8kyu/grasshopper_create_the_rooms.py
@@ -0,0 +1,21 @@
+"""Kata url: https://www.codewars.com/kata/56a29b237e9e997ff2000048."""
+
+from typing import Dict
+
+rooms: Dict[str, Dict[str, str]] = {
+    '1': {
+        'name': '...',
+        'description': '...',
+        'completed': '...'
+    },
+    '2': {
+        'name': '...',
+        'description': '...',
+        'completed': '...'
+    },
+    '3': {
+        'name': '...',
+        'description': '...',
+        'completed': '...'
+    }
+}
