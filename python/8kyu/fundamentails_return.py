35bef1de0521f2edfa4a89ba79fbff44129062e4
Yohann Boniface <edhyjox@gmail.com>
2021-08-18 23:50:33 +0200

    Fundamentals: Return

---
+++ python/8kyu/fundamentails_return.py
@@ -0,0 +1,25 @@
+"""Kata url: https://www.codewars.com/kata/55a5befdf16499bffb00007b."""
+
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+
+def divide(a: int, b: int) -> float:
+    return a / b
+
+
+def mod(a: int, b: int) -> int:
+    return a % b
+
+
+def exponent(a: int, b: int) -> int:
+    return a ** b
+
+
+def subt(a: int, b: int) -> int:
+    return a - b
