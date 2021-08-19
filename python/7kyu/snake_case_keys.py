a804d29c4ac418330a5a05128530c3385455f74d
Yohann Boniface <edhyjox@gmail.com>
2021-08-19 23:47:12 +0200

    Snake_Casify_Keys

---
+++ python/7kyu/snake_case_keys.py
@@ -0,0 +1,16 @@
+# Kata url: https://www.codewars.com/kata/610c8308ed5ee4003279d112.
+
+from typing import Dict
+
+
+def snake_case(string) -> str:
+    return ''.join(
+        char if char.islower() else f'_{char.lower()}'
+        for char in string
+    )
+
+
+def snake_casify(dictionary) -> Dict[str, str]:
+    return {
+        snake_case(k): v for k, v in dictionary.items()
+    }
