dca382011e195376eaea65afc918888fb3950b8b
Yohann Boniface <edhyjox@gmail.com>
2021-09-12 23:25:03 +0200

    Leonardo Dicaprio and Oscars

---
+++ python/8kyu/loenardo_dicaprio_and_oscars.py
@@ -0,0 +1,14 @@
+"""Kata url: https://www.codewars.com/kata/56d49587df52101de70011e4."""
+
+
+def leo(oscar: int) -> str:
+    if oscar == 88:
+        return "Leo finally won the oscar! Leo is happy"
+
+    if oscar == 86:
+        return "Not even for Wolf of wallstreet?!"
+
+    if oscar < 88:
+        return "When will you give Leo an Oscar?"
+
+    return "Leo got one already!"
