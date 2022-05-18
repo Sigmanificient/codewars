a80b549ed7d6633da5a68ec233bc7c8186eccd90
Yohann Boniface <edhyjox@gmail.com>
2022-05-18 07:46:20 +0200

    Update python/8kyu/convert_a_string_to_number.py

--- python/8kyu/convert_a_string_to_number.py
+++ python/8kyu/convert_a_string_to_number.py
@@ -1,4 +1,5 @@
 """Kata url: https://www.codewars.com/kata/544675c6f971f7399a000e79."""
 
-def string_to_number(s: int) -> int:
+def string_to_number(s: str) -> int:
+    return int(s)
     return s
