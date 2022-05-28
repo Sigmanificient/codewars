812b017e15ea4c8db4f9984b0eb5b4d1a09e71f2
sigmanificient <edhyjox@gmail.com>
2022-05-29 00:46:28 +0200

    :white_check_mark: Added test for permutation

--- python/katas/4kyu/permutations.py
+++ python/katas/4kyu/permutations.py
@@ -5,3 +5,9 @@ from typing import List
 
 def permutations(string: str) -> List[str]:
     return list({''.join(string) for string in p(string)})
+
+
+def test_permutations():
+    assert sorted(permutations('a')) == ['a']
+    assert sorted(permutations('ab')) == ['ab', 'ba']
+    assert sorted(permutations('aabb')) == ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
