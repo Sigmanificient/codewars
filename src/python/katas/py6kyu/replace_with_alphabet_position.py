6d26072cf658abbfb4dc3a3efcb7085a520bfe5d
sigmanificient <edhyjox@gmail.com>
2022-05-29 03:44:57 +0200

    :test_tube: Added test for alphabet position

--- src/python/katas/py6kyu/replace_with_alphabet_position.py
+++ src/python/katas/py6kyu/replace_with_alphabet_position.py
@@ -9,3 +9,16 @@ def alphabet_position(text: str) -> str:
         for char in text.lower()
         if char in alphabet
     )
+
+
+def test_alphabet_position():
+    from random import randint
+
+    r1 = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+    r2 = "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"
+
+    assert alphabet_position("The sunset sets at twelve o' clock.") == r1
+    assert alphabet_position("The narwhal bacons at midnight.") == r2
+
+    number_test = ''.join(str(randint(1, 9)) for _ in range(10))
+    assert alphabet_position(number_test) == ""
