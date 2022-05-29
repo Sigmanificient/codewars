9197f773219af00f3e25299a709b6bcd217e5073
sigmanificient <edhyjox@gmail.com>
2022-05-29 02:46:04 +0200

    :test_tube: Added test for perimeter

--- src/python/katas/py5kyu/perimeter.py
+++ src/python/katas/py5kyu/perimeter.py
@@ -12,3 +12,11 @@ def perimeter(n: int) -> int:
         a, b = b, a + b
         c -= 1
     return 4 * sum(r)
+
+
+def test_perimeter():
+    assert perimeter(5) == 80
+    assert perimeter(7) == 216
+    assert perimeter(20) == 114624
+    assert perimeter(30) == 14098308
+    assert perimeter(100) == 6002082144827584333104
