6afd6bde2cddd14145cd3d0592b35f8c80d5dc82
sigmanificient <edhyjox@gmail.com>
2022-05-29 05:53:22 +0200

    :test_tube: Added test for loop array

--- src/js/katas/7kyu/loop_array.js
+++ src/js/katas/7kyu/loop_array.js
@@ -1,6 +1,8 @@
-// Kata url: https://www.codewars.com/kata/5fd8aa5743b49e0012d43e50.
+/* Kata url: https://www.codewars.com/kata/5fd8aa5743b49e0012d43e50. */
 
 function loopArr(arr, direction, steps) {
     const p = arr.splice(0, (direction === 'left') ? steps : arr.length - steps);
     return [].concat(arr, p);
 }
+
+module.exports = loopArr;
