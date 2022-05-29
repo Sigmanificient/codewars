f65bcc61b4571eeaf948a3804cf6e7b50ccfda4e
sigmanificient <edhyjox@gmail.com>
2022-05-29 03:03:03 +0200

    :test_tube: Added test for perfect power

--- src/python/katas/py5kyu/whats_a_perfect_power_anyway.py
+++ src/python/katas/py5kyu/whats_a_perfect_power_anyway.py
@@ -1,5 +1,6 @@
 """Kata url: https://www.codewars.com/kata/54d4c8b08776e4ad92000835."""
-
+from math import floor, log
+from random import random, randrange
 from typing import Optional, List
 
 
@@ -22,3 +23,22 @@ def isPP(n: int) -> Optional[List[int]]:
             return [possible, c - 1]
 
     return None
+
+
+def test_isPP():
+    pp = [
+        4, 8, 9, 16, 25, 27, 32, 36, 49, 64, 81, 100, 121, 125, 128, 144, 169,
+        196, 216, 225, 243, 256, 289, 324, 343, 361, 400, 441, 484
+    ]
+
+    for item in pp:
+        assert isPP(item) is not None
+
+    for _ in range(100):
+        m = 2 + floor(random() * 255)
+        k = 2 + floor(random() * log(268435455) / log(m))
+        l = m ** k
+        r = isPP(l)
+
+        assert r is not None
+        assert r[0]**r[1] == l
