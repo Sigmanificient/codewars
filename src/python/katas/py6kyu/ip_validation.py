a341ee41a3397fe09c5300bd1718be4873345c65
sigmanificient <edhyjox@gmail.com>
2022-05-29 03:39:09 +0200

    :test_tube: Added test for ip validation

--- src/python/katas/py6kyu/ip_validation.py
+++ src/python/katas/py6kyu/ip_validation.py
@@ -21,3 +21,19 @@ def is_valid_IP(string: str) -> bool:
         return False
 
     return True
+
+
+def test_ips():
+    assert is_valid_IP('12.255.56.1')
+    assert is_valid_IP('127.1.1.0')
+    assert is_valid_IP('0.0.0.0')
+    assert is_valid_IP('0.34.82.53')
+    assert not is_valid_IP('192.168.1.300')
+
+    assert not is_valid_IP('')
+    assert not is_valid_IP('abc.def.ghi.jkl')
+    assert not is_valid_IP('123.456.789.0')
+    assert not is_valid_IP('12.34.56')
+    assert not is_valid_IP('12.34.56 .1')
+    assert not is_valid_IP('12.34.56.-1')
+    assert not is_valid_IP('123.045.067.089')
