17601057e197fce6101a56beef8931e50d05ba32
sigmanificient <edhyjox@gmail.com>
2022-05-29 03:14:51 +0200

    :test_tube: Added test for comp

--- src/python/katas/py6kyu/are_they_the_same.py
+++ src/python/katas/py6kyu/are_they_the_same.py
@@ -13,3 +13,16 @@ def comp(array: Optional[List[int]], array2: Optional[List[int]]) -> bool:
     return all(
         i * i == j for i, j in zip(sorted(array, key=abs), sorted(array2))
     )
+
+
+def test_comp():
+    a1 = [121, 144, 19, 161, 19, 144, 19, 11]
+
+    a2 = [11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19]
+    assert comp(a1, a2)
+
+    a2 = [11 * 21, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19]
+    assert not comp(a1, a2)
+
+    a2 = [11 * 11, 121 * 121, 144 * 144, 190 * 190, 161 * 161, 19 * 19, 144 * 144, 19 * 19]
+    assert not comp(a1, a2)
