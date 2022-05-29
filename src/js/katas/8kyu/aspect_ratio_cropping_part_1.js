f7e422809830ecf992b1fedb97efed607eb329e3
sigmanificient <edhyjox@gmail.com>
2022-05-29 05:56:24 +0200

    :test_tube: Added test for aspect ratio

--- src/js/katas/8kyu/aspect_ratio_cropping_part_1.js
+++ src/js/katas/8kyu/aspect_ratio_cropping_part_1.js
@@ -1,4 +1,7 @@
+/* Kata url: https://www.codewars.com/kata/596e4ef7b61e25981200009f. */
+
 function aspectRatio(x, y) {
-    // Kata url: https://www.codewars.com/kata/596e4ef7b61e25981200009f
     return [Math.ceil(y * 16 / 9), y];
 }
+
+module.exports = aspectRatio;
