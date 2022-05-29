9efb5cc1bed46732c21672a54e086c82d49511ab
sigmanificient <edhyjox@gmail.com>
2022-05-29 03:30:59 +0200

    :test_tube: Added test for counting duplicates

--- src/python/katas/py6kyu/counting_duplicates.py
+++ src/python/katas/py6kyu/counting_duplicates.py
@@ -4,3 +4,11 @@
 def duplicate_count(text: str) -> int:
     text = text.lower()
     return sum(True for x in set(text) if text.count(x) > 1)
+
+
+def test_duplicate_count():
+    assert duplicate_count("") == 0
+    assert duplicate_count("abcde") == 0
+    assert duplicate_count("abcdeaa") == 1
+    assert duplicate_count("abcdeaB") == 2
+    assert duplicate_count("Indivisibilities") == 2
