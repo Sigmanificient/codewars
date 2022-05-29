7671f5422cf195cc5307b36dfd62716891df190a
sigmanificient <edhyjox@gmail.com>
2022-05-29 03:09:24 +0200

    :test_tube: Added test for my anagram

--- src/python/katas/py5kyu/where_my_anagrams_at.py
+++ src/python/katas/py5kyu/where_my_anagrams_at.py
@@ -6,3 +6,8 @@ from typing import List
 def anagrams(word: str, words: List[str]) -> List[str]:
     sorted_word: List[str] = sorted(word)
     return [wrd for wrd in words if sorted(wrd) == sorted_word]
+
+
+def test_anagrams():
+    assert anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) == ['aabb', 'bbaa']
+    assert anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) == ['carer', 'racer']
