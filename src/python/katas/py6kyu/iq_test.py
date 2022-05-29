9b6c2ed806536c41568a7a5f09594d2169349676
sigmanificient <edhyjox@gmail.com>
2022-05-29 03:39:59 +0200

    :test_tube: Added test for iq test

--- src/python/katas/py6kyu/iq_test.py
+++ src/python/katas/py6kyu/iq_test.py
@@ -1,5 +1,11 @@
 """Kata url: https://www.codewars.com/kata/552c028c030765286c00007d."""
 
+
 def iq_test(numbers: str) -> int:
     oddness = [x % 2 for x in map(int, numbers.split())]
     return oddness.index(sum(oddness) == 1) + 1
+
+
+def test_iqs():
+    assert iq_test("2 4 7 8 10") == 3
+    assert iq_test("1 2 2") == 1
