51f5b396384b34953fd89d19100b60260c213162
sigmanificient <edhyjox@gmail.com>
2022-05-31 00:15:23 +0200

    :memo: fixing comment

--- src/php/katas/8kyu/return_negative.php
+++ src/php/katas/8kyu/return_negative.php
@@ -1,12 +1,7 @@
 <?php
+/* https://www.codewars.com/kata/55685cd7ad70877c23000102 */
 
 function makeNegative(float $num) : float
 {
-    /*
-    https://www.codewars.com/kata/55685cd7ad70877c23000102
-
-    In this simple assignment you are given a number and have to make it negative.
-    But maybe the number is already negative?
-    */
     return -abs($num);
 }
