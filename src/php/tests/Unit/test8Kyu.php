900348f64c32217d37a6f98da68f7c33754ebd03
sigmanificient <edhyjox@gmail.com>
2022-05-31 00:52:02 +0200

    :green_heart: Fixed Php coverage

--- src/php/tests/Unit/test8Kyu.php
+++ src/php/tests/Unit/test8Kyu.php
@@ -10,7 +10,8 @@ require_once dirname(__DIR__, 2) . '/katas/8kyu/return_negative.php';
 
 class Test8Kyu extends TestCase
 {
-    /** @test @covers */
+    /* @covers */
+    /* @test */
     public function makeNegative()
     {
         $this->assertEquals(0, makeNegative(0));
