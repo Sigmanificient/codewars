42e8fcd4eb8b9b55bd13aa67b5c38fd3abaa7ef5
sigmanificient <edhyjox@gmail.com>
2022-06-02 00:00:25 +0200

    :white_check_mark: Test for all star challenge 20

--- src/python/katas/py7kyu/all_star_code_challenge_20.py
+++ src/python/katas/py7kyu/all_star_code_challenge_20.py
@@ -1,6 +1,8 @@
 """Kata url: https://www.codewars.com/kata/5865a75da5f19147370000c7."""
 from typing import List, TypeVar
 
+import pytest
+
 Addable = TypeVar("Addable", str, int)
 
 
@@ -9,3 +11,11 @@ def add_arrays(array: List[Addable], array2: List[Addable]) -> List[Addable]:
         raise ValueError
 
     return [a + b for a, b in zip(array, array2)]
+
+
+def test_add_arrays():
+    assert add_arrays([1, 2], [4, 5]) == [5, 7]
+    assert add_arrays(['a'], ['b']) == ['ab']
+
+    with pytest.raises(ValueError):
+        add_arrays([1, 2, 3], [4, 5])
