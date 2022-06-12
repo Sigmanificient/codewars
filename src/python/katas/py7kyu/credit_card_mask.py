76b150eef52c6989436dcdce85f7b06bc3dd5579
sigmanificient <edhyjox@gmail.com>
2022-06-12 23:47:13 +0200

    :white_check_mark: Added test for credit card mask

--- src/python/katas/py7kyu/credit_card_mask.py
+++ src/python/katas/py7kyu/credit_card_mask.py
@@ -3,3 +3,11 @@
 
 def maskify(cc: str) -> str:
     return cc[-4:].rjust(len(cc), '#')
+
+
+def test_maskify():
+    assert maskify("") == ""
+    assert maskify("1") == "1"
+    assert maskify("64607935616") == "#######5616"
+    assert maskify("4556364607935616") == "############5616"
+    assert maskify('SF$SDfgsd2eA') == "########d2eA"
