57c3aed63ddc8eb86636b279133d5d45f794eb4e
sigmanificient <edhyjox@gmail.com>
2022-06-12 23:55:24 +0200

    :white_check_mark: Added test for disemvowel

--- src/python/katas/py7kyu/disemvowel_trolls.py
+++ src/python/katas/py7kyu/disemvowel_trolls.py
@@ -1,4 +1,14 @@
 """Kata url: https://www.codewars.com/kata/52fba66badcd10859f00097e."""
 
+
 def disemvowel(string):
     return ''.join(char for char in string if char.lower() not in 'aeiou')
+
+
+def test_disemvowel():
+    assert disemvowel("") == ""
+    assert disemvowel("aeiou") == ""
+    assert disemvowel("AEIOU") == ""
+
+    assert disemvowel("Hello, world!") == "Hll, wrld!"
+    assert disemvowel("This website is for losers LOL!") == "Ths wbst s fr lsrs LL!"
