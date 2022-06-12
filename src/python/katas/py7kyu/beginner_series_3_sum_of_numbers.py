dc3f5f907de7f4ee4d8ef45e6ccc527a31da541f
sigmanificient <edhyjox@gmail.com>
2022-06-12 23:30:19 +0200

    :white_check_mark: Added test for get sum

--- src/python/katas/py7kyu/beginner_series_3_sum_of_numbers.py
+++ src/python/katas/py7kyu/beginner_series_3_sum_of_numbers.py
@@ -1,7 +1,13 @@
 """Kata url: https://www.codewars.com/kata/55f2b110f61eb01779000053."""
 
+
 def get_sum(a: int, b: int) -> int:
     if b < a:
         a, b = b, a
 
     return sum(range(a, b + 1))
+
+
+def test_get_sum():
+    assert get_sum(0, 1), 1
+    assert get_sum(0, -1) == -1
