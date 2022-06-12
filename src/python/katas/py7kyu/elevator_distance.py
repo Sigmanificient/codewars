e1d54d5ca8f2d4f582bb1b7e8614c43f8ecace24
sigmanificient <edhyjox@gmail.com>
2022-06-12 23:57:13 +0200

    :white_check_mark: Added test for elevator distance

--- src/python/katas/py7kyu/elevator_distance.py
+++ src/python/katas/py7kyu/elevator_distance.py
@@ -5,3 +5,9 @@ from typing import List
 
 def elevator_distance(array: List[int]) -> int:
     return sum(abs(new - previous) for previous, new in zip(array, array[1:]))
+
+
+def test_elevator_distance():
+    assert elevator_distance([5, 2, 8]) == 9
+    assert elevator_distance([1, 2, 3]) == 2
+    assert elevator_distance([7, 1, 7, 1]) == 18
