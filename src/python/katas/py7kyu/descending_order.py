209cdc24b782e485655b632707b945e502a1ada0
sigmanificient <edhyjox@gmail.com>
2022-06-12 23:53:05 +0200

    :white_check_mark: Added test for descending order

--- src/python/katas/py7kyu/descending_order.py
+++ src/python/katas/py7kyu/descending_order.py
@@ -3,3 +3,9 @@
 
 def descending_order(num: int) -> int:
     return int(''.join(sorted(str(num), reverse=True)))
+
+
+def test_order():
+    assert descending_order(0) == 0
+    assert descending_order(15) == 51
+    assert descending_order(123456789) == 987654321
