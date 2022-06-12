6da9c40b75f196ac195e816adf335add3b430156
sigmanificient <edhyjox@gmail.com>
2022-06-12 23:58:46 +0200

    :white_check_mark: Added test for evaporator

--- src/python/katas/py7kyu/evaporator.py
+++ src/python/katas/py7kyu/evaporator.py
@@ -9,3 +9,9 @@ def evaporator(content: float, evap_per_day: int, threshold: int) -> int:
         content *= 1 - (evap_per_day / 100)
         c += 1
     return c
+
+
+def test_evaporator():
+    assert evaporator(1, 1, 1) == 459
+    assert evaporator(5, 8, 10) == 28
+    assert evaporator(10, 10, 10) == 22
