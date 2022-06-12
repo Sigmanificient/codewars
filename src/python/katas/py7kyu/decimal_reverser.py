5497d040315e4bcfdcafcb53e502bd1f4b807a48
sigmanificient <edhyjox@gmail.com>
2022-06-12 23:49:12 +0200

    :white_check_mark: Added test for decimal reverser

--- src/python/katas/py7kyu/decimal_reverser.py
+++ src/python/katas/py7kyu/decimal_reverser.py
@@ -3,3 +3,9 @@
 
 def reverse_float_decimal(n: float) -> float:
     return int(n) + float(f'0.{str(round(n % 1, 5))[2:][::-1]}')
+
+
+def test_reverse_float_decimal():
+    assert reverse_float_decimal(2.5) == 2.5
+    assert reverse_float_decimal(1.12345) == 1.54321
+    assert reverse_float_decimal(7.0007) == 7.7
