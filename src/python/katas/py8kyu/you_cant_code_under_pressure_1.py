d6852c202894f0c4eb5e2126b7f7f006d5ffc759
sigmanificient <edhyjox@gmail.com>
2022-06-17 23:28:48 +0200

    :white_check_mark: Added test for double

--- src/python/katas/py8kyu/you_cant_code_under_pressure_1.py
+++ src/python/katas/py8kyu/you_cant_code_under_pressure_1.py
@@ -3,3 +3,12 @@
 
 def double_integer(i: int) -> int:
     return i * 2
+
+
+def test_double_integer():
+    assert double_integer(1) == 2
+    assert double_integer(2) == 4
+    assert double_integer(10) == 20
+
+    assert double_integer(0) == 0
+    assert double_integer(-1) == -2
