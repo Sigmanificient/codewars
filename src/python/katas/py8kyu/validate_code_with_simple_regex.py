a97ebdeb978f276447753cbb50990f4ce8af32a0
sigmanificient <edhyjox@gmail.com>
2022-06-18 01:07:33 +0200

    :white_check_mark: Added test for code validation

--- src/python/katas/py8kyu/validate_code_with_simple_regex.py
+++ src/python/katas/py8kyu/validate_code_with_simple_regex.py
@@ -1,7 +1,15 @@
-# Kata url: https://www.codewars.com/kata/56a25ba95df27b7743000016.
+"""Kata url: https://www.codewars.com/kata/56a25ba95df27b7743000016."""
 
 import re
 
 
-def validate_code(code: str) -> bool:
-    return bool(re.match(r'[1-3].*', code))
+def validate_code(code: int) -> bool:
+    return bool(re.match(r'[1-3].*', str(code)))
+
+
+def test_validate_code():
+    assert validate_code(123)
+    assert validate_code(248)
+    assert not validate_code(8)
+    assert validate_code(321)
+    assert not validate_code(9453)
