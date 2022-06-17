8e82ed5cb330171083564836b1e1f74156504653
sigmanificient <edhyjox@gmail.com>
2022-06-17 23:33:00 +0200

    :white_check_mark: Added test for enough

--- src/python/katas/py8kyu/will_there_be_enough_space.py
+++ src/python/katas/py8kyu/will_there_be_enough_space.py
@@ -4,3 +4,9 @@
 def enough(cap: int, on: int, wait: int) -> int:
     is_enough: bool = (cap - on) > wait
     return 0 if is_enough else abs((cap - on) - wait)
+
+
+def test_enough():
+    assert enough(10, 5, 5) == 0
+    assert enough(100, 60, 50) == 10
+    assert enough(20, 5, 5) == 0
