5d265fb60ccaf1e84f9f06b2e8f440a714e990ee
sigmanificient <edhyjox@gmail.com>
2022-06-18 01:00:29 +0200

    :white_check_mark: Added test for vowel remover

--- src/python/katas/py8kyu/vowel_remover.py
+++ src/python/katas/py8kyu/vowel_remover.py
@@ -3,3 +3,10 @@
 
 def shortcut(s: str) -> str:
     return ''.join(x for x in s if x not in 'aeiou')
+
+
+def test_shortcut():
+    assert shortcut("") == ""
+    assert shortcut("aeiou") == ""
+    assert shortcut("abc") == "bc"
+    assert shortcut("abcdefghijklmnopqrstuvwxyz") == "bcdfghjklmnpqrstvwxyz"
