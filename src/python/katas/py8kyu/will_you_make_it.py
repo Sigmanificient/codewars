d38a7dccee05229e69a2715215ffc586a4481ecc
sigmanificient <edhyjox@gmail.com>
2022-06-17 23:31:08 +0200

    :white_check_mark: Added test for zero_dual

--- src/python/katas/py8kyu/will_you_make_it.py
+++ src/python/katas/py8kyu/will_you_make_it.py
@@ -1,5 +1,14 @@
 """Kata url: https://www.codewars.com/kata/5861d28f124b35723e00005."""
 
 
-def zero_fuel(distance_to_pump: int,mpg: int, fuel_left: int) -> bool:
+def zero_fuel(distance_to_pump: int, mpg: int, fuel_left: int) -> bool:
     return distance_to_pump <= mpg * fuel_left
+
+
+def test_zero_fuel():
+    assert zero_fuel(50, 25, 2)
+    assert zero_fuel(100, 50, 2)
+    assert zero_fuel(25, 100, 2)
+
+    assert not zero_fuel(100, 25, 2)
+    assert not zero_fuel(6, 6, 0)
