5548ce06cedcbeae64c05baece318872fbf2812e
sigmanificient <edhyjox@gmail.com>
2022-06-17 23:25:37 +0200

    :white_check_mark: Added test for check

--- src/python/katas/py8kyu/you_only_need_one_beginner.py
+++ src/python/katas/py8kyu/you_only_need_one_beginner.py
@@ -5,3 +5,22 @@ from typing import List
 
 def check(seq: List[int], elem: int) -> bool:
     return elem in seq
+
+
+def test_check():
+    tests = [
+        (True, ([66, 101], 66)),
+        (False, ([78, 117, 110, 99, 104, 117, 107, 115], 8)),
+        (True, ([101, 45, 75, 105, 99, 107], 107)),
+        (True, ([80, 117, 115, 104, 45, 85, 112, 115], 45)),
+        (True, (['t', 'e', 's', 't'], 'e')),
+        (False, (["what", "a", "great", "kata"], "kat")),
+        (True, ([66, "codewars", 11, "alex loves pushups"], "alex loves pushups")),
+        (False, (["come", "on", 110, "2500", 10, '!', 7, 15], "Come")),
+        (True, (["when's", "the", "next", "Katathon?", 9, 7], "Katathon?")),
+        (False, ([8, 7, 5, "bored", "of", "writing", "tests", 115], 45)),
+        (True, (["anyone", "want", "to", "hire", "me?"], "me?")),
+    ]
+
+    for exp, inp in tests:
+        assert check(*inp) == exp
