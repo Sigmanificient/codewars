07cb2c39d7b699779b27a86ee570abfea32f7e32
sigmanificient <edhyjox@gmail.com>
2022-06-17 23:35:42 +0200

    :white_check_mark: Added test for test between

--- src/python/katas/py8kyu/what_in_between.py
+++ src/python/katas/py8kyu/what_in_between.py
@@ -5,3 +5,9 @@ from typing import List
 
 def between(a: int, b: int) -> List[int]:
     return list(range(a, b + 1))
+
+
+def test_between():
+    assert between(1, 3) == [1, 2, 3]
+    assert between(1, 5) == [1, 2, 3, 4, 5]
+    assert between(1, 10) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
