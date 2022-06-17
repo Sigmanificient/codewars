13a0f66e49478a2e36bd1a929ded5a8cdce42600
sigmanificient <edhyjox@gmail.com>
2022-06-17 23:50:23 +0200

    :white_check_mark: Added test for watermelon

--- src/python/katas/py8kyu/watermelon.py
+++ src/python/katas/py8kyu/watermelon.py
@@ -3,3 +3,16 @@
 
 def divide(weight: int) -> bool:
     return not weight % 2 and weight != 2
+
+
+def test_divide():
+    assert divide(4)
+    assert not divide(2)
+    assert not divide(5)
+    assert divide(88)
+    assert divide(100)
+    assert not divide(67)
+    assert divide(90)
+    assert divide(10)
+    assert not divide(99)
+    assert divide(32)
