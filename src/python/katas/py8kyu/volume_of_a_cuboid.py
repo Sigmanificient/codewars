faf26830decc1f2a919a58c8a83696ba4d5ee272
sigmanificient <edhyjox@gmail.com>
2022-06-18 01:05:03 +0200

    :white_check_mark: Added test for cuboid

--- src/python/katas/py8kyu/volume_of_a_cuboid.py
+++ src/python/katas/py8kyu/volume_of_a_cuboid.py
@@ -1,5 +1,12 @@
 """Kata url: https://www.codewars.com/kata/58261acb22be6e2ed800003a."""
 
 
-def get_volume_of_cubiod(length: int, width: int, height: int) -> int:
+def get_volume_of_cuboid(length: float, width: float, height: float) -> float:
     return length * width * height
+
+
+def test_volume():
+    assert get_volume_of_cuboid(1, 1, 1) == 1
+    assert get_volume_of_cuboid(1, 2, 3) == 6
+    assert get_volume_of_cuboid(2, 3, 4) == 24
+    assert get_volume_of_cuboid(6.3, 2, 5) == 63
