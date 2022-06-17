ced07dddaf037b7dec0a93394d7843a2d8570af3
sigmanificient <edhyjox@gmail.com>
2022-06-17 23:47:38 +0200

    :white_check_mark: Added test for welcome

--- src/python/katas/py8kyu/welcome_to_the_city.py
+++ src/python/katas/py8kyu/welcome_to_the_city.py
@@ -6,3 +6,12 @@ from typing import List
 
 def say_hello(name: List[str], city: str, state: str) -> str:
     return f"Hello, {' '.join(name)}! Welcome to {city}, {state}!"
+
+
+def test_say_hello():
+    assert say_hello(['John', 'Smith'], 'Phoenix', 'Arizona') == 'Hello, John Smith! Welcome to Phoenix, Arizona!'
+    assert say_hello(['Franklin', 'Delano', 'Roosevelt'], 'Chicago', 'Illinois') == 'Hello, Franklin Delano Roosevelt! Welcome to Chicago, Illinois!'
+    assert say_hello(['Wallace', 'Russel', 'Osbourne'], 'Albany', 'New York') == 'Hello, Wallace Russel Osbourne! Welcome to Albany, New York!'
+    assert say_hello(['Lupin', 'the', 'Third'], 'Los Angeles', 'California') == 'Hello, Lupin the Third! Welcome to Los Angeles, California!'
+    assert say_hello(['Marlo', 'Stanfield'], 'Baltimore', 'Maryland') == 'Hello, Marlo Stanfield! Welcome to Baltimore, Maryland!'
+    assert say_hello(['', '', ''], '', '') == 'Hello, ! Welcome to , !'
