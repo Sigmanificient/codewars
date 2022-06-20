4a884767e332a53d60c3838d35f703d601067f81
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:37:40 +0200

    :white_check_mark: Added test for square | square root

--- src/python/katas/py8kyu/to_square_or_not_to_square.py
+++ src/python/katas/py8kyu/to_square_or_not_to_square.py
@@ -6,3 +6,14 @@ from math import sqrt
 
 def square_or_square_root(arr):
     return [x ** 2 if (s := sqrt(x)) % 1 else s for x in arr]
+
+
+def test_square_or_square_root():
+    tests = [
+        [[4, 3, 9, 7, 2, 1], [2, 9, 3, 49, 4, 1]],
+        [[100, 101, 5, 5, 1, 1], [10, 10201, 25, 25, 1, 1]],
+        [[1, 2, 3, 4, 5, 6], [1, 4, 9, 2, 25, 36]],
+    ]
+
+    for inp, exp in tests:
+        assert square_or_square_root(inp) == exp
