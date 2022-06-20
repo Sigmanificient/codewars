f49b01cc46b3c3620673aacccad4a0fda10c9ed0
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:38:36 +0200

    :white_check_mark: Added test for third angle of triangle

--- src/python/katas/py8kyu/third_angle_of_a_triangle.py
+++ src/python/katas/py8kyu/third_angle_of_a_triangle.py
@@ -3,3 +3,10 @@
 
 def other_angle(a: int, b: int) -> int:
     return 180 - (a + b)
+
+
+def test_other_angle():
+    assert other_angle(30, 60) == 90
+    assert other_angle(60, 60) == 60
+    assert other_angle(43, 78) == 59
+    assert other_angle(10, 20) == 150
