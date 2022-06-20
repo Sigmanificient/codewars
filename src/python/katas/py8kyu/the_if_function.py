bbdd1348ba35c7fffd8dba52377071bbf4736497
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:48:15 +0200

    :white_check_mark: Added test for _if

--- src/python/katas/py8kyu/the_if_function.py
+++ src/python/katas/py8kyu/the_if_function.py
@@ -6,3 +6,8 @@ from typing import Any, Callable
 
 def _if(_bool: bool, func1: Callable, func2: Callable) -> Any:
     return (func1 if _bool else func2)()
+
+
+def test_if():
+    assert _if(True, lambda: 1, lambda: 0) == 1
+    assert _if(False, lambda: 1, lambda: 0) == 0
