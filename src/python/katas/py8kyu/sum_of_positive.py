86c1277755b121bb9e4db0197867e81321986919
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:56:59 +0200

    :white_check_mark: Added test for sum of positive

--- src/python/katas/py8kyu/sum_of_positive.py
+++ src/python/katas/py8kyu/sum_of_positive.py
@@ -5,3 +5,12 @@ from typing import List
 
 def positive_sum(arr: List[int]) -> int:
     return sum(x for x in arr if x > 0)
+
+
+def test_positive_sum():
+    assert positive_sum([1, 2, 3, 4, 5]) == 15
+    assert positive_sum([1, -2, 3, 4, 5]) == 13
+    assert positive_sum([-1, 2, 3, 4, -5]) == 9
+
+    assert positive_sum([]) == 0
+    assert positive_sum([-1, -2, -3, -4, -5]) == 0
