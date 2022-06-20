4cd27f732a3a084719568079aa07e0e6714e3fcb
sigmanificient <edhyjox@gmail.com>
2022-06-21 00:05:55 +0200

    :white_check_mark: Added test for smallest next id
    Signed-off-by: sigmanificient <edhyjox@gmail.com>

--- src/python/katas/py8kyu/smallest_unused_id.py
+++ src/python/katas/py8kyu/smallest_unused_id.py
@@ -11,3 +11,16 @@ def next_id(arr: List[int]) -> int:
             return c
         c += 1
     return c
+
+
+def test_next_id():
+    assert next_id([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 11
+    assert next_id([5, 4, 3, 2, 1]) == 0
+    assert next_id([0, 1, 2, 3, 5]) == 4
+    assert next_id([0, 0, 0, 0, 0, 0]) == 1
+    assert next_id([]) == 0
+    assert next_id([0, 0, 1, 1, 2, 2]) == 3
+    assert next_id([0, 1, 1, 1, 3, 2]) == 4
+    assert next_id([0, 1, 0, 2, 0, 3]) == 4
+    assert next_id([9, 8, 0, 1, 7, 6]) == 2
+    assert next_id([9, 8, 7, 6, 5, 4]) == 0
