81c0f69624906474239616af7bf5a7af41946732
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:21:18 +0200

    :white_check_mark: Added test for twice as old

--- src/python/katas/py8kyu/twice_as_old.py
+++ src/python/katas/py8kyu/twice_as_old.py
@@ -3,3 +3,11 @@
 
 def twice_as_old(dad_years_old: int, son_years_old: int) -> int:
     return abs(dad_years_old - (2 * son_years_old))
+
+
+def test_twice_as_old():
+    assert twice_as_old(36, 7) == 22
+    assert twice_as_old(55, 30) == 5
+    assert twice_as_old(42, 21) == 0
+    assert twice_as_old(22, 1) == 20
+    assert twice_as_old(29, 0) == 29
