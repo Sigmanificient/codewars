3fc4105be9c4c8cb5d52d8bc8d5df43bbc038631
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:41:06 +0200

    :white_check_mark: Added test for is opposite

--- src/python/katas/py8kyu/they_say_that_only_the_name_is_long_enough_to_attract_attention_they_also_said that_only_a_simple_kata_will_have_someone_to_solve_it_This_is_a_sadly_story_#1_Are_they_opposite.py
+++ src/python/katas/py8kyu/they_say_that_only_the_name_is_long_enough_to_attract_attention_they_also_said that_only_a_simple_kata_will_have_someone_to_solve_it_This_is_a_sadly_story_#1_Are_they_opposite.py
@@ -2,3 +2,11 @@
 
 def is_opposite(s1: str, s2: str) -> bool:
     return bool(len(s1)) and s1 == s2.swapcase()
+
+
+def test_is_opposite():
+    assert is_opposite("ab", "AB")
+    assert is_opposite("aB", "Ab")
+    assert is_opposite("aBcd", "AbCD")
+    assert not is_opposite("AB", "Ab")
+    assert not is_opposite("", "")
