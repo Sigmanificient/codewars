2c24fd24883b46d4a2522aab6dc1ec1c75c0a06a
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:49:09 +0200

    :white_check_mark: Added test for template strings

--- src/python/katas/py8kyu/template_feature.py
+++ src/python/katas/py8kyu/template_feature.py
@@ -3,3 +3,10 @@
 
 def temple_strings(obj: str, feature: str) -> str:
     return f"{obj} are {feature}"
+
+
+def test_temple_strings():
+    assert temple_strings("Animals","Good") == 'Animals are Good'
+    assert temple_strings("Animals","Good") == 'Animals are Good'
+    assert temple_strings("You","Special") == 'You are Special'
+    assert temple_strings("lives","frozen") == 'lives are frozen'
