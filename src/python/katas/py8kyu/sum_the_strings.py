a2b511c66d94332ed44c0a94876e1afec5c40237
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:56:23 +0200

    :white_check_mark: Added test for str sum

--- src/python/katas/py8kyu/sum_the_strings.py
+++ src/python/katas/py8kyu/sum_the_strings.py
@@ -3,3 +3,12 @@
 
 def sum_str(a: str, b: str) -> str:
     return f"{int(f'0{a}') + int(f'0{b}')}"
+
+
+def test_sum_str():
+    assert sum_str("4", "5") == "9"
+    assert sum_str("34", "5") == "39"
+
+    assert sum_str("9", "") == "9"
+    assert sum_str("", "9") == "9"
+    assert sum_str("", "") == "0"
