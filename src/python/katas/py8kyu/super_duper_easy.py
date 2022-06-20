3875944d30df35a6ab8d18f3313e2ca455dd3d11
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:53:52 +0200

    :white_check_mark: Added test for "problem"

--- src/python/katas/py8kyu/super_duper_easy.py
+++ src/python/katas/py8kyu/super_duper_easy.py
@@ -5,3 +5,8 @@ from typing import Union
 
 def problem(a: Union[int, str]) -> Union[int, str]:
     return 'Error' if isinstance(a, str) else a * 50 + 6
+
+
+def test_problem():
+    assert problem("hello") == "Error"
+    assert problem(1) == 56
