f5403f04dfb34573fe2e76a45dc9a54e1a6383d5
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:22:06 +0200

    :white_check_mark: Added triple double

--- src/python/katas/py8kyu/triple_double.py
+++ src/python/katas/py8kyu/triple_double.py
@@ -3,3 +3,11 @@
 
 def triple_trouble(one: str, two: str, three: str) -> str:
     return ''.join(a + b + c for a, b, c in zip(one, two, three))
+
+
+def test_triple_trouble():
+    assert triple_trouble("aaa", "bbb", "ccc") == "abcabcabc"
+    assert triple_trouble("aaaaaa", "bbbbbb", "cccccc") == "abcabcabcabcabcabc"
+    assert triple_trouble("burn", "reds", "roll") == "brrueordlnsl"
+    assert triple_trouble("Bm", "aa", "tn") == "Batman"
+    assert triple_trouble("LLh", "euo", "xtr") == "LexLuthor"
