1069552782a41b90bd367df844721113de85166f
sigmanificient <edhyjox@gmail.com>
2022-06-21 00:03:27 +0200

    :white_check_mark: Added test for square n sum

--- src/python/katas/py8kyu/square_n_sum.py
+++ src/python/katas/py8kyu/square_n_sum.py
@@ -5,3 +5,11 @@ from typing import List
 
 def square_sum(numbers: List[int]) -> int:
     return sum(x ** 2 for x in numbers)
+
+
+def test_square_sum():
+    assert square_sum([1, 2]) == 5
+    assert square_sum([0, 3, 4, 5]) == 50
+    assert square_sum([]) == 0
+    assert square_sum([-1, -2]) == 5
+    assert square_sum([-1, 0, 1]) == 2
