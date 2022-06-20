b9f37204a97ca30e200e44e57b818af7cc3d306c
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:19:13 +0200

    :white_check_mark: Added test for create array

--- src/python/katas/py8kyu/unfinished_loop_bug_Fixing_#1.py
+++ src/python/katas/py8kyu/unfinished_loop_bug_Fixing_#1.py
@@ -5,3 +5,16 @@ from typing import List
 
 def create_array(n: int) -> List[int]:
     return list(range(1, n + 1))
+
+
+def test_create_array():
+    assert create_array(1) == [1]
+    assert create_array(2) == [1, 2]
+    assert create_array(3) == [1, 2, 3]
+    assert create_array(4) == [1, 2, 3, 4]
+    assert create_array(5) == [1, 2, 3, 4, 5]
+    assert create_array(6) == [1, 2, 3, 4, 5, 6]
+    assert create_array(7) == [1, 2, 3, 4, 5, 6, 7]
+    assert create_array(8) == [1, 2, 3, 4, 5, 6, 7, 8]
+    assert create_array(9) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    assert create_array(10) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
