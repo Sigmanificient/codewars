1449d0218975f4ec3020a24a62e3279a7180742c
sigmanificient <edhyjox@gmail.com>
2022-06-21 00:02:54 +0200

    :white_check_mark: Added test for squash the bugs

--- src/python/katas/py8kyu/squash_the_bugs.py
+++ src/python/katas/py8kyu/squash_the_bugs.py
@@ -3,3 +3,11 @@
 
 def find_longest(string: str) -> int:
     return max(len(x) for x in string.split())
+
+
+def test_find_longest():
+    assert find_longest("The quick white fox jumped around the massive dog") == 7
+    assert find_longest("Take me to tinseltown with you") == 10
+    assert find_longest("Sausage chops") == 7
+    assert find_longest("Wind your body and wiggle your belly") == 6
+    assert find_longest("Lets all go on holiday") == 7
