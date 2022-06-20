e57209813a9cec14bbce27bb4fcc0a670ee6038e
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:54:48 +0200

    :white_check_mark: Added test for array sum

--- src/python/katas/py8kyu/sum_without_highest_and_lowest_number.py
+++ src/python/katas/py8kyu/sum_without_highest_and_lowest_number.py
@@ -5,3 +5,19 @@ from typing import Optional, List
 
 def sum_array(arr: Optional[List[int]]) -> int:
     return sum(sorted(arr)[1:-1]) if arr else 0
+
+
+def test_sum_array():
+    assert sum_array(None) == 0
+    assert sum_array([]) == 0
+
+    assert sum_array([3]) == 0
+    assert sum_array([-3]) == 0
+
+    assert sum_array([3, 5]) == 0
+    assert sum_array([-3, -5]) == 0
+
+    assert sum_array([6, 2, 1, 8, 10]) == 16
+    assert sum_array([6, 0, 1, 10, 10]) == 17
+    assert sum_array([-6, -20, -1, -10, -12]) == -28
+    assert sum_array([-6, 20, -1, 10, -12]) == 3
