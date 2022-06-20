00703a81880190293cb9c797c83fcebc31829cec
sigmanificient <edhyjox@gmail.com>
2022-06-21 00:01:21 +0200

    :white_check_mark: Added test repeat str

--- src/python/katas/py8kyu/string_repeat.py
+++ src/python/katas/py8kyu/string_repeat.py
@@ -3,3 +3,9 @@
 
 def repeat_str(repeat: int, string: str) -> str:
     return string * repeat
+
+
+def test_repeat_str():
+    assert repeat_str(4, 'a') == 'aaaa'
+    assert repeat_str(3, 'hello ') == 'hello hello hello '
+    assert repeat_str(2, 'abc') == 'abcabc'
