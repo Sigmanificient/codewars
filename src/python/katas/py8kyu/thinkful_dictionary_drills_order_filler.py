513702a654eeb5399a214f7ca2278d258690ff30
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:40:04 +0200

    :white_check_mark: Added test for fillabe

--- src/python/katas/py8kyu/thinkful_dictionary_drills_order_filler.py
+++ src/python/katas/py8kyu/thinkful_dictionary_drills_order_filler.py
@@ -5,3 +5,16 @@ from typing import Dict
 
 def fillable(stock: Dict[str, int], merch: str, n: int):
     return stock.get(merch, False) >= n
+
+
+def test_fillable():
+    stock = {
+        'football': 4,
+        'boardgame': 10,
+        'leggos': 1,
+        'doll': 5
+    }
+
+    assert fillable(stock, 'football', 3)
+    assert not fillable(stock, 'leggos', 2)
+    assert not fillable(stock, 'action figure', 1)
