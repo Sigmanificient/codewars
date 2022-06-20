60604c452f25efdded569f079ae84bc4d57446a0
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:20:45 +0200

    :white_check_mark: Added test for uefa

--- src/python/katas/py8kyu/uefa_euro_2016.py
+++ src/python/katas/py8kyu/uefa_euro_2016.py
@@ -11,3 +11,11 @@ def uefa_euro_2016(teams: List[str], scores: List[int]) -> str:
         if scores[0] == scores[1] else f"{teams[scores[0] < scores[1]]} won!"
     )
     return f"At match {team_1} - {team_2}, {won}"
+
+
+def test_uefa_euro_2016():
+    assert uefa_euro_2016(['Germany', 'Ukraine'], [2, 0]) == "At match Germany - Ukraine, Germany won!";
+    assert uefa_euro_2016(['Belgium', 'Italy'], [0, 2]) == "At match Belgium - Italy, Italy won!"
+    assert uefa_euro_2016(['Portugal', 'Iceland'], [1, 1]) == "At match Portugal - Iceland, teams played draw."
+    assert uefa_euro_2016(['Albania', 'Switzerland'], [1, 2]) == "At match Albania - Switzerland, Switzerland won!"
+    assert uefa_euro_2016(['Republic of Ireland', 'Sweden'], [0, 0]) == "At match Republic of Ireland - Sweden, teams played draw."
