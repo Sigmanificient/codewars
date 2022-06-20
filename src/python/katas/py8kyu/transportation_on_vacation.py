00233ee21e731550955ee5f875ef3c3e0384fae2
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:28:54 +0200

    :white_check_mark: Added test for rental cost

--- src/python/katas/py8kyu/transportation_on_vacation.py
+++ src/python/katas/py8kyu/transportation_on_vacation.py
@@ -11,3 +11,10 @@ def rental_car_cost(d: int) -> int:
         total -= 20
 
     return total
+
+
+def get_rental_car_cost():
+    assert rental_car_cost(1) == 40
+    assert rental_car_cost(4) == 140
+    assert rental_car_cost(7) == 230
+    assert rental_car_cost(8) == 270
