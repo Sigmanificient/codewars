b9a34346f59cd24f9da84fff67dd7c2b2d7dcd54
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:59:59 +0200

    :white_check_mark: Added test for sringy string

--- src/python/katas/py8kyu/stringy_strings.py
+++ src/python/katas/py8kyu/stringy_strings.py
@@ -3,3 +3,17 @@
 
 def stringy(size):
     return '10' * (size // 2) + ('1' * (size % 2))
+
+
+def test_stringy():
+    assert type(stringy(5)) == str
+    assert stringy(10)[0] == '1'
+
+    for i in range(1, 5):
+        assert len(stringy(i)) == i
+
+    assert stringy(3) == '101'
+    assert stringy(5) == '10101'
+    assert stringy(12) == '101010101010'
+    assert stringy(26) == '10101010101010101010101010'
+    assert stringy(28) == '1010101010101010101010101010'
