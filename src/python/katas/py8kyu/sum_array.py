243563c02953ccbcbbf85c57a6221c1ff0acf650
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:58:20 +0200

    :white_check_mark: Added test for sum array

--- src/python/katas/py8kyu/sum_array.py
+++ src/python/katas/py8kyu/sum_array.py
@@ -5,3 +5,11 @@ from typing import List
 
 def sum_array(a: List[int]) -> int:
     return sum(a)
+
+
+def test_sum_array():
+    assert sum_array([]) == 0
+    assert sum_array([1, 2, 3]) == 6
+    assert sum_array([1.1, 2.2, 3.3]) == 6.6
+    assert sum_array([4, 5, 6]) == 15
+    assert sum_array(range(101)) == 5050
