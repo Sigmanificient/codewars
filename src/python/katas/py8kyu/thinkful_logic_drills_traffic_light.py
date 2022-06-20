9ce069753fa8e9fef5070b9699697a15e1109f05
sigmanificient <edhyjox@gmail.com>
2022-06-20 23:39:16 +0200

    :white_check_mark: Added test for update light

--- src/python/katas/py8kyu/thinkful_logic_drills_traffic_light.py
+++ src/python/katas/py8kyu/thinkful_logic_drills_traffic_light.py
@@ -7,3 +7,9 @@ lights: List[str] = ['green', 'yellow', 'red']
 
 def update_light(current: str) -> str:
     return lights[(lights.index(current) + 1) % len(lights)]
+
+
+def test_update_light():
+    assert update_light('green') == 'yellow'
+    assert update_light('yellow') == 'red'
+    assert update_light('red') == 'green'
