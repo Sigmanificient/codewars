c066ef7c357a2298997222fe986e402e2e8e0d45
sigmanificient <edhyjox@gmail.com>
2022-06-21 00:04:14 +0200

    :white_check_mark: Added test for array madness

--- src/python/katas/py8kyu/speed_code_#2_array_madness.py
+++ src/python/katas/py8kyu/speed_code_#2_array_madness.py
@@ -5,3 +5,8 @@ from typing import List
 
 def array_madness(a: List[int], b: List[int]) -> bool:
     return sum(i ** 2 for i in a) > sum(j ** 3 for j in b)
+
+
+def test_array_madness():
+    assert array_madness([4, 5, 6], [1, 2, 3])
+    assert not array_madness([1, 2, 3], [4, 5, 6])
