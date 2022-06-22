16db58c6d84dce1b17c0610af85644a757f4a14b
sigmanificient <edhyjox@gmail.com>
2022-06-22 23:56:24 +0200

    :white_check_mark: Added test for simple_multiplication
    Signed-off-by: sigmanificient <edhyjox@gmail.com>

--- src/python/katas/py8kyu/simple_multplication.py
+++ src/python/katas/py8kyu/simple_multplication.py
@@ -3,3 +3,11 @@
 
 def simple_multiplication(number: int) -> int:
     return number * (8 + number % 2)
+
+
+def test_simple_multiplication():
+    assert simple_multiplication(2) == 16
+    assert simple_multiplication(1) == 9
+    assert simple_multiplication(8) == 64
+    assert simple_multiplication(4) == 32
+    assert simple_multiplication(5) == 45
