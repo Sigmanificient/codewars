ac82da390ca6444d412c7829bdca490e566561f5
sigmanificient <edhyjox@gmail.com>
2022-06-22 23:56:56 +0200

    :white_check_mark: Added test for calculator
    Signed-off-by: sigmanificient <edhyjox@gmail.com>

--- src/python/katas/py8kyu/simple_calculator.py
+++ src/python/katas/py8kyu/simple_calculator.py
@@ -24,3 +24,11 @@ def calculator(
         return _err
 
     return func(x, y)
+
+
+def test_calculator():
+    assert calculator(6, 2, '+') == 8
+    assert calculator(4, 3, '-') == 1
+    assert calculator(5, 5, '*') == 25
+    assert calculator(5, 4, '/') == 1.25
+    assert calculator(6, 2, '&') == "unknown value"
