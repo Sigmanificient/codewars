35f6a878fee0cb44afefd715ce26ab1d88a102d3
sigmanificient <edhyjox@gmail.com>
2022-06-23 00:00:24 +0200

    :white_check_mark: Added test for smash
    Signed-off-by: sigmanificient <edhyjox@gmail.com>

--- src/python/katas/py8kyu/sentence_smash.py
+++ src/python/katas/py8kyu/sentence_smash.py
@@ -6,3 +6,12 @@ from typing import List
 
 def smash(words: List[str]) -> str:
     return ' '.join(words)
+
+
+def test_smash():
+    assert smash([]) == ""
+    assert smash(["hello"]) == "hello"
+    assert smash(["hello", "world"]) == "hello world"
+    assert smash(["hello", "amazing", "world"]) == "hello amazing world"
+    assert smash(["this", "is", "a", "really", "long", "sentence"]) == "this is a really long sentence"
+
