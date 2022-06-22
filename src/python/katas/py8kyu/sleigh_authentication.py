610c5d96cec2c11f45dac21708187d27b7f7db41
sigmanificient <edhyjox@gmail.com>
2022-06-22 23:55:50 +0200

    :white_check_mark: Added test for sleigh
    Signed-off-by: sigmanificient <edhyjox@gmail.com>

--- src/python/katas/py8kyu/sleigh_authentication.py
+++ src/python/katas/py8kyu/sleigh_authentication.py
@@ -6,3 +6,12 @@ class Sleigh:
     @staticmethod
     def authenticate(name, password):
         return name == "Santa Claus" and password == "Ho Ho Ho!"
+
+
+def test_sleigh():
+    sleigh = Sleigh()
+
+    assert sleigh.authenticate('Santa Claus', 'Ho Ho Ho!')
+    assert not sleigh.authenticate('Santa', 'Ho Ho Ho!')
+    assert not sleigh.authenticate('Santa Claus', 'Ho Ho!')
+    assert not sleigh.authenticate('jhoffner', 'CodeWars')
