5666648f9a809ee4a4fb4571869448fe4e26db15
sigmanificient <edhyjox@gmail.com>
2022-06-22 23:57:30 +0200

    :white_check_mark: Added test for long short
    Signed-off-by: sigmanificient <edhyjox@gmail.com>

--- src/python/katas/py8kyu/short_long_short.py
+++ src/python/katas/py8kyu/short_long_short.py
@@ -6,3 +6,10 @@ def solution(a, b):
         a, b = b, a
 
     return f"{a}{b}{a}"
+
+
+def test_solution():
+    assert solution('45', '1') == '1451'
+    assert solution('13', '200') == '1320013'
+    assert solution('Soon', 'Me') == 'MeSoonMe'
+    assert solution('U', 'False') == 'UFalseU'
