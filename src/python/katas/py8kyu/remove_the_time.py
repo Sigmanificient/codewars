5db2a3fae04aea3b42413fe696d9bfbc9ec04b0a
sigmanificient <edhyjox@gmail.com>
2022-06-24 00:04:07 +0200

    :white_check_mark: test for remove the time

--- src/python/katas/py8kyu/remove_the_time.py
+++ src/python/katas/py8kyu/remove_the_time.py
@@ -2,3 +2,11 @@
 
 def shorten_to_date(long_date: str) -> str:
     return long_date.split(',')[0]
+
+
+def test_shorten_to_date():
+    assert shorten_to_date("Monday February 2, 8pm") == "Monday February 2"
+    assert shorten_to_date("Tuesday May 29, 8pm") == "Tuesday May 29"
+    assert shorten_to_date("Wed September 1, 3am") == "Wed September 1"
+    assert shorten_to_date("Friday May 2, 9am") == "Friday May 2"
+    assert shorten_to_date("Tuesday January 29, 10pm") == "Tuesday January 29"
