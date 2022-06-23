587abfa6ffd902a7b5fdbd8101081cc34000d340
sigmanificient <edhyjox@gmail.com>
2022-06-24 00:11:49 +0200

    :white_check_mark: test for quarter of

--- src/python/katas/py8kyu/quarter_of_the_year.py
+++ src/python/katas/py8kyu/quarter_of_the_year.py
@@ -5,3 +5,9 @@ from math import ceil
 
 def quarter_of(month: int) -> int:
     return ceil(month / 3)
+
+
+def test_quarter_of():
+    assert quarter_of(3) == 1
+    assert quarter_of(8) == 3
+    assert quarter_of(11) == 4
