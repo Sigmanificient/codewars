5b7f163d93d3dbc755ffbb1562f2ceddb3660008
sigmanificient <edhyjox@gmail.com>
2022-06-24 00:03:22 +0200

    :white_check_mark: test for removing every other

--- src/python/katas/py8kyu/removing_elements.py
+++ src/python/katas/py8kyu/removing_elements.py
@@ -7,3 +7,10 @@ def remove_every_other(
         my_list: List[Union[int, str]]
 ) -> List[Union[int, str]]:
     return [x for c, x in enumerate(my_list) if not c % 2]
+
+
+def test_remove_every_other():
+    assert remove_every_other(['Hello', 'Goodbye', 'Hello Again']) == ['Hello', 'Hello Again']
+    assert remove_every_other([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == [1, 3, 5, 7, 9]
+    assert remove_every_other([[1, 2]]) == [[1, 2]]
+    assert remove_every_other([['Goodbye'], {'Great': 'Job'}]) == [['Goodbye']]
