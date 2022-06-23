18c89baecaff859b9ac5ad66ce3317b9c29ad1b0
sigmanificient <edhyjox@gmail.com>
2022-06-24 00:11:12 +0200

    :white_check_mark: test for count lowercase letters

--- src/python/katas/py8kyu/regex_count_lowercase_letters.py
+++ src/python/katas/py8kyu/regex_count_lowercase_letters.py
@@ -3,3 +3,12 @@
 
 def lowercase_count(string: str) -> int:
     return sum(x.islower() for x in string)
+
+
+def test_lowercase_count():
+    assert lowercase_count("") == 0
+    assert lowercase_count("abc") == 3
+    assert lowercase_count("abcABC123") == 3
+    assert lowercase_count("abcABC123!@#$%^&*()_-+=}{[]|\':;?/>.<,~") == 3
+    assert lowercase_count("ABC123!@#$%^&*()_-+=}{[]|\':;?/>.<,~") == 0
+    assert lowercase_count("abcdefghijklmnopqrstuvwxyz") == 26
