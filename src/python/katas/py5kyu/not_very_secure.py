3dc7ec2443a92ab60da27649857e114f212735d2
sigmanificient <edhyjox@gmail.com>
2022-06-24 00:43:13 +0200

    :sparkles: Added not very secure (5kyu)

---
+++ src/python/katas/py5kyu/not_very_secure.py
@@ -0,0 +1,18 @@
+import string
+
+
+def alphanumeric(password):
+    return len(password) > 0 and all(
+        x.lower() in string.ascii_lowercase + string.digits
+        for x in password
+    )
+
+
+def test_alphanumeric():
+    assert not alphanumeric('')
+    assert not alphanumeric(' ' * 5)
+
+    assert alphanumeric('abc')
+    assert alphanumeric('abc123')
+    assert not alphanumeric('abc123%')
+    assert not alphanumeric('abc123@')
