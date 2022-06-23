17f7221f4ec71c454ebbfadb84e9714e881aeb22
sigmanificient <edhyjox@gmail.com>
2022-06-23 23:59:07 +0200

    :white_check_mark: test for whatday

--- src/python/katas/py8kyu/return_the_day.py
+++ src/python/katas/py8kyu/return_the_day.py
@@ -13,3 +13,11 @@ def whatday(num: int) -> str:
         return "Wrong, please enter a number between 1 and 7"
 
     return week[num - 1]
+
+
+def test_whatday():
+    assert whatday(1) == 'Sunday'
+    assert whatday(2) == 'Monday'
+    assert whatday(3) == 'Tuesday'
+    assert whatday(8) == 'Wrong, please enter a number between 1 and 7'
+    assert whatday(20) == 'Wrong, please enter a number between 1 and 7'
