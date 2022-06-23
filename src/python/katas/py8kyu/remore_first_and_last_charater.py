100ecb2b7c525bd2392d3de0c2d0a302ecf89619
sigmanificient <edhyjox@gmail.com>
2022-06-24 00:08:30 +0200

    :white_check_mark: test for remove char from string

--- src/python/katas/py8kyu/remore_first_and_last_charater.py
+++ src/python/katas/py8kyu/remore_first_and_last_charater.py
@@ -3,3 +3,12 @@
 
 def remove_char(s: str) -> str:
     return s[1:-1]
+
+
+def test_remove_char():
+    assert remove_char('ok') == ''
+    assert remove_char('ooopsss') == 'oopss'
+    assert remove_char('eloquent') == 'loquen'
+    assert remove_char('country') == 'ountr'
+    assert remove_char('person') == 'erso'
+    assert remove_char('place') == 'lac'
