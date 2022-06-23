bc94c426303b3e3bc4f89b8bf44513b80c857f74
sigmanificient <edhyjox@gmail.com>
2022-06-23 23:57:54 +0200

    :white_check_mark: test for highest values

--- src/python/katas/py8kyu/return_two_highest_values_in_list.py
+++ src/python/katas/py8kyu/return_two_highest_values_in_list.py
@@ -5,3 +5,10 @@ from typing import List
 
 def two_highest(arg1: List[int]) -> List[int]:
     return sorted(set(arg1), reverse=True)[:2]
+
+
+def test_two_highest():
+    assert two_highest([]) == []
+    assert two_highest([1, 2, 3, 4, 5]) == [5, 4]
+    assert two_highest([15, 20, 20, 17]) == [20, 17]
+    assert two_highest([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == [10, 9]
