c6276c08634d9d90bcb99b30f70077103f72ba73
sigmanificient <edhyjox@gmail.com>
2022-06-23 23:56:15 +0200

    :white_check_mark: test for reverse sequence

--- src/python/katas/py8kyu/reversed_sequence.py
+++ src/python/katas/py8kyu/reversed_sequence.py
@@ -5,3 +5,9 @@ from typing import List
 
 def reverse_seq(n) -> List[int]:
     return list(range(n, 0, -1))
+
+
+def test_reverse_seq():
+    assert reverse_seq(0) == []
+    assert reverse_seq(5) == [5, 4, 3, 2, 1]
+    assert reverse_seq(10) == [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
