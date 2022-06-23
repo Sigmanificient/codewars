c3f1ee5222c7fa26634b7b56768f57cd49e6d46a
sigmanificient <edhyjox@gmail.com>
2022-06-24 00:02:31 +0200

    :white_check_mark: test for repeat it

--- src/python/katas/py8kyu/repeat_it.py
+++ src/python/katas/py8kyu/repeat_it.py
@@ -5,3 +5,8 @@ from typing import Optional
 
 def repeat_it(string: Optional[str], n: int) -> str:
     return string * n if isinstance(string, str) else 'Not a string'
+
+
+def test_repeat_it():
+    assert repeat_it("*", 3) == "***"
+    assert repeat_it("Hello", 5) == "HelloHelloHelloHelloHello"
