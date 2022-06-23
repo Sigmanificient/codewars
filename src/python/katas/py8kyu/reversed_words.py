37dd649e34961396eac320df88350949f5125c26
sigmanificient <edhyjox@gmail.com>
2022-06-23 23:54:50 +0200

    :white_check_mark: test for reverse word

--- src/python/katas/py8kyu/reversed_words.py
+++ src/python/katas/py8kyu/reversed_words.py
@@ -3,3 +3,8 @@
 
 def reverse_words(s: str) -> str:
     return ' '.join(s.split()[::-1])
+
+
+def test_reverse_words():
+    assert reverse_words('Hello World') == 'World Hello'
+    assert reverse_words('Hi There.') == 'There. Hi'
