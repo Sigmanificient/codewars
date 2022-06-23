b5350363082ec988fc2cd51517c89cef803caa56
sigmanificient <edhyjox@gmail.com>
2022-06-24 00:07:47 +0200

    :white_check_mark: test for remove duplicate

--- src/python/katas/py8kyu/remove_duplicates_from_list.py
+++ src/python/katas/py8kyu/remove_duplicates_from_list.py
@@ -5,3 +5,11 @@ from typing import List
 
 def distinct(seq: List[int]) -> List[int]:
     return sorted(list(set(seq)), key=lambda x: seq.index(x))
+
+
+def test_distinct():
+    assert distinct([1]) == [1]
+    assert distinct([1, 2]) == [1, 2]
+    assert distinct([1, 1, 2]) == [1, 2]
+    assert distinct([1, 1, 1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+    assert distinct([1, 2, 2, 3, 3, 4, 4, 5, 6, 7, 7, 7]) == [1, 2, 3, 4, 5, 6, 7]
