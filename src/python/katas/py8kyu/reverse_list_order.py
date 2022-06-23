2ace41245845194dfe6f8a58917e707016dff765
sigmanificient <edhyjox@gmail.com>
2022-06-23 23:56:41 +0200

    :white_check_mark: test for reverse list

--- src/python/katas/py8kyu/reverse_list_order.py
+++ src/python/katas/py8kyu/reverse_list_order.py
@@ -5,3 +5,10 @@ from typing import List
 
 def reverse_list(collection: List[int]) -> List[int]:
     return collection[::-1]
+
+
+def test_reverse_list():
+    assert reverse_list([1, 2, 3, 4]) == [4, 3, 2, 1]
+    assert reverse_list([3, 1, 5, 4]) == [4, 5, 1, 3]
+    assert reverse_list([3, 6, 9, 2]) == [2, 9, 6, 3]
+    assert reverse_list([1]) == [1]
