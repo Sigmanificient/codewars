f65c9e191d4c07bd4cf1fffd0272ad4b67ffb004
sigmanificient <edhyjox@gmail.com>
2022-06-24 19:54:18 +0200

    :white_check_mark: Added tests for get middle

--- src/python/katas/py7kyu/get_the_middle_character.py
+++ src/python/katas/py7kyu/get_the_middle_character.py
@@ -5,3 +5,11 @@ def get_middle(s: str) -> str:
     length: int = len(s)
     middle: int = length // 2
     return s[middle - (not length % 2):middle + 1]
+
+
+def test_get_middle():
+    assert get_middle("test") == "es"
+    assert get_middle("testing") == "t"
+    assert get_middle("middle") == "dd"
+    assert get_middle("A") == "A"
+    assert get_middle("of") == "of"
