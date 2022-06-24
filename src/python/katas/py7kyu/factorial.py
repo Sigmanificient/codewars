2f1df99c582a44cc3a6455d48c8be508e54926ac
sigmanificient <edhyjox@gmail.com>
2022-06-24 19:50:39 +0200

    :white_check_mark: Added tests for factorial

--- src/python/katas/py7kyu/factorial.py
+++ src/python/katas/py7kyu/factorial.py
@@ -3,3 +3,19 @@
 
 def factorial(n: int) -> int:
     return n * factorial(n - 1) if n else 1
+
+
+def test_factorial():
+    tests = (
+        (0, 1),
+        (1, 1),
+        (2, 2),
+        (3, 6),
+        (4, 24),
+        (5, 120),
+        (6, 720),
+        (7, 5040),
+    )
+
+    for inp, exp in tests:
+        assert factorial(inp) == exp
