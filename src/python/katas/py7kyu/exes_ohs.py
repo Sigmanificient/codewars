cc1e6ec4786a957eb9378b6e8cc4bde9272aa944
sigmanificient <edhyjox@gmail.com>
2022-06-24 23:58:04 +0200

    :sparkles: Added solution for xo

---
+++ src/python/katas/py7kyu/exes_ohs.py
@@ -0,0 +1,16 @@
+"""Kara url: https://www.codewars.com/kata/55908aad6620c066bc00002a."""
+
+
+def xo(s: str) -> bool:
+    s = s.lower()
+    return s.count('x') == s.count('o')
+
+
+def test_xo():
+    assert xo('xo')
+    assert xo('xxOo')
+    assert not xo('xxxm')
+    assert not xo('ooom')
+    assert not xo('xxxoooxx')
+    assert not xo('xx')
+    assert not xo('Oo')
