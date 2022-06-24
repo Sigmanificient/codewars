24e32ceadeb40ed61504d541e080b3d0483099e6
sigmanificient <edhyjox@gmail.com>
2022-06-24 19:49:24 +0200

    :white_check_mark: Added tests for even and odd

--- src/python/katas/py7kyu/even_numbers_in_an_array.py
+++ src/python/katas/py7kyu/even_numbers_in_an_array.py
@@ -6,3 +6,9 @@ from typing import List
 def even_numbers(arr: List[int], n: int) -> List[int]:
     odds: List[int] = [x for x in arr if not x % 2]
     return odds[len(odds) - n:]
+
+
+def test_even_and_odd():
+    assert even_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) == [4, 6, 8]
+    assert even_numbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) == [-8, 26]
+    assert even_numbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) == [6]
