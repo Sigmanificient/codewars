5fcbbcb7a8c323c8645c05ec1725b8ae74432daf
sigmanificient <edhyjox@gmail.com>
2022-06-24 23:48:19 +0200

    :white_check_mark: Added test for printing array

--- src/python/katas/py8kyu/printing_array_elements_with_command_delimiters.py
+++ src/python/katas/py8kyu/printing_array_elements_with_command_delimiters.py
@@ -5,3 +5,20 @@ from typing import List
 
 def print_array(arr: List[int]) -> str:
     return ','.join(map(str, arr))
+
+
+def test_print_array():
+    assert print_array([2]) == "2"
+    assert print_array([2, 4, 5, 2]) == "2,4,5,2"
+    assert print_array([2, 4, 5, 2]) == "2,4,5,2"
+    assert print_array([2.0, 4.2, 5.1, 2.2]) == "2.0,4.2,5.1,2.2"
+    assert print_array(["2", "4", "5", "2"]) == "2,4,5,2"
+    assert print_array([True, False, False]) == "True,False,False"
+
+    assert print_array(
+        ["hello", "this", "is", "an", "array!", "a", "b", "c", "d", "e!"]
+    ) == "hello,this,is,an,array!,a,b,c,d,e!"
+
+    assert print_array(
+        [["hello", "this", "is", "an", "array!"], [1, 2, 3, 4, 5]]
+    ) == "['hello', 'this', 'is', 'an', 'array!'],[1, 2, 3, 4, 5]"
