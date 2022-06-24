c11a984ab2ec390c6a65e99a9d6ab58f55e841bc
sigmanificient <edhyjox@gmail.com>
2022-06-24 16:04:43 +0200

    :sparkles: String incrementer

---
+++ src/python/katas/py5kyu/string_incrementer.py
@@ -0,0 +1,22 @@
+"""Kata url: https://www.codewars.com/kata/54a91a4883a7de5d7800009c."""
+
+
+def increment_string(string: str) -> str:
+    i = 0
+    while i < len(string) and string[-(i + 1)].isdigit():
+        i += 1
+
+    if not i:
+        return string + '1'
+
+    return f"{string[:-i]}{int(string[-i:]) + 1:0{i}}"
+
+
+def test_increment_string():
+    assert increment_string("foo") == "foo1"
+    assert increment_string("foobar001") == "foobar002"
+    assert increment_string("foobar1") == "foobar2"
+    assert increment_string("foobar00") == "foobar01"
+    assert increment_string("foobar99") == "foobar100"
+    assert increment_string("foobar099") == "foobar100"
+    assert increment_string("") == "1"
