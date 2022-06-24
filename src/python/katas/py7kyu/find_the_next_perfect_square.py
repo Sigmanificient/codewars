6c0c36bbd66deec0f26873c63cd559ab7c2e0288
sigmanificient <edhyjox@gmail.com>
2022-06-24 19:52:53 +0200

    :white_check_mark: Added tests for find next square

--- src/python/katas/py7kyu/find_the_next_perfect_square.py
+++ src/python/katas/py7kyu/find_the_next_perfect_square.py
@@ -7,3 +7,12 @@ def find_next_square(sq: int) -> float:
     r: float = math.sqrt(sq)
 
     return -1 if r % 1 else (r + 1) ** 2
+
+
+def test_find_next_square():
+    assert find_next_square(121) == 144
+    assert find_next_square(625) == 676
+    assert find_next_square(319225) == 320356
+    assert find_next_square(15241383936) == 15241630849
+
+    assert find_next_square(342786627) == -1
