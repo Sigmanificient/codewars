f7a5088f8bcb7683267249db8b726bda7cf7e2fd
sigmanificient <edhyjox@gmail.com>
2022-06-24 15:39:48 +0200

    :sparkles: Added valid parentheses

---
+++ src/python/katas/py5kyu/valid_parentheses.py
@@ -0,0 +1,21 @@
+def valid_parentheses(string):
+    depth = 0
+
+    for ch in string:
+        if ch == '(':
+            depth += 1
+        elif ch == ')':
+            depth -= 1
+
+        if depth < 0:
+            return False
+
+    return depth == 0
+
+
+def test_valid_parentheses():
+    assert valid_parentheses("")
+    assert valid_parentheses("hi(hi)()")
+    assert not valid_parentheses("  (")
+    assert not valid_parentheses(")test")
+    assert not valid_parentheses("hi())(")
