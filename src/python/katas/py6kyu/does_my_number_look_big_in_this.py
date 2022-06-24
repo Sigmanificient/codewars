807ec2f3f66c68ea4edf37498684866d4607faab
sigmanificient <edhyjox@gmail.com>
2022-06-24 15:18:16 +0200

    :sparkles: Added narcissistic number checker

---
+++ src/python/katas/py6kyu/does_my_number_look_big_in_this.py
@@ -0,0 +1,15 @@
+"""https://www.codewars.com/kata/5287e858c6b5a9678200083c."""
+
+
+def narcissistic(value):
+    _str_val = str(value)
+    _len = len(_str_val)
+    return sum(int(d) ** _len for d in _str_val) == value
+
+
+def test_narcissistic():
+    assert narcissistic(0)
+    assert narcissistic(7)
+    assert narcissistic(371)
+    assert not narcissistic(122)
+    assert not narcissistic(4887)
