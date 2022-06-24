db2fe80e05b7c2f3f82b312a910e123d8200b19c
sigmanificient <edhyjox@gmail.com>
2022-06-24 19:54:51 +0200

    :white_check_mark: Added tests for growth

--- src/python/katas/py7kyu/growth_of_a_population.py
+++ src/python/katas/py7kyu/growth_of_a_population.py
@@ -10,3 +10,9 @@ def nb_year(p0: int, percent: float, aug: int, final: int) -> int:
         m = int(m + (m * percent) + aug)
         y += 1
     return y
+
+
+def test_growth():
+    assert nb_year(1500, 5, 100, 5000) == 15
+    assert nb_year(1500000, 2.5, 10000, 2000000) == 10
+    assert nb_year(1500000, 0.25, 1000, 2000000) == 94
