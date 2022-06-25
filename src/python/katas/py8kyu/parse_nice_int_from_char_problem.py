b03d5af6af231eef5a2080e474eeb08d0a109ac9
sigmanificient <edhyjox@gmail.com>
2022-06-25 07:08:23 +0200

    :white_check_mark: Added tests for get age

--- src/python/katas/py8kyu/parse_nice_int_from_char_problem.py
+++ src/python/katas/py8kyu/parse_nice_int_from_char_problem.py
@@ -3,3 +3,10 @@
 
 def get_age(age: str) -> int:
     return int(age[0])
+
+
+def test_get_age():
+    assert get_age("2 years old") == 2
+    assert get_age("4 years old") == 4
+    assert get_age("5 years old") == 5
+    assert get_age("7 years old") == 7
