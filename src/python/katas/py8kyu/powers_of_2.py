3d49edb1977ba8f034f7a2b2b13fe593aea62568
sigmanificient <edhyjox@gmail.com>
2022-06-25 07:06:19 +0200

    :white_check_mark: Added tests for powers of 2

--- src/python/katas/py8kyu/powers_of_2.py
+++ src/python/katas/py8kyu/powers_of_2.py
@@ -5,3 +5,9 @@ from typing import List
 
 def powers_of_two(n: int) -> List[int]:
     return [2 ** i for i in range(n + 1)]
+
+
+def test_powers_of_two():
+    assert powers_of_two(0) == [1]
+    assert powers_of_two(1) == [1, 2]
+    assert powers_of_two(4) == [1, 2, 4, 8, 16]
