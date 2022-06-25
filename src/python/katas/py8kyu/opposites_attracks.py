1343fb8f0b2548815b878a1b49c53112a601d1fa
sigmanificient <edhyjox@gmail.com>
2022-06-25 07:11:00 +0200

    :white_check_mark: Added tests for lovefunc

--- src/python/katas/py8kyu/opposites_attracks.py
+++ src/python/katas/py8kyu/opposites_attracks.py
@@ -3,3 +3,10 @@
 
 def lovefunc(flower1: int, flower2: int) -> bool:
     return bool(flower1 % 2 - flower2 % 2)
+
+
+def test_lovefunc():
+    assert lovefunc(1, 4)
+    assert not lovefunc(2, 2)
+    assert lovefunc(0, 1)
+    assert not lovefunc(0, 0)
