27bf0487607a191f1a7b76c114e4c3ca8dba6bce
sigmanificient <edhyjox@gmail.com>
2022-06-25 07:13:19 +0200

    :white_check_mark: Added tests for super size

--- src/python/katas/py8kyu/noob_code_01_supersize_me_or_rather_this_integer.py
+++ src/python/katas/py8kyu/noob_code_01_supersize_me_or_rather_this_integer.py
@@ -3,3 +3,16 @@
 
 def super_size(n: int) -> int:
     return int(''.join(sorted(str(n))))
+
+
+def test_super_size():
+    assert super_size(0) == 0
+    assert super_size(2) == 2
+    assert super_size(69) == 96
+    assert super_size(513) == 531
+    assert super_size(2017) == 7210
+    assert super_size(414) == 441
+    assert super_size(666666) == 666666
+    assert super_size(608719) == 987610
+    assert super_size(123456789) == 987654321
+    assert super_size(700000000001) == 710000000000
