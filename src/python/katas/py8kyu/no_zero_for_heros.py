dcdfbb273b03a89b126c82a5c02fab1ca5879872
sigmanificient <edhyjox@gmail.com>
2022-06-25 07:13:58 +0200

    :white_check_mark: Added tests for no boring zeros

--- src/python/katas/py8kyu/no_zero_for_heros.py
+++ src/python/katas/py8kyu/no_zero_for_heros.py
@@ -8,3 +8,12 @@ def no_boring_zeros(n: int) -> int:
     while not n % 10:
         n //= 10
     return n
+
+
+def test_no_boring_zeros():
+    assert no_boring_zeros(0) == 0
+    assert no_boring_zeros(1450) == 145
+    assert no_boring_zeros(960000) == 96
+    assert no_boring_zeros(1050) == 105
+    assert no_boring_zeros(-1050) == -105
+    assert no_boring_zeros(2016) == 2016
