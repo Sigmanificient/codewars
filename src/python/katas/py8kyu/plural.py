f018484204a3d29eaa24d659c23ffb75a349e958
sigmanificient <edhyjox@gmail.com>
2022-06-25 07:07:29 +0200

    :white_check_mark: Added tests for plural

--- src/python/katas/py8kyu/plural.py
+++ src/python/katas/py8kyu/plural.py
@@ -5,3 +5,9 @@ from typing import Union
 
 def plural(n: Union[float, int]) -> bool:
     return n != 1
+
+
+def test_plural():
+    assert plural(0)
+    assert not plural(1)
+    assert plural(100)
