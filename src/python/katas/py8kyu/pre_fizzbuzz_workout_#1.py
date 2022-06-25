fefe79d9cb165b87032ce19e409e441fe6e47af4
sigmanificient <edhyjox@gmail.com>
2022-06-25 07:05:55 +0200

    :white_check_mark: Added tests for pre_fizz

--- src/python/katas/py8kyu/pre_fizzbuzz_workout_#1.py
+++ src/python/katas/py8kyu/pre_fizzbuzz_workout_#1.py
@@ -6,3 +6,11 @@ from typing import List
 
 def pre_fizz(n: int) -> List[int]:
     return list(range(1, n + 1))
+
+
+def test_pre_fizz():
+    assert pre_fizz(1) == [1]
+    assert pre_fizz(2) == [1, 2]
+    assert pre_fizz(3) == [1, 2, 3]
+    assert pre_fizz(4) == [1, 2, 3, 4]
+    assert pre_fizz(5) == [1, 2, 3, 4, 5]
