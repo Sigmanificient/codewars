5ac400781b4658eb4d607f7425270ef5e025f71b
sigmanificient <edhyjox@gmail.com>
2022-06-25 07:11:30 +0200

    :white_check_mark: Added tests for opposite

--- src/python/katas/py8kyu/opposite_number.py
+++ src/python/katas/py8kyu/opposite_number.py
@@ -3,3 +3,12 @@
 
 def opposite(number: float) -> float:
     return -number
+
+
+def test_opposite():
+    assert opposite(1) == -1
+    assert opposite(25.6) == -25.6
+    assert opposite(0) == 0
+    assert opposite(1425.2222) == -1425.2222
+    assert opposite(-3.1458) == 3.1458
+    assert opposite(-95858588225) == 95858588225
