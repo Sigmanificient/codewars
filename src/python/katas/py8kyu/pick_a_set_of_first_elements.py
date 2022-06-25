212c4650eee3c956bc767cc522e9bd4509264849
sigmanificient <edhyjox@gmail.com>
2022-06-25 07:08:00 +0200

    :white_check_mark: Added tests for first

--- src/python/katas/py8kyu/pick_a_set_of_first_elements.py
+++ src/python/katas/py8kyu/pick_a_set_of_first_elements.py
@@ -6,3 +6,12 @@ from typing import List
 
 def first(seq: List[int], n: int = 1) -> List[int]:
     return seq[:n]
+
+
+def test_first():
+    seq = ['a', 'b', 'c', 'd', 'e']
+    assert first(seq) == ['a']
+    assert first(seq, 0) == []
+    assert first(seq, 1) == ['a']
+    assert first(seq, 2) == ['a', 'b']
+    assert first(seq, 10) == ['a', 'b', 'c', 'd', 'e']
