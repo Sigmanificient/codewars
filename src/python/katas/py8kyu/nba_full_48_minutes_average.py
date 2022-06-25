ac83db9590685a1ac27d8bd4b5fcca690c25f58b
sigmanificient <edhyjox@gmail.com>
2022-06-25 07:16:44 +0200

    :test_tube: Added tests nba_full

--- src/python/katas/py8kyu/nba_full_48_minutes_average.py
+++ src/python/katas/py8kyu/nba_full_48_minutes_average.py
@@ -3,3 +3,12 @@
 
 def nba_extrap(ppg: int, mpg: int) -> float:
     return round((ppg / mpg) * 48, 1) or 0
+
+
+def test_nba_extrap():
+    assert nba_extrap(0, 0) == 0
+    assert nba_extrap(12, 20) == 28.8
+    assert nba_extrap(10, 10) == 48.0
+    assert nba_extrap(5, 17) == 14.1
+    assert nba_extrap(30.8, 34.7) == 42.6
+    assert nba_extrap(22.9, 33.8) == 32.5
