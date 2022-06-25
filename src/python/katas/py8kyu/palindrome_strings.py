2781e00890f0272ff3272c51fcbf9ceb916b2452
sigmanificient <edhyjox@gmail.com>
2022-06-25 07:10:20 +0200

    :white_check_mark: Added tests for is palindrome

--- src/python/katas/py8kyu/palindrome_strings.py
+++ src/python/katas/py8kyu/palindrome_strings.py
@@ -7,3 +7,10 @@ from typing import Union
 def is_palindrome(string: Union[str, int]) -> bool:
     string = str(string)
     return string == string[::-1]
+
+
+def test_is_palindrome():
+    assert is_palindrome("anna")
+    assert not is_palindrome("walter")
+    assert is_palindrome(12321)
+    assert not is_palindrome(123456)
