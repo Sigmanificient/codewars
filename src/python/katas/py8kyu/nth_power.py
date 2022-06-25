7a0e3142fb6f42ae1ca1faab12bedfed4dbb3dad
sigmanificient <edhyjox@gmail.com>
2022-06-25 07:19:39 +0200

    :white_check_mark: Added test for nth power

--- src/python/katas/py8kyu/nth_power.py
+++ src/python/katas/py8kyu/nth_power.py
@@ -5,3 +5,9 @@ from typing import List
 
 def index(array: List[int], n: int) -> int:
     return array[n] ** n if (len(array) > n) else -1
+
+
+def test_index():
+    assert index([1, 2, 3, 4], 2) == 9
+    assert index([1, 3, 10, 100], 3) == 1000000
+    assert index([1, 3, 10, 100], 4) == -1
