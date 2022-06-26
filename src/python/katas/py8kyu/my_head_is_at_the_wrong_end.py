e147584a9581e1f734eac223d3aa350dbcf7062b
sigmanificient <edhyjox@gmail.com>
2022-06-26 23:34:52 +0200

    :white_check_mark: Added test for fix the meerkat

--- src/python/katas/py8kyu/my_head_is_at_the_wrong_end.py
+++ src/python/katas/py8kyu/my_head_is_at_the_wrong_end.py
@@ -5,3 +5,11 @@ from typing import List
 
 def fix_the_meerkat(arr: List[str]) -> List[str]:
     return arr[::-1]
+
+
+def test_fix_the_meerkat():
+    assert fix_the_meerkat(["tail", "body", "head"]) == ["head", "body", "tail"]
+    assert fix_the_meerkat(["tails", "body", "heads"]) == ["heads", "body", "tails"]
+    assert fix_the_meerkat(["bottom", "middle", "top"]) == ["top", "middle", "bottom"]
+    assert fix_the_meerkat(["lower legs", "torso", "upper legs"]) == ["upper legs", "torso", "lower legs"]
+    assert fix_the_meerkat(["ground", "rainbow", "sky"]) == ["sky", "rainbow", "ground"]
