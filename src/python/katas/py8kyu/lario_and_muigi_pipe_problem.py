202a4c68e8df9a063eae4930eb2ed080d5fa92a3
sigmanificient <edhyjox@gmail.com>
2022-06-26 23:46:46 +0200

    :white_check_mark: Added test for pipe fix

--- src/python/katas/py8kyu/lario_and_muigi_pipe_problem.py
+++ src/python/katas/py8kyu/lario_and_muigi_pipe_problem.py
@@ -5,3 +5,11 @@ from typing import List
 
 def pipe_fix(nums: List[int]) -> List[int]:
     return list(range(nums[0], nums[-1] + 1))
+
+
+def test_pipe_fix():
+    assert pipe_fix([1, 2, 3, 5, 6, 8, 9]) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    assert pipe_fix([1, 2, 3, 12]) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    assert pipe_fix([6, 9]) == [6, 7, 8, 9]
+    assert pipe_fix([-1, 4]) == [-1, 0, 1, 2, 3, 4]
+    assert pipe_fix([1, 2, 3]) == [1, 2, 3]
