c6d4fb9083077abe78aee46510eeca06cd2321d6
sigmanificient <edhyjox@gmail.com>
2022-06-26 23:43:53 +0200

    :white_check_mark: Added test for make uppercase

--- src/python/katas/py8kyu/make_uppercase.py
+++ src/python/katas/py8kyu/make_uppercase.py
@@ -3,3 +3,10 @@
 
 def make_upper_case(s: str) -> str:
     return s.upper()
+
+
+def test_make_upper_case():
+    assert make_upper_case("hello") == "HELLO"
+    assert make_upper_case("world") == "WORLD"
+    assert make_upper_case("hello world") == "HELLO WORLD"
+    assert make_upper_case("") == ""
