6699b44fcb18746c4e809fe81cafc92b58ae5450
sigmanificient <edhyjox@gmail.com>
2022-06-26 23:40:45 +0200

    :white_check_mark: Merging sorted integer array

--- src/python/katas/py8kyu/merging_sorted_integer_arrays.py
+++ src/python/katas/py8kyu/merging_sorted_integer_arrays.py
@@ -5,3 +5,8 @@ from typing import List
 
 def merge_arrays(first: List[int], second: List[int]) -> List[int]:
     return sorted(set(first + second))
+
+
+def test_merge_arrays():
+    assert merge_arrays([1, 3, 5], [2, 4, 6]) == [1, 2, 3, 4, 5, 6]        
+    assert merge_arrays([2, 4, 8], [2, 4, 6]) == [2, 4, 6, 8]
