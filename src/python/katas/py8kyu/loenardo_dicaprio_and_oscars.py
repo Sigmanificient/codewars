b140c770e2974c39565de036726ea796695804f6
sigmanificient <edhyjox@gmail.com>
2022-06-26 23:46:13 +0200

    :white_check_mark: Added test for leo func

--- src/python/katas/py8kyu/loenardo_dicaprio_and_oscars.py
+++ src/python/katas/py8kyu/loenardo_dicaprio_and_oscars.py
@@ -12,3 +12,10 @@ def leo(oscar: int) -> str:
         return "When will you give Leo an Oscar?"
 
     return "Leo got one already!"
+
+
+def test_leo():
+    assert leo(89) == "Leo got one already!"
+    assert leo(88) == "Leo finally won the oscar! Leo is happy"
+    assert leo(87) == "When will you give Leo an Oscar?"
+    assert leo(86) == "Not even for Wolf of wallstreet?!"
