5bea3b506c08ebf647f7e4ef109ef2e4f1c403db
sigmanificient <edhyjox@gmail.com>
2022-06-26 23:36:28 +0200

    :white_check_mark: Added test for multiply

--- src/python/katas/py8kyu/multiply.py
+++ src/python/katas/py8kyu/multiply.py
@@ -3,3 +3,13 @@
 
 def multiply(a: int, b: int) -> int:
     return a * b
+
+
+def test_multiply():
+    assert multiply(1, 0) == 0
+    assert multiply(1, -1) == -1
+
+    assert multiply(2, 1) == 2
+    assert multiply(2, 2) == 4
+    assert multiply(2, 3) == 6
+    assert multiply(99, 99) == 9801
