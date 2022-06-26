67c5b3da79f9fe2390ff67dffb26df2350d23d2a
sigmanificient <edhyjox@gmail.com>
2022-06-26 23:41:55 +0200

    :white_check_mark: Added test for merging array

--- src/python/katas/py8kyu/merg_two_sorted_arrays_into_one.py
+++
@@ -1,8 +0,0 @@
-"""Kata url: https://www.codewars.com/kata/5899642f6e1b25935d000161."""
-
-
-from typing import List
-
-
-def merge_arrays(arr1: List[int], arr2: List[int]) -> List[int]:
-    return sorted(set(arr1 + arr2))
---
+++ src/python/katas/py8kyu/merge_two_sorted_arrays_into_one.py
@@ -0,0 +1,14 @@
+"""Kata url: https://www.codewars.com/kata/5899642f6e1b25935d000161."""
+
+
+from typing import List
+
+
+def merge_arrays(arr1: List[int], arr2: List[int]) -> List[int]:
+    return sorted(set(arr1 + arr2))
+
+
+def test_merge_arrays():
+    assert merge_arrays([1, 2, 3, 4], [5, 6, 7, 8]) == [1, 2, 3, 4, 5, 6, 7, 8]
+    assert merge_arrays([1, 3, 5, 7, 9], [10, 8, 6, 4, 2]) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert merge_arrays([1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12]) == [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
