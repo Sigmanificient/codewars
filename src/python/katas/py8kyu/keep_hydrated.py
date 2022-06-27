546c82e0a9d937818dcf0e98ac47b4b13085e794
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:30:10 +0200

    :white_check_mark: Added test for keep hydrated

--- src/python/katas/py8kyu/keep_hydrated.py
+++ src/python/katas/py8kyu/keep_hydrated.py
@@ -3,3 +3,13 @@
 
 def litres(time: float) -> float:
     return time // 2
+
+
+def test_litres():
+    assert litres(2) == 1
+    assert litres(1.4) == 0
+    assert litres(12.3) == 6
+    assert litres(0.82) == 0
+    assert litres(11.8) == 5
+    assert litres(1787) == 893
+    assert litres(0) == 0
