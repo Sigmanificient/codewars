4a123aaf746fa48f8f5a8f7f794b6e4d4aef6899
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:40:24 +0200

    :white_check_mark: Added test for is divisible by x & y

--- src/python/katas/py8kyu/is_divisible_by_x_and_y.py
+++ src/python/katas/py8kyu/is_divisible_by_x_and_y.py
@@ -3,3 +3,10 @@
 
 def is_divisible(n: int, x: int, y: int) -> bool:
     return (n / x).is_integer() and (n / y).is_integer()
+
+
+def test_is_divisible():
+    assert is_divisible(12, 3, 4)
+    assert not is_divisible(3, 2, 2)
+    assert not is_divisible(3, 3, 4)
+    assert not is_divisible(8, 3, 4)
