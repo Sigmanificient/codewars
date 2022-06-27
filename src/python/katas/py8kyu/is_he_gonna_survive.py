f2b477a8b79a6f5c781e17785883f707b10e3eb4
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:39:23 +0200

    :white_check_mark: Added test for hero

--- src/python/katas/py8kyu/is_he_gonna_survive.py
+++ src/python/katas/py8kyu/is_he_gonna_survive.py
@@ -3,3 +3,14 @@
 
 def hero(bullets: int, dragons: int) -> bool:
     return bullets >= dragons * 2
+
+
+def test_hero():
+    assert hero(10, 5)
+    assert hero(100, 40)
+    assert not hero(4, 5)
+    assert not hero(7, 4)
+    assert not hero(1500, 751)
+    assert not hero(0, 1)
+
+
