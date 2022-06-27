5f75840121dc5600a92c7584c3cd8ca281b37318
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:31:14 +0200

    :white_check_mark: Added test for greet

--- src/python/katas/py8kyu/johny_secret_message.py
+++ src/python/katas/py8kyu/johny_secret_message.py
@@ -1,4 +1,12 @@
 """Kata url: https://www.codewars.com/kata/55225023e1be1ec8bc000390."""
 
+
 def greet(name: str) -> str:
     return "Hello, my love!" if name == "Johnny" else f"Hello, {name}!"
+
+
+def test_greet():
+    assert greet("James") == "Hello, James!"
+    assert greet("Jane") == "Hello, Jane!"
+    assert greet("Jim") == "Hello, Jim!"
+    assert greet("Johnny") == "Hello, my love!"
