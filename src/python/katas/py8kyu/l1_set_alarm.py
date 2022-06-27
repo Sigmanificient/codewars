956f7816f2949058e4d16689046a67492127b190
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:27:57 +0200

    :white_check_mark: Added test for set alarm

--- src/python/katas/py8kyu/l1_set_alarm.py
+++ src/python/katas/py8kyu/l1_set_alarm.py
@@ -3,3 +3,10 @@
 
 def set_alarm(employed: bool, vacation: bool) -> bool:
     return employed and not vacation
+
+
+def test_set_alarm():
+    assert not set_alarm(True, True)
+    assert not set_alarm(False, True)
+    assert not set_alarm(False, False)
+    assert set_alarm(True, False)
