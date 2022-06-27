ab77b2f0c7bec6f5b8c2c32b31794ca9bbeaffb2
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:32:28 +0200

    :white_check_mark: Added test for period is late

--- src/python/katas/py8kyu/is_your_period_late.py
+++ src/python/katas/py8kyu/is_your_period_late.py
@@ -1,7 +1,20 @@
 """Kata url: https://www.codewars.com/kata/578a8a01e9fd1549e50001f1."""
 
-from datetime import datetime
+from datetime import datetime, date
 
 
 def period_is_late(last: datetime, today: datetime, cycle_length: int) -> int:
     return (today - last).days > cycle_length
+
+
+def test_period_is_late():
+    assert not period_is_late(date(2016, 6, 13), date(2016, 7, 16), 35)
+    assert period_is_late(date(2016, 6, 13), date(2016, 7, 16), 28)
+    assert not period_is_late(date(2016, 6, 13), date(2016, 7, 16), 35)
+    assert not period_is_late(date(2016, 6, 13), date(2016, 6, 29), 28)
+    assert not period_is_late(date(2016, 7, 12), date(2016, 8, 9), 28)
+    assert period_is_late(date(2016, 7, 12), date(2016, 8, 10), 28)
+    assert period_is_late(date(2016, 7, 1), date(2016, 8, 1), 30)
+    assert not period_is_late(date(2016, 6, 1), date(2016, 6, 30), 30)
+    assert not period_is_late(date(2016, 1, 1), date(2016, 1, 31), 30)
+    assert period_is_late(date(2016, 1, 1), date(2016, 2, 1), 30)
