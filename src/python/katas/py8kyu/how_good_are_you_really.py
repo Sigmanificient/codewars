9356550fc445f96c088a6fa07a907fc8fc0b34ff
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:49:32 +0200

    :white_check_mark: Added test for how good are you really

--- src/python/katas/py8kyu/how_good_are_you_really.py
+++ src/python/katas/py8kyu/how_good_are_you_really.py
@@ -5,3 +5,10 @@ from typing import List
 
 def better_than_average(class_points: List[int], your_points: int) -> bool:
     return sum(class_points) / len(class_points) < your_points
+
+
+def test_better_than_average():
+    assert better_than_average([2, 3], 5)
+    assert better_than_average([100, 40, 34, 57, 29, 72, 57, 88], 75)
+    assert better_than_average([12, 23, 34, 45, 56, 67, 78, 89, 90], 69)
+    assert not better_than_average([41, 75, 72, 56, 80, 82, 81, 33], 50)
