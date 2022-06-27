b5d99800f53682978b5932705ae9e3ea2b8c9db6
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:41:51 +0200

    :white_check_mark: Added test for divide numbers

--- src/python/katas/py8kyu/incorrect_division_method.py
+++ src/python/katas/py8kyu/incorrect_division_method.py
@@ -3,3 +3,12 @@
 
 def divide_numbers(x: int, y: int) -> float:
     return x / y
+
+
+def test_divide_numbers():
+    assert divide_numbers(4, 2) == 2
+    assert divide_numbers(10, 2) == 5
+    assert divide_numbers(9, 4) == 2.25
+    assert divide_numbers(21, 5) == 4.2
+    assert divide_numbers(9, 3) == 3
+    assert divide_numbers(1, 100) == 0.01
