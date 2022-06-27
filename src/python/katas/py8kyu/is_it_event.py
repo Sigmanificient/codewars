4b60fa1a8687c2f7bbb4e74066b66a05e16a354d
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:36:41 +0200

    :white_check_mark: Added test for is even

--- src/python/katas/py8kyu/is_it_event.py
+++ src/python/katas/py8kyu/is_it_event.py
@@ -3,3 +3,16 @@
 
 def is_even(n: int) -> int:
     return not n % 2
+
+
+def test_is_event():
+    assert is_even(0)
+    assert not is_even(0.5)
+    assert not is_even(1)
+    assert is_even(2)
+    assert is_even(-4)
+    assert not is_even(15)
+    assert is_even(20)
+    assert is_even(220)
+    assert not is_even(222222221)
+    assert is_even(500000000)
