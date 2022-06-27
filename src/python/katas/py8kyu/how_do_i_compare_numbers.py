1521d8e29a7a326afe9919d84a854ac48639d6a1
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:51:54 +0200

    :white_check_mark: Added test for how do I compare numbers

--- src/python/katas/py8kyu/how_do_i_compare_numbers.py
+++ src/python/katas/py8kyu/how_do_i_compare_numbers.py
@@ -8,3 +8,13 @@ def what_is(x: int) -> str:
         return 'everything squared'
     else:
         return 'nothing'
+
+
+def test_what_is():
+    assert what_is(0) == 'nothing'
+    assert what_is(1) == 'nothing'
+    assert what_is(123) == 'nothing'
+    assert what_is(-1) == 'nothing'
+
+    assert what_is(42) == 'everything'
+    assert what_is(42 * 42) == 'everything squared'
