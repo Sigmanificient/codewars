966c27095ae95162f21eb431ba0944274de35b0d
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:46:20 +0200

    :white_check_mark: Added test for how much water

--- src/python/katas/py8kyu/how_much_water_do_i_need.py
+++ src/python/katas/py8kyu/how_much_water_do_i_need.py
@@ -10,3 +10,10 @@ def how_much_water(water, load, clothes) -> Union[float, str]:
         return 'Too much clothes'
 
     return round(water * (1.1 ** abs(load - clothes)), 2)
+
+
+def test_how_much_water():
+    assert how_much_water(10, 10, 21) == 'Too much clothes'
+    assert how_much_water(10, 10, 2) == 'Not enough clothes'
+    assert how_much_water(10, 11, 20) == 23.58
+    assert how_much_water(50, 15, 29) == 189.87
