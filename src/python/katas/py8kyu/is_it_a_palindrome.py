61eab19e92632d4f3b6de6a2602516a7390e41e5
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:37:45 +0200

    :white_check_mark: Added test for is palindrome

--- src/python/katas/py8kyu/is_it_a_palindrome.py
+++ src/python/katas/py8kyu/is_it_a_palindrome.py
@@ -3,3 +3,13 @@
 
 def is_palindrome(s: str) -> bool:
     return s.lower() == s[::-1].lower()
+
+
+def test_is_palindrome():
+    assert is_palindrome('a')
+    assert is_palindrome('aba')
+    assert is_palindrome('Abba')
+    assert is_palindrome('malam')
+    assert is_palindrome('kodok')
+    assert not is_palindrome('walter')
+    assert not is_palindrome('Kasue')
