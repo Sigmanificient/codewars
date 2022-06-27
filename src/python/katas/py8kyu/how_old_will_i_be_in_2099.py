fbbc2d605f5c76ad8fc18c94385240096c2b1501
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:45:38 +0200

    :white_check_mark: Added test for how old will i be

--- src/python/katas/py8kyu/how_old_will_i_be_in_2099.py
+++ src/python/katas/py8kyu/how_old_will_i_be_in_2099.py
@@ -11,3 +11,16 @@ def calculate_age(year_of_birth: int, current_year: int) -> str:
         return f"You will be born in {abs(v)} year{'s' * (abs(v) > 1)}."
 
     return f"You are {v} year{'s' * (v > 1)} old."
+
+
+def test_calculate_age():
+    assert calculate_age(2012, 2016) == "You are 4 years old."
+    assert calculate_age(1989, 2016) == "You are 27 years old."
+    assert calculate_age(2000, 2090) == "You are 90 years old."
+    assert calculate_age(2000, 1990) == "You will be born in 10 years."
+    assert calculate_age(2000, 2000) == "You were born this very year!"
+    assert calculate_age(900, 2900) == "You are 2000 years old."
+    assert calculate_age(2010, 1990) == "You will be born in 20 years."
+    assert calculate_age(2010, 1500) == "You will be born in 510 years."
+    assert calculate_age(2011, 2012) == "You are 1 year old."
+    assert calculate_age(2000, 1999) == "You will be born in 1 year."
