8b9e443708b467105148778da89082af428a525f
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:28:52 +0200

    :white_check_mark: Added test for bartender drinks

--- src/python/katas/py8kyu/l1_bartender_drinks.py
+++ src/python/katas/py8kyu/l1_bartender_drinks.py
@@ -14,3 +14,14 @@ drinks: Dict[str, str] = {
 
 def get_drink_by_profession(param: str) -> str:
     return drinks.get(param.lower(), "Beer")
+
+
+def test_get_drink_by_profession():
+    assert get_drink_by_profession("jabrOni") == "Patron Tequila", "'Jabroni' should map to 'Patron Tequila'"
+    assert get_drink_by_profession("scHOOl counselor") == "Anything with Alcohol", "'School Counselor' should map to 'Anything with alcohol'"
+    assert get_drink_by_profession("prOgramMer") == "Hipster Craft Beer", "'Programmer' should map to 'Hipster Craft Beer'"
+    assert get_drink_by_profession("bike ganG member") == "Moonshine", "'Bike Gang Member' should map to 'Moonshine'"
+    assert get_drink_by_profession("poLiTiCian") == "Your tax dollars", "'Politician' should map to 'Your tax dollars'"
+    assert get_drink_by_profession("rapper") == "Cristal", "'Rapper' should map to 'Cristal'"
+    assert get_drink_by_profession("pundit") == "Beer", "'Pundit' should map to 'Beer'"
+    assert get_drink_by_profession("Pug") == "Beer", "'Pug' should map to 'Beer'"
