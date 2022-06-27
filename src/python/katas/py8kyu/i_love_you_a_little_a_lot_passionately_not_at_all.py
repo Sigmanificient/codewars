2f848808a7a2ec0118125ef1ae0ae837f3570a0c
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:44:52 +0200

    :white_check_mark: Added test for how much i love you

--- src/python/katas/py8kyu/i_love_you_a_little_a_lot_passionately_not_at_all.py
+++ src/python/katas/py8kyu/i_love_you_a_little_a_lot_passionately_not_at_all.py
@@ -5,3 +5,18 @@ def how_much_i_love_you(nb_petals: int) -> str:
     return (
         "I love you", "a little", "a lot", "passionately", "madly", "not at all"
     )[(nb_petals - 1) % 6]
+
+
+def test_how_much_i_love_you():
+    assert how_much_i_love_you(1) == "I love you"
+    assert how_much_i_love_you(2) == "a little"
+    assert how_much_i_love_you(3) == "a lot"
+    assert how_much_i_love_you(4) == "passionately"
+    assert how_much_i_love_you(5) == "madly"
+    assert how_much_i_love_you(6) == "not at all"
+    assert how_much_i_love_you(7) == "I love you"
+
+    assert how_much_i_love_you(500) == "a little"
+
+    assert how_much_i_love_you(501) == "a lot"
+    assert how_much_i_love_you(9999) == "a lot"
