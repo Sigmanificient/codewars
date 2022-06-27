bdbc73177b7575c509e28d6b4bde0fd3a9ce0ad8
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:35:42 +0200

    :white_check_mark: Added test for is upper case

--- src/python/katas/py8kyu/is_the_string_upper.py
+++ src/python/katas/py8kyu/is_the_string_upper.py
@@ -3,3 +3,11 @@
 
 def is_uppercase(inp: str) -> bool:
     return inp.isupper()
+
+
+def test_is_uppercase():
+    assert not is_uppercase("c")
+    assert "C"
+    assert not is_uppercase("hello I AM DONALD")
+    assert "HELLO I AM DONALD"
+    assert "$%&"
