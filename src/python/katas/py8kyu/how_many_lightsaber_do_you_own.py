d2b09e90edad44ff20021240440cf9726090bf5a
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:47:59 +0200

    :white_check_mark: Added test for how many light sabers

--- src/python/katas/py8kyu/how_many_lightsaber_do_you_own.py
+++ src/python/katas/py8kyu/how_many_lightsaber_do_you_own.py
@@ -3,3 +3,9 @@
 
 def how_many_light_sabers_do_you_own(name: str = '') -> int:
     return 0 if name == "Zach" else 18
+
+
+def test_how_many_light_sabers():
+    assert how_many_light_sabers_do_you_own("Zach") == 18
+    assert how_many_light_sabers_do_you_own() == 0
+    assert how_many_light_sabers_do_you_own("zach") == 0
