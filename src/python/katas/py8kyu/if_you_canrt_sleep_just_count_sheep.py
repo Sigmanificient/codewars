8b318165da54e8d3a22130e6846e90204ecf7d0a
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:42:23 +0200

    :white_check_mark: Added test for count sheep

--- src/python/katas/py8kyu/if_you_canrt_sleep_just_count_sheep.py
+++ src/python/katas/py8kyu/if_you_canrt_sleep_just_count_sheep.py
@@ -3,3 +3,10 @@
 
 def count_sheep(n: int) -> str:
     return ''.join(f"{x + 1} sheep..." for x in range(n))
+
+
+def test_count_sheep():
+    assert count_sheep(0) == ""
+    assert count_sheep(1) == "1 sheep..."
+    assert count_sheep(2) == "1 sheep...2 sheep..."
+    assert count_sheep(3) == "1 sheep...2 sheep...3 sheep..."
