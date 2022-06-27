a003cd7b2d2eae24da55855616b695bf0b135265
sigmanificient <edhyjox@gmail.com>
2022-06-27 23:41:03 +0200

    :white_check_mark: Added test for invert

--- src/python/katas/py8kyu/invert_values.py
+++ src/python/katas/py8kyu/invert_values.py
@@ -5,3 +5,9 @@ from typing import List
 
 def invert(lst: List[int]) -> List[int]:
     return [-x for x in lst]
+
+
+def test_invert():
+    assert invert([]) == []
+    assert invert([1, 2, 3, 4, 5]) == [-1, -2, -3, -4, -5]
+    assert invert([1, -2, 3, -4, 5]) == [-1, 2, -3, 4, -5]
