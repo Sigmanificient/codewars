6fa1a8836e3b353f7dc8504aa68c7f999a57bf25
sigmanificient <edhyjox@gmail.com>
2022-06-28 23:57:49 +0200

    :white_check_mark: Added test for distance between points

--- src/python/katas/py8kyu/geometry_basics_distance_between_points_in_2d.py
+++ src/python/katas/py8kyu/geometry_basics_distance_between_points_in_2d.py
@@ -1,7 +1,6 @@
 """Kata url: https://www.codewars.com/kata/58dced7b702b805b200000be."""
 from dataclasses import dataclass
 from math import sqrt
-from typing import Tuple
 
 
 @dataclass
@@ -12,3 +11,10 @@ class Point:
 
 def distance_between_points(a: Point, b: Point) -> float:
     return sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2)
+
+
+def test_distance_between_points():
+    assert distance_between_points(Point(3, 3), Point(3, 3)) == 0
+    assert distance_between_points(Point(1, 6), Point(4, 2)) == 5
+    assert round(distance_between_points(Point(-10.2, 12.5), Point(0.3, 14.7)), 6) == 10.728001
+    assert round(distance_between_points(Point(0, 0), Point(0, 0)), 6) == 0
