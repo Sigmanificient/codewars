858fb23ab2eab830d9efdeae51c8652de0f9cb94
sigmanificient <edhyjox@gmail.com>
2022-06-28 23:35:30 +0200

    :sparkles: Added solution for group in 10s

---
+++ src/python/katas/py5kyu/group_in_tens.py
@@ -0,0 +1,34 @@
+"""Kata url: https://www.codewars.com/kata/5694d22eb15d78fe8d00003a."""
+
+from collections import defaultdict
+from typing import Optional, List
+
+
+def group_in_10s(*args: int) -> List[Optional[int]]:
+    if not args:
+        return []
+
+    groups = defaultdict(list)
+
+    for a in sorted(args):
+        groups[a // 10].append(a)
+
+    return [
+        groups.get(x)
+        for x in range(0, (max(args) // 10) + 1)
+    ]
+
+
+def test_group_in_10s():
+    assert group_in_10s() == []
+    assert group_in_10s(1, 2, 3) == [[1, 2, 3]]
+    assert group_in_10s(100) == [None] * 10 + [[100]]
+
+    assert group_in_10s(8, 12, 38, 3, 17, 19, 25, 35, 50) == [
+        [3, 8], [12, 17, 19], [25], [35, 38], None, [50]
+    ]
+
+    assert group_in_10s(12, 10, 11, 13, 25, 28, 29, 49, 51, 90) == [
+        None, [10, 11, 12, 13], [25, 28, 29], None,
+        [49], [51], None, None, None, [90]
+    ]
