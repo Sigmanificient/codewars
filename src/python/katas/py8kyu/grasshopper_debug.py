766664a173c73e52ad98966c5f5a04a4b36051ac
sigmanificient <edhyjox@gmail.com>
2022-06-28 23:49:25 +0200

    :white_check_mark: Added test for weather info

--- src/python/katas/py8kyu/grasshopper_debug.py
+++ src/python/katas/py8kyu/grasshopper_debug.py
@@ -8,3 +8,18 @@ def weather_info(temp: float) -> str:
 
 def convert_to_celsius(temperature: float) -> float:
     return (temperature - 32) * (5 / 9)
+
+
+def test_weather_info():
+    assert weather_info(50) == '10.0 is above freezing temperature'
+    assert weather_info(23) == '-5.0 is freezing temperature'
+
+
+def test_convert_to_celsius():
+    assert convert_to_celsius(32) == 0
+    assert convert_to_celsius(68) == 20
+
+    assert round(convert_to_celsius(100), 3) == 37.778
+    assert round(convert_to_celsius(0), 3) == -17.778
+    assert round(convert_to_celsius(20), 3) == -6.667
+    assert round(convert_to_celsius(30), 3) == -1.111
