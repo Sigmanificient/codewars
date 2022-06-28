42c6c8ff36e7486de27a11ef542f39ed450e8607
sigmanificient <edhyjox@gmail.com>
2022-06-28 23:37:16 +0200

    :white_check_mark: Added test for hello name | world

--- src/python/katas/py8kyu/hello_name_or_world.py
+++ src/python/katas/py8kyu/hello_name_or_world.py
@@ -3,3 +3,9 @@
 
 def hello(name: str = '') -> str:
     return f"Hello, {name.capitalize() if name else 'World'}!"
+
+
+def test_hello():
+    assert hello("John") == "Hello, John!"
+    assert hello("aLIce") == "Hello, Alice!"
+    assert hello("") == "Hello, World!"
