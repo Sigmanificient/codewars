065fe5ce129bca5592f4be7352dc9cca507bd106
sigmanificient <edhyjox@gmail.com>
2022-06-28 23:40:17 +0200

    :white_check_mark: Added test for messi goals func

--- src/python/katas/py8kyu/grasshoppper_messi_goals_function.py
+++ src/python/katas/py8kyu/grasshoppper_messi_goals_function.py
@@ -3,3 +3,13 @@
 
 def goals(la_liga: int, copa_del_rey: int, champions_league: int) -> int:
     return la_liga + copa_del_rey + champions_league
+
+
+def test_goals():
+    assert goals(0, 0, 0) == 0
+    assert goals(5, 10, 2) == 17
+    assert goals(1, 2, 3) == 6
+
+    assert goals(10, 0, 0) == 10
+    assert goals(0, 10, 0) == 10
+    assert goals(0, 0, 10) == 10
