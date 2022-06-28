abe958d37ccce38efd0c606d0c8e8ab682d0396b
sigmanificient <edhyjox@gmail.com>
2022-06-28 23:50:59 +0200

    :white_check_mark: Added test for combine strings

--- src/python/katas/py8kyu/grasshopper_combine_strings.py
+++ src/python/katas/py8kyu/grasshopper_combine_strings.py
@@ -2,3 +2,9 @@
 
 def combine_names(a: str, b: str) -> str:
     return f'{a} {b}'
+
+
+def test_combine_names():
+    assert combine_names("James", "Stevens") == "James Stevens"
+    assert combine_names("Davy", "Back") == "Davy Back"
+    assert combine_names("Arthur", "Dent") == "Arthur Dent"
