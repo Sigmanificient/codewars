8352f22c7b1f54c5d8376860e1aea12339e3354a
sigmanificient <edhyjox@gmail.com>
2022-06-28 23:43:11 +0200

    :white_check_mark: Added test for combat

--- src/python/katas/py8kyu/grasshopper_terminal_combat_function.py
+++ src/python/katas/py8kyu/grasshopper_terminal_combat_function.py
@@ -3,3 +3,13 @@
 
 def combat(health, damage):
     return max(0, health - damage)
+
+
+def test_combat():
+    assert combat(100, 5) == 95
+    assert combat(83, 16) == 67
+    assert combat(50, 10) == 40
+
+    assert combat(20, 30) == 0
+    assert combat(10, 10) == 0
+    assert combat(0, 10) == 0
