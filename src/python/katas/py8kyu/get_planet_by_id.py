0d408de7f8138a0052b23bbbbf672ddfedb4409a
sigmanificient <edhyjox@gmail.com>
2022-06-28 23:55:26 +0200

    :white_check_mark: Added test for get planet by id

--- src/python/katas/py8kyu/get_planet_by_id.py
+++ src/python/katas/py8kyu/get_planet_by_id.py
@@ -16,3 +16,12 @@ planets: Tuple[str, ...] = (
 
 def get_planet_name(_id: int) -> str:
     return planets[_id - 1]
+
+
+def test_get_planet_name():
+    assert get_planet_name(2) == 'Venus'
+    assert get_planet_name(5) == 'Jupiter'
+    assert get_planet_name(3) == 'Earth'
+    assert get_planet_name(4) == 'Mars'
+    assert get_planet_name(8) == 'Neptune'
+    assert get_planet_name(1) == 'Mercury'
