d10e20d525c8c134db2d7e0a7f8286cf93a4e115
sigmanificient <edhyjox@gmail.com>
2022-06-28 23:47:15 +0200

    :white_check_mark: Added test for say hello

--- src/python/katas/py8kyu/grasshopper_debug_say_hello.py
+++ src/python/katas/py8kyu/grasshopper_debug_say_hello.py
@@ -3,3 +3,11 @@
 
 def say_hello(name: str) -> str:
     return f"Hello, {name}"
+
+
+def test_say_hello():
+    assert say_hello('Mr. Spock') == 'Hello, Mr. Spock'
+    assert say_hello('Captain Kirk') == 'Hello, Captain Kirk'
+    assert say_hello('Liutenant Uhura') == 'Hello, Liutenant Uhura'
+    assert say_hello('Dr. McCoy') == 'Hello, Dr. McCoy'
+    assert say_hello('Mr. Scott') == 'Hello, Mr. Scott'
