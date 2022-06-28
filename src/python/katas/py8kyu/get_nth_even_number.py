a9773768a605b37ebbbec6f757bbf2addf540e20
sigmanificient <edhyjox@gmail.com>
2022-06-28 23:55:54 +0200

    :white_check_mark: Added test for get nth even number

--- src/python/katas/py8kyu/get_nth_even_number.py
+++ src/python/katas/py8kyu/get_nth_even_number.py
@@ -3,3 +3,11 @@
 
 def nth_even(n: int) -> int:
     return (n - 1) * 2
+
+
+def test_nth_even():
+    assert nth_even(1) == 0
+    assert nth_even(2) == 2
+    assert nth_even(3) == 4
+    assert nth_even(100) == 198
+    assert nth_even(1298734) == 2597466
