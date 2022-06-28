d884188b1dc9e3ed17f911fff377de33240e715a
sigmanificient <edhyjox@gmail.com>
2022-06-29 01:57:59 +0200

    :white_check_mark: Added test for multiply

--- src/python/katas/py8kyu/function_3_multiplying_two_numbers.py
+++ src/python/katas/py8kyu/function_3_multiplying_two_numbers.py
@@ -3,3 +3,10 @@
 
 def multiply(a: int, b: int) -> int:
     return a * b
+
+
+def test_multiply():
+    assert multiply(1, 2) == 2
+    assert multiply(1, -2) == -2
+    assert multiply(3, 3) == 9
+    assert multiply(3, -3) == -9
