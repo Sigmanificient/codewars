c36d65083647e2cacbdee6152593c1e9eff47235
sigmanificient <edhyjox@gmail.com>
2022-06-28 23:52:01 +0200

    :white_check_mark: Added test for check for factor

--- src/python/katas/py8kyu/grasshopper_check_for_factor.py
+++ src/python/katas/py8kyu/grasshopper_check_for_factor.py
@@ -3,3 +3,15 @@
 
 def check_for_factor(base: int, factor: int) -> bool:
     return not base % factor
+
+
+def test_check_for_factor():
+    assert check_for_factor(10, 2)
+    assert check_for_factor(63, 7)
+    assert check_for_factor(2450, 5)
+    assert check_for_factor(24612, 3)
+
+    assert not check_for_factor(9, 2)
+    assert not check_for_factor(653, 7)
+    assert not check_for_factor(2453, 5)
+    assert not check_for_factor(24617, 3)
