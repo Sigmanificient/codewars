1baf0c46d03299c14fd6f5d07c03381f29de6aea
sigmanificient <edhyjox@gmail.com>
2022-06-28 23:58:36 +0200

    :white_check_mark: Added test for generate range

--- src/python/katas/py8kyu/generate_range_of_integers.py
+++ src/python/katas/py8kyu/generate_range_of_integers.py
@@ -5,3 +5,11 @@ from typing import List
 
 def generate_range(_min: int, _max: int, step: int) -> List[int]:
     return list(range(_min, _max + 1, step))
+
+
+def test_generate_range():
+    assert generate_range(1, 10, 1) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert generate_range(-10, 1, 1) == [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1]
+    assert generate_range(1, 15, 20) == [1]
+    assert generate_range(1, 7, 2) == [1, 3, 5, 7]
+    assert generate_range(0, 20, 3) == [0, 3, 6, 9, 12, 15, 18]
