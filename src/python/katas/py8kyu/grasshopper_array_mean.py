8071e4642e74ca8e06df17d878687f4b02b92b9f
sigmanificient <edhyjox@gmail.com>
2022-06-28 23:53:03 +0200

    :white_check_mark: Added test for array mean

--- src/python/katas/py8kyu/grasshopper_array_mean.py
+++ src/python/katas/py8kyu/grasshopper_array_mean.py
@@ -5,3 +5,11 @@ from typing import List
 
 def find_average(nums: List[int]) -> float:
     return (sum(nums) / len(nums)) if nums else 0
+
+
+def test_find_average():
+    assert find_average([1]) == 1
+    assert find_average([1, 3, 5, 7]) == 4
+    assert find_average([-1, 3, 5, -7]) == 0
+    assert find_average([5, 7, 3, 7]) == 5.5
+    assert find_average([]) == 0
