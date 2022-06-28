9358b5062e0c7f1cf456c514dc36437eff244f99
sigmanificient <edhyjox@gmail.com>
2022-06-28 23:41:05 +0200

    :white_check_mark: Added test for syntax debugging

--- src/python/katas/py8kyu/Grasshoppper_function_syntax_debugging.py
+++
@@ -1,5 +0,0 @@
-"""Kata url: https://www.codewars.com/kata/56dae9dc54c0acd29d00109a."""
-
-
-def main(verb: str, noun: str) -> str:
-    return verb + noun
---
+++ src/python/katas/py8kyu/grasshoppper_function_syntax_debugging.py
@@ -0,0 +1,13 @@
+"""Kata url: https://www.codewars.com/kata/56dae9dc54c0acd29d00109a."""
+
+
+def main(verb: str, noun: str) -> str:
+    return verb + noun
+
+
+def test_main():
+    assert main('take ', 'item') == 'take item'
+    assert main('use ', 'sword') == 'use sword'
+    assert main('drop ', 'item') == 'drop item'
+    assert main('go ', 'north') == 'go north'
+    assert main('go ', 'south') == 'go south'
