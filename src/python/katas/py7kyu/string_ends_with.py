d616697a03bd352685bc030e98ea83de9a8941f7
sigmanificient <edhyjox@gmail.com>
2022-06-29 21:48:22 +0200

    :sparkles: Added solution string ends with (7kyu)

---
+++ src/python/katas/py7kyu/string_ends_with.py
@@ -0,0 +1,15 @@
+"""Kata url: https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d"""
+
+
+def solution(string: str, ending: str) -> bool:
+    return string.endswith(ending)
+
+
+def test_solution():
+    assert solution('abcde', 'cde')
+    assert not solution('abcde', 'abc')
+    assert solution('abcde', '')
+
+    assert solution('abcde', 'cde')
+    assert not solution('abcde', 'abc')
+    assert solution('abcde', '')
