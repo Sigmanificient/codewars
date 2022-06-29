e10cf3eebf43f4d139a546ea27e6d970c6df8b51
sigmanificient <edhyjox@gmail.com>
2022-06-29 21:50:11 +0200

    :white_check_mark: Added test for square

--- src/python/katas/py8kyu/function_2_squaring_an_argument.py
+++ src/python/katas/py8kyu/function_2_squaring_an_argument.py
@@ -3,3 +3,10 @@
 
 def square(n: int) -> int:
     return n ** 2
+
+
+def test_square():
+    assert square(0) == 0
+    assert square(1) == 1
+    assert square(2) == 4
+    assert square(50) == 2500
