2e3997a2fe84afb2c6e3cdfb1dc7d8530d199792
sigmanificient <edhyjox@gmail.com>
2022-06-29 21:51:31 +0200

    :white_check_mark: Added test for two dec places

--- src/python/katas/py8kyu/formatting_decimal_places_#0.py
+++
@@ -1,4 +0,0 @@
-"""Kata url: https://www.codewars.com/kata/5641a03210e973055a00000d."""
-
-def two_decimal_places(n: float) -> float:
-    return round(n, 2)
---
+++ src/python/katas/py8kyu/formatting_decimal_places_0.py
@@ -0,0 +1,11 @@
+"""Kata url: https://www.codewars.com/kata/5641a03210e973055a00000d."""
+
+
+def two_decimal_places(n: float) -> float:
+    return round(n, 2)
+
+
+def test_two_decimal_places():
+    assert two_decimal_places(4.659725356) == 4.66
+    assert two_decimal_places(173735326.3783732637948948) == 173735326.38
+    assert two_decimal_places(4.653725356) == 4.65
