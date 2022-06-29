9b24426951284a71283fab736bf63ac6647c2192
sigmanificient <edhyjox@gmail.com>
2022-06-29 22:18:47 +0200

    :sparkles: Added solution for give me a diamond

---
+++ src/python/katas/py6kyu/give_me_a_diamond.py
@@ -0,0 +1,15 @@
+def diamond(n: int) -> str:
+    if not n % 2 or n < 0:
+        return None
+
+    half = [' ' * ((n - i) // 2) + '*' * i for i in range(1, n + 1, 2)]
+    return '\n'.join(half[:-1] + half[::-1]) + '\n'
+
+
+def test_diamond():
+    assert diamond(1) == "*\n"
+    assert diamond(2) is None
+    assert diamond(3) == " *\n***\n *\n"
+    assert diamond(5) == "  *\n ***\n*****\n ***\n  *\n"
+    assert diamond(0) is None
+    assert diamond(-3) is None
