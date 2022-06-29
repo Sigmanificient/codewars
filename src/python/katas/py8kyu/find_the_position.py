78ff0338a4049f6b95194640902fe26fef3dddc3
sigmanificient <edhyjox@gmail.com>
2022-06-29 22:05:03 +0200

    :white_check_mark: Added test for find the position

--- src/python/katas/py8kyu/find_the_position.py
+++ src/python/katas/py8kyu/find_the_position.py
@@ -3,3 +3,9 @@
 
 def position(alphabet: str) -> str:
     return f'Position of alphabet: {"abcdefghijklmnopqrstuvwxyz".index(alphabet) + 1}'
+
+
+def test_position():
+    assert position("a") == "Position of alphabet: 1"
+    assert position("z") == "Position of alphabet: 26"
+    assert position("e") == "Position of alphabet: 5"
