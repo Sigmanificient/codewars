d95c327455b396760294e43a3451009be16ec84f
sigmanificient <edhyjox@gmail.com>
2022-06-29 21:57:45 +0200

    :white_check_mark: Added test for 5 without numbers

--- src/python/katas/py8kyu/five_without_numbers.py
+++ src/python/katas/py8kyu/five_without_numbers.py
@@ -1,4 +1,9 @@
 """Kata url: https://www.codewars.com/kata/59441520102eaa25260000bf."""
 
+
 def unusual_five() -> int:
     return sum([True, True, True, True, True])
+
+
+def test_unusual_five():
+    assert unusual_five() == 5
