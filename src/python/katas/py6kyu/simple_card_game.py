ca52d85e4802ffa38b5d8d82915e929807d1c0ce
sigmanificient <edhyjox@gmail.com>
2022-06-29 23:02:37 +0200

    :sparkles: Added solution for simple card game

---
+++ src/python/katas/py6kyu/simple_card_game.py
@@ -0,0 +1,36 @@
+from typing import List
+
+CARDS = "23456789TJQKA"
+
+
+def winner(deck_steve: List[str], deck_josh: List[str]) -> str:
+    j = s = 0
+
+    for a, b in zip(deck_steve, deck_josh):
+        p = CARDS.index(a) - CARDS.index(b)
+
+        j += p < 0
+        s += p > 0
+
+    if j == s:
+        return 'Tie'
+
+    if s > j:
+        return f"Steve wins {s} to {j}"
+
+    return f"Josh wins {j} to {s}"
+
+
+def test_winner():
+    assert winner(
+        ['2', '3', '4', '5', '6'], ['7', '8', '9', 'T', 'J']
+    ) == "Josh wins 5 to 0"
+
+    assert winner(
+        ['7', '4', '3', 'T', 'J', 'Q'], ['8', '9', '2', '5', '6', '2']
+    ) == "Steve wins 4 to 2"
+
+    assert winner(
+        ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'],
+        ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'][::-1]
+    ) == "Tie"
