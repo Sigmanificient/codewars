ad8001810dfcf70f7ee691e6e271fca32f4ca00e
sigmanificient <edhyjox@gmail.com>
2022-06-29 21:53:28 +0200

    :white_check_mark: Added test for replace dots

--- src/python/katas/py8kyu/fixme_replace_all_dots.py
+++ src/python/katas/py8kyu/fixme_replace_all_dots.py
@@ -1,4 +1,12 @@
 """Kata url: https://www.codewars.com/kata/596c6eb85b0f515834000049."""
 
+
 def replace_dots(string: str) -> str:
     return string.replace('.', '-')
+
+
+def test_replace_dots():
+    assert replace_dots("") == ""
+    assert replace_dots("no dots") == "no dots"
+    assert replace_dots("one.two.three") == "one-two-three"
+    assert replace_dots("........") == "--------"
