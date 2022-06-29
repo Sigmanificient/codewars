4bf4615057f9147b58cd5e6f4a42488cdd2adffb
sigmanificient <edhyjox@gmail.com>
2022-06-29 17:32:43 +0200

    :sparkles: Added solution of categorize member (7kyu)

---
+++ src/python/katas/py7kyu/categorize_member.py
@@ -0,0 +1,15 @@
+def open_or_senior(data):
+    return [
+        'Senior' if p[0] >= 55 and p[1] > 7 else 'Open'
+        for p in data
+    ]
+
+
+def test_open_or_senior():
+    assert open_or_senior(
+        [(45, 12), (55, 21), (19, -2), (104, 20)]
+    ) == ['Open', 'Senior', 'Open', 'Senior']
+
+    assert open_or_senior(
+        [(16, 23), (73, 1), (56, 20), (1, -1)]
+    ) == ['Open', 'Open', 'Senior', 'Open']
