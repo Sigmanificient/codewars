94e001227cbe4f7be745b777458d2c3a756975a6
sigmanificient <edhyjox@gmail.com>
2022-06-29 21:38:53 +0200

    :sparkles: Added solution for sort array by string length

---
+++ src/python/katas/py7kyu/sort_array_by_string_length.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def sort_by_length(arr: List[str]) -> List[str]:
+    return sorted(arr, key=len)
+
+
+def test_sort_by_length():
+    assert sort_by_length(["beg", "life", "i", "to"]) == ["i", "to", "beg", "life"]
+    assert sort_by_length(["", "moderately", "brains", "pizza"]) == ["", "pizza", "brains", "moderately"]
+    assert sort_by_length(["longer", "longest", "short"]) == ["short", "longer", "longest"]
+    assert sort_by_length(["dog", "food", "a", "of"]) == ["a", "of", "dog", "food"]
+    assert sort_by_length(["", "dictionary", "eloquent", "bees"]) == ["", "bees", "eloquent", "dictionary"]
+    assert sort_by_length(
+        ["a longer sentence", "the longest sentence", "a short sentence"]
+    ) == ["a short sentence", "a longer sentence", "the longest sentence"]
