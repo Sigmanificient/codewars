2cde8599a55476ad0fbd9607d6924131a971c603
sigmanificient <edhyjox@gmail.com>
2022-06-29 21:52:17 +0200

    :white_check_mark: Added test for quote

--- src/python/katas/py8kyu/for_ufc_fans_total_beginners_conor_mcgregor_vs_george_saint_pierre.py
+++ src/python/katas/py8kyu/for_ufc_fans_total_beginners_conor_mcgregor_vs_george_saint_pierre.py
@@ -1,4 +1,5 @@
-# Kata url: https://www.codewars.com/kata/582dafb611d576b745000b74
+"""Kata url: https://www.codewars.com/kata/582dafb611d576b745000b74."""
+
 
 def quote(fighter: str) -> str:
     return (
@@ -6,3 +7,11 @@ def quote(fighter: str) -> str:
         if fighter.lower() == "george saint pierre" else
         "I'd like to take this chance to apologize.. To absolutely NOBODY!"
     )
+
+
+def test_quote():
+    assert quote('george saint pierre') == "I am not impressed by your performance."
+    assert quote('conor mcgregor') == "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+
+    assert quote('George Saint Pierre') == "I am not impressed by your performance."
+    assert quote('Conor McGregor') == "I'd like to take this chance to apologize.. To absolutely NOBODY!"
