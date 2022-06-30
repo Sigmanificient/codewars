103b6a92601376aaf70ed92e60724ecb2f884593
sigmanificient <edhyjox@gmail.com>
2022-06-30 11:12:20 +0200

    :white_check_mark: Added test for even or odd

--- src/python/katas/py8kyu/even_or_odd.py
+++ src/python/katas/py8kyu/even_or_odd.py
@@ -1,4 +1,19 @@
 """Kata url: https://www.codewars.com/kata/53da3dbb4a5168369a0000fe."""
 
+
 def even_or_odd(number: int) -> str:
     return 'Odd' if number % 2 else 'Even'
+
+
+def test_even_or_odd():
+    assert even_or_odd(2) == "Even"
+    assert even_or_odd(1) == "Odd"
+    assert even_or_odd(0) == "Even"
+    assert even_or_odd(1545452) == "Even"
+    assert even_or_odd(7) == "Odd"
+    assert even_or_odd(78) == "Even"
+    assert even_or_odd(17) == "Odd"
+    assert even_or_odd(74156741) == "Odd"
+    assert even_or_odd(100000) == "Even"
+    assert even_or_odd(-123) == "Odd"
+    assert even_or_odd(-456) == "Even"
