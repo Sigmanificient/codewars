dea87c4bd1d33ce63962b84e064a1d31b6aa3700
sigmanificient <edhyjox@gmail.com>
2022-06-30 10:45:42 +0200

    :white_check_mark: Added test for divisible by

--- src/python/katas/py8kyu/find_numbers_which_are_divisible_by_given_number.py
+++ src/python/katas/py8kyu/find_numbers_which_are_divisible_by_given_number.py
@@ -5,3 +5,12 @@ from typing import List
 
 def divisible_by(numbers: List[int], divisor: int) -> List[int]:
     return [n for n in numbers if (n / divisor).is_integer()]
+
+
+def test_divisible_by():
+    assert divisible_by([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]
+    assert divisible_by([1, 2, 3, 4, 5, 6], 3) == [3, 6]
+    assert divisible_by([0, 1, 2, 3, 4, 5, 6], 4) == [0, 4]
+    assert divisible_by([0], 4) == [0]
+    assert divisible_by([1, 3, 5], 2) == []
+    assert divisible_by([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
