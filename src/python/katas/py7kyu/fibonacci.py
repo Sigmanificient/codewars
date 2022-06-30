8fe3525a96a6bb58c8976aa10256ea64679c14d8
sigmanificient <edhyjox@gmail.com>
2022-07-01 00:00:01 +0200

    :sparkles: Added solution for 'fib' (7kyu)

--- src/python/katas/py7kyu/fibonacci.py
+++ src/python/katas/py7kyu/fibonacci.py
@@ -4,3 +4,15 @@ def fibonacci(n: int) -> int:
         a, b = b, a + b
 
     return a
+
+
+def test_fib():
+    assert fibonacci(0) == 0
+    assert fibonacci(1) == 1
+    assert fibonacci(2) == 1
+    assert fibonacci(3) == 2
+    assert fibonacci(4) == 3
+    assert fibonacci(5) == 5
+    assert fibonacci(6) == 8
+    assert fibonacci(34) == 5702887
+    assert fibonacci(299) == 137347080577163115432025771710279131845700275212767467264610201
