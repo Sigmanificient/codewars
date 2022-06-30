4135e31c598a21ec7ba28780852f5adc7b6f1b05
sigmanificient <edhyjox@gmail.com>
2022-06-30 10:55:10 +0200

    :white_check_mark: Added test for find multiples

--- src/python/katas/py8kyu/find_multiples_of_a_number.py
+++ src/python/katas/py8kyu/find_multiples_of_a_number.py
@@ -4,3 +4,10 @@ from typing import List
 
 def find_multiples(integer: int, limit: int) -> List[int]:
     return [integer + integer * c for c in range(limit // integer)]
+
+
+def test_find_multiples():
+    assert find_multiples(1, 0) == []
+    assert find_multiples(1, 1) == [1]
+    assert find_multiples(5, 25) == [5, 10, 15, 20, 25]
+    assert find_multiples(1, 2) == [1, 2]
