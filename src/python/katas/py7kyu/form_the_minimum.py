7b6d3d4acfca8bab2a306a4b16b4f36caaaf4bb3
sigmanificient <edhyjox@gmail.com>
2022-06-30 08:29:53 +0200

    :white_check_mark: Added tests for last 2 katas

--- src/python/katas/py7kyu/form_the_minimum.py
+++ src/python/katas/py7kyu/form_the_minimum.py
@@ -4,3 +4,14 @@ from typing import List
 
 def min_value(digits: List[int]) -> int:
     return int(''.join(map(str, sorted(set(digits)))))
+
+
+def test_min_value():
+    assert min_value([1, 3, 1]) == 13
+    assert min_value([4, 7, 5, 7]) == 457
+    assert min_value([4, 8, 1, 4]) == 148
+
+    assert min_value([1] * 10 + [2]) == 12
+    assert min_value([9] * 50) == 9
+
+    assert min_value(list(range(10))) == 123456789
--- src/python/katas/py7kyu/sum_of_all_the_multiples_of_3_and_5.py
+++ src/python/katas/py7kyu/sum_of_all_the_multiples_of_3_and_5.py
@@ -3,3 +3,11 @@
 
 def find(n: int) -> int:
     return sum(k for k in range(n + 1) if not (k % 3 and k % 5))
+
+
+def test_find():
+    assert find(1) == 0
+    assert find(3) == 3
+    assert find(5) == 8
+    assert find(10) == 33
+    assert find(1000) == 234168
