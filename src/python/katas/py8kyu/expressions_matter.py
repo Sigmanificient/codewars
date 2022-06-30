5cfcfe7c41f3e923bc290ad71880e6a849d48aea
sigmanificient <edhyjox@gmail.com>
2022-06-30 11:02:32 +0200

    :white_check_mark: Added test for expressions matters

--- src/python/katas/py8kyu/expressions_matter.py
+++ src/python/katas/py8kyu/expressions_matter.py
@@ -8,3 +8,27 @@ def expression_matter(a: int, b: int, c: int) -> int:
         a + b + c,
         a * b * c
     )
+
+
+def test_expression_matter():
+    assert expression_matter(2, 1, 2) == 6
+    assert expression_matter(2, 1, 1) == 4
+    assert expression_matter(2, 2, 4) == 16
+    assert expression_matter(3, 3, 3) == 27
+    assert expression_matter(1, 1, 1) == 3
+    assert expression_matter(1, 2, 3) == 9
+    assert expression_matter(1, 3, 1) == 5
+    assert expression_matter(2, 2, 2) == 8
+    assert expression_matter(5, 1, 3) == 20
+    assert expression_matter(3, 5, 7) == 105
+    assert expression_matter(5, 6, 1) == 35
+    assert expression_matter(1, 6, 1) == 8
+    assert expression_matter(2, 6, 1) == 14
+    assert expression_matter(6, 7, 1) == 48
+    assert expression_matter(2, 10, 3) == 60
+    assert expression_matter(1, 8, 3) == 27
+    assert expression_matter(9, 7, 2) == 126
+    assert expression_matter(1, 1, 10) == 20
+    assert expression_matter(9, 1, 1) == 18
+    assert expression_matter(10, 5, 6) == 300
+    assert expression_matter(1, 10, 1) == 12
