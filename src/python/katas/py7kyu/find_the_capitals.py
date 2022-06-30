451da78af807a79cc9d613060aa737169a1c9a4a
sigmanificient <edhyjox@gmail.com>
2022-06-30 23:53:13 +0200

    :sparkles: Added solution for 'find the capitals'' (7kyu)

---
+++ src/python/katas/py7kyu/find_the_capitals.py
@@ -0,0 +1,13 @@
+"""Kata url: https://www.codewars.com/kata/539ee3b6757843632d00026b."""
+
+from typing import List
+
+
+def capitals(word: str) -> List[int]:
+    return [c for c, x in enumerate(word) if x.isupper()]
+
+
+def test_capitals():
+    assert capitals('codewars') == []
+    assert capitals('CodEWaRs') == [0, 3, 4, 6]
+    assert capitals('Hello, World') == [0, 7]
