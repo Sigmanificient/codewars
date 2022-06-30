c8c4ea8aa3bf0e9abc92e494c0663ad5f1791201
sigmanificient <edhyjox@gmail.com>
2022-06-30 08:36:51 +0200

    :sparkles: Added solution for title case (6kyu)

---
+++ src/python/katas/py6kyu/title_case.py
@@ -0,0 +1,13 @@
+def title_case(title: str, minor_words: str = '') -> str:
+    minor_wl = tuple(map(str.lower, minor_words.split(' ')))
+    return ' '.join(
+        word.lower() if (c and word.lower() in minor_wl) else word.capitalize()
+        for c, word in enumerate(title.split(' '))
+    )
+
+
+def test_title_case():
+    assert title_case('') == ''
+    assert title_case('a clash of KINGS', 'a an the of') == 'A Clash of Kings'
+    assert title_case('THE WIND IN THE WILLOWS', 'The In') == 'The Wind in the Willows'
+    assert title_case('the quick brown fox') == 'The Quick Brown Fox'
