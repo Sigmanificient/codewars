e04f4fe1dbf73df11a1865ce87e57cce3fa0f0e2
sigmanificient <edhyjox@gmail.com>
2022-06-30 09:52:57 +0200

    :sparkles: Added solution for 'find the unique string' (5kyu)

---
+++ src/python/katas/py5kyu/find_the_unique_string.py
@@ -0,0 +1,19 @@
+"""Kata url: https://www.codewars.com/kata/585d8c8a28bc7403ea0000c3."""
+from typing import List
+
+
+def find_uniq(arr: List[str]) -> str:
+    k, hash_word = {}, []
+
+    for word in arr:
+        s = ''.join(sorted(set(word.lower())))
+        hash_word.append(s)
+        k[s] = word
+
+    return k[min(k.keys(), key=hash_word[:3].count)]
+
+
+def test_find_uniq():
+    assert find_uniq(['Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a']) == 'BbBb'
+    assert find_uniq(['abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba']) == 'foo'
+    assert find_uniq(['    ', 'a', '  ']) == 'a'
