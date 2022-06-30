41e6dc9ee43c0908b3076ee1c70ce784dbc3cec9
sigmanificient <edhyjox@gmail.com>
2022-06-30 10:50:03 +0200

    :white_check_mark: Added test for nearest square

--- src/python/katas/py8kyu/find_nearest_square_number.py
+++ src/python/katas/py8kyu/find_nearest_square_number.py
@@ -5,3 +5,11 @@ from math import sqrt
 
 def nearest_sq(n: int) -> int:
     return round(sqrt(n)) ** 2
+
+
+def test_nearest_sq():
+    assert nearest_sq(1) == 1
+    assert nearest_sq(2) == 1
+    assert nearest_sq(10) == 9
+    assert nearest_sq(111) == 121
+    assert nearest_sq(9999) == 10000
