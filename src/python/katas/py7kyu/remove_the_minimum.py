1ff87eedc47b567ff0913f2fcd6db3707e3f25e8
sigmanificient <edhyjox@gmail.com>
2022-06-30 23:58:18 +0200

    :sparkles: Added solution for 'remove the smallest' (7kyu)

---
+++ src/python/katas/py7kyu/remove_the_minimum.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def remove_smallest(numbers: List[int]) -> List[int]:
+    if not numbers:
+        return []
+
+    out = numbers[:]
+    out.remove(min(numbers))
+    return out
+
+
+def test_remove_smallest():
+    assert remove_smallest([1, 2, 3, 4, 5]) == [2, 3, 4, 5]
+    assert remove_smallest([5, 3, 2, 1, 4]) == [5, 3, 2, 4]
+    assert remove_smallest([1, 2, 3, 1, 1]) == [2, 3, 1, 1]
+    assert remove_smallest([1]) == []
+    assert remove_smallest([]) == []
