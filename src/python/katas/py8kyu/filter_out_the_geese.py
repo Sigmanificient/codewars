d1e878a537f53cdffe54b6e356e73e54770b642c
sigmanificient <edhyjox@gmail.com>
2022-06-30 10:57:41 +0200

    :white_check_mark: Added test for filtering

--- src/python/katas/py8kyu/filter_out_the_geese.py
+++ src/python/katas/py8kyu/filter_out_the_geese.py
@@ -9,3 +9,9 @@ geese: List[str] = [
 
 def goose_filter(birds: List[str]) -> List[str]:
     return [bird for bird in birds if bird not in geese]
+
+
+def test_goose_filter():
+    assert goose_filter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]) == ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+    assert goose_filter(["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]) == ["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]
+    assert goose_filter(["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]) == []
--- src/python/katas/py8kyu/filtering_even_numbers.py
+++ src/python/katas/py8kyu/filtering_even_numbers.py
@@ -5,3 +5,8 @@ from typing import List
 
 def kata_13_december(lst: List[int]) -> List[int]:
     return [x for x in lst if x % 2]
+
+
+def test_kata_13_december():
+    assert kata_13_december([1, 2, 2, 2, 4, 3, 4, 5, 6, 7]) == [1, 3, 5, 7]
+    assert kata_13_december([1, 2, 2, 2, 4, 3, 4]) == [1, 3]
