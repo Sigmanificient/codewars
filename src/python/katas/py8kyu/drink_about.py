1c78e72e1462717ee9bcf0513e66f3f78c1b9d31
sigmanificient <edhyjox@gmail.com>
2022-06-30 11:17:39 +0200

    :white_check_mark: Added test for drink about

--- src/python/katas/py8kyu/drink_about.py
+++ src/python/katas/py8kyu/drink_about.py
@@ -17,3 +17,18 @@ def people_with_age_drink(age: int) -> str:
             return f"drink {drink}"
 
     return "drink toddy"
+
+
+def test_people_with_age_drink():
+    assert people_with_age_drink(13) == 'drink toddy'
+    assert people_with_age_drink(0) == 'drink toddy'
+
+    assert people_with_age_drink(17) == 'drink coke'
+    assert people_with_age_drink(15) == 'drink coke'
+    assert people_with_age_drink(14) == 'drink coke'
+
+    assert people_with_age_drink(20) == 'drink beer'
+    assert people_with_age_drink(18) == 'drink beer'
+
+    assert people_with_age_drink(22) == 'drink whisky'
+    assert people_with_age_drink(21) == 'drink whisky'
