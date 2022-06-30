afc40b472be59aa08b85af4d20643ecf26f20da3
sigmanificient <edhyjox@gmail.com>
2022-06-30 11:01:27 +0200

    :white_check_mark: Added test for fake bin

--- src/python/katas/py8kyu/fake_binary.py
+++ src/python/katas/py8kyu/fake_binary.py
@@ -3,3 +3,12 @@
 
 def fake_bin(x: str) -> str:
     return ''.join('1' if int(c) >= 5 else '0' for c in x)
+
+
+def test_fake_bin():
+    assert fake_bin("101") == "01"
+    assert fake_bin("45385593107843568") == "01011110001100111"
+    assert fake_bin("509321967506747") == "101000111101101"
+    assert fake_bin("366058562030849490134388085") == "011011110000101010000011011"
+    assert fake_bin("15889923") == "01111100"
+    assert fake_bin("800857237867") == "100111001111"
