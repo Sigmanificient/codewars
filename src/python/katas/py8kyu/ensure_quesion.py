e9927c584410725a528a812c99e575b3d86683d7
sigmanificient <edhyjox@gmail.com>
2022-06-30 11:17:31 +0200

    :white_check_mark: Added test for ensure question

--- src/python/katas/py8kyu/ensure_quesion.py
+++ src/python/katas/py8kyu/ensure_quesion.py
@@ -1,4 +1,12 @@
 """Kata url: https://www.codewars.com/kata/5866fc43395d9138a7000006."""
 
+
 def ensure_question(s: str) -> str:
     return s if s.endswith('?') else f"{s}?"
+
+
+def test_ensure_question():
+    assert ensure_question("") == "?", "Expected: '?'"
+    assert ensure_question("Yes") == "Yes?", "Expected: '?'"
+    assert ensure_question("No?") == "No?", "Expected: '?'"
+    assert ensure_question("Well????") == "Well????", "Expected: '?'"
