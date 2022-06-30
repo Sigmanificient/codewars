c2a26ed1b3c179f1dd4ac8b4066fa317ed578044
sigmanificient <edhyjox@gmail.com>
2022-06-30 11:42:21 +0200

    :sparkles: Added solution for 'product of consecutive fib' (5kyu)

---
+++ src/python/katas/py5kyu/product_of_consecutive_fib_numbers.py
@@ -0,0 +1,19 @@
+"""Kata url: https://www.codewars.com/kata/5541f58a944b85ce6d00006a."""
+
+
+def product_fib(prod: int) -> int:
+    a, b = 0, 1
+    while a * b < prod:
+        a, b = b, a + b
+
+    return [a, b, a * b == prod]
+
+
+def test_product_fib():
+    assert product_fib(4895) == [55, 89, True]
+    assert product_fib(5895) == [89, 144, False]
+
+    assert product_fib(74049690) == [6765, 10946, True]
+    assert product_fib(84049690) == [10946, 17711, False]
+
+
