641997dccf07fdfe2f9bce68778e9b4d54353fea
sigmanificient <edhyjox@gmail.com>
2022-07-01 22:13:12 +0200

    :white_check_mark: Added test for double char

--- src/python/katas/py8kyu/double_char.py
+++ src/python/katas/py8kyu/double_char.py
@@ -3,3 +3,9 @@
 
 def double_char(s: str) -> str:
     return ''.join(c * 2 for c in s)
+
+
+def test_double_char():
+    assert double_char("String") == "SSttrriinngg"
+    assert double_char("Hello World") == "HHeelllloo  WWoorrlldd"
+    assert double_char("1234!_ ") == "11223344!!__  "
