000bc403aad93cfb3983e89048c429fa55ec8125
sigmanificient <edhyjox@gmail.com>
2022-07-01 22:14:19 +0200

    :white_check_mark: Added test for dollar and cents

--- src/python/katas/py8kyu/dollar_and_cents.py
+++ src/python/katas/py8kyu/dollar_and_cents.py
@@ -1,4 +1,11 @@
 """Kata url: https://www.codewars.com/kata/55902c5eaa8069a5b4000083."""
 
+
 def format_money(amount: int) -> str:
     return f"${amount:.2f}"
+
+
+def test_format_money():
+    assert format_money(0) == '$0.00'
+    assert format_money(39.99) == '$39.99'
+    assert format_money(1000000000) == '$1000000000.00'
