ea2b76ee93efc8b2cc521f803bd74a3b398c87fe
sigmanificient <edhyjox@gmail.com>
2022-07-01 22:24:31 +0200

    :white_check_mark: Added test for sp_eng

--- src/python/katas/py8kyu/do_you_speak_english.py
+++ src/python/katas/py8kyu/do_you_speak_english.py
@@ -3,3 +3,16 @@
 
 def sp_eng(sentence: str) -> bool:
     return "english" in sentence.lower()
+
+
+def test_sp_eng():
+    assert not sp_eng("")
+    assert sp_eng("english")
+    assert sp_eng("1234english ;k")
+    assert sp_eng("English")
+    assert sp_eng("eNgliSh")
+    assert sp_eng("1234#$%%eNglish ;k9")
+    assert not sp_eng("1234egn lis;h")
+    assert not sp_eng("egnlish")
+    assert not sp_eng("EGNlihs")
+    assert not sp_eng("1234englihs**")
