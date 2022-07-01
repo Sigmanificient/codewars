2e302e138c8b793dbe14e1604a3f374bb97b3edc
sigmanificient <edhyjox@gmail.com>
2022-07-01 21:11:16 +0200

    :sparkles: Added solution for 'human-readable time' (5kyu)

---
+++ src/python/katas/py5kyu/human_readable_time.py
@@ -0,0 +1,15 @@
+"""Kata url: https://www.codewars.com/kata/52685f7382004e774f0001f7"""
+
+
+def make_readable(seconds: int) -> str:
+    r, s = divmod(seconds, 60)
+    h, m = divmod(r, 60)
+    return f'{h:02}:{m:02}:{s:02}'
+
+
+def test_make_readable():
+    assert make_readable(0) == "00:00:00"
+    assert make_readable(5) == "00:00:05"
+    assert make_readable(60) == "00:01:00"
+    assert make_readable(86399) == "23:59:59"
+    assert make_readable(359999) == "99:59:59"
