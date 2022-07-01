8990ad5c246087f9a1c6c68a45543d8d31bb35a5
sigmanificient <edhyjox@gmail.com>
2022-07-01 22:56:36 +0200

    :white_check_mark: Added test for counting sheep

--- src/python/katas/py8kyu/couting_sheep.py
+++ src/python/katas/py8kyu/couting_sheep.py
@@ -5,3 +5,16 @@ from typing import Optional, List
 
 def count_sheeps(sheep: List[Optional[bool]]) -> int:
     return sum(x for x in sheep if isinstance(x, bool))
+
+
+def test_count_sheeps():
+    assert count_sheeps(
+        [
+            True, True, True, False,
+            True, True, True, True,
+            True, False, True, False,
+            True, False, False, True,
+            True, True, True, True,
+            False, False, True, True
+        ]
+    ) == 17
