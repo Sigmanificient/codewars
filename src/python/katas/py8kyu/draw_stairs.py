99bf06c8b7d3b3bcb07ae4f504736fd3f10bb359
sigmanificient <edhyjox@gmail.com>
2022-07-01 22:12:30 +0200

    :white_check_mark: Added test for draw stairs

--- src/python/katas/py8kyu/draw_stairs.py
+++ src/python/katas/py8kyu/draw_stairs.py
@@ -1,4 +1,11 @@
 """Kata url: https://www.codewars.com/kata/5b4e779c578c6a898e0005c5."""
 
+
 def draw_stairs(n: int) -> str:
     return '\n'.join(' ' * i + 'I' for i in range(n))
+
+
+def test_draw_stairs():
+    assert draw_stairs(1) == 'I'
+    assert draw_stairs(3) == 'I\n I\n  I'
+    assert draw_stairs(6) == 'I\n I\n  I\n   I\n    I\n     I'
