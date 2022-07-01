4be62c2b6c8267e6b3ebaf36f80ff8e0ca6df578
sigmanificient <edhyjox@gmail.com>
2022-07-01 22:25:57 +0200

    :white_check_mark: Added test for bonus time

--- src/python/katas/py8kyu/do_i_get_a_bonus.py
+++ src/python/katas/py8kyu/do_i_get_a_bonus.py
@@ -3,3 +3,14 @@
 
 def bonus_time(salary: int, bonus: bool) -> str:
     return f"${salary * 10 if bonus else salary}"
+
+
+def test_bonus_time():
+    assert bonus_time(2, True) == '$20'
+    assert bonus_time(78, False) == '$78'
+    assert bonus_time(67890, True) == '$678900'
+
+    assert bonus_time(10000, True) == '$100000'
+    assert bonus_time(25000, True) == '$250000'
+    assert bonus_time(10000, False) == '$10000'
+    assert bonus_time(60000, False) == '$60000'
