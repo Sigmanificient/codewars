6de65eaf389224b492c84e1e11b364bf82de750a
sigmanificient <edhyjox@gmail.com>
2022-07-01 22:26:29 +0200

    :white_check_mark: Added test for dna_to_rna

--- src/python/katas/py8kyu/dna_to_rna_conversion.py
+++ src/python/katas/py8kyu/dna_to_rna_conversion.py
@@ -3,3 +3,9 @@
 
 def dna_to_rna(dna: str) -> str:
     return dna.replace('T', 'U')
+
+
+def test_dna_to_rna():
+    assert dna_to_rna("TTTT") == "UUUU"
+    assert dna_to_rna("GCAT") == "GCAU"
+    assert dna_to_rna("GACCGCCGCC") == "GACCGCCGCC"
