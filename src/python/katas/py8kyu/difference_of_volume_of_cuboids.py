dc2d7fa522e77825e8148b6181967af08ffda18c
sigmanificient <edhyjox@gmail.com>
2022-07-01 22:37:10 +0200

    :white_check_mark: Added test for difference of volume of cuboids

--- src/python/katas/py8kyu/difference_of_volume_of_cuboids.py
+++ src/python/katas/py8kyu/difference_of_volume_of_cuboids.py
@@ -13,3 +13,9 @@ def mul(x: List[int]) -> int:
 
 def find_difference(a: List[int], b: List[int]) -> int:
     return abs(mul(a) - mul(b))
+
+
+def test_find_difference():
+    assert find_difference([1, 2, 3], [1, 2, 3]) == 0
+    assert find_difference([3, 2, 5], [1, 4, 4]) == 14
+    assert find_difference([9, 7, 2], [5, 2, 2]) == 106
