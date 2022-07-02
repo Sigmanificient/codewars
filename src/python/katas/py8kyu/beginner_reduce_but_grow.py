4b756de52b523e16a0a15b7ca108ea3ec3a6cff0
sigmanificient <edhyjox@gmail.com>
2022-07-02 17:51:00 +0000

    :white_check_mark: Added test for reduce but grow

--- src/python/katas/py8kyu/beginner_reduce_but_grow.py
+++ src/python/katas/py8kyu/beginner_reduce_but_grow.py
@@ -8,3 +8,9 @@ def grow(arr: List[int]) -> int:
     for b in arr:
         a *= b
     return a
+
+
+def test_grow():
+    assert grow([1, 2, 3]) == 6
+    assert grow([4, 1, 1, 1, 4]) == 16
+    assert grow([2, 2, 2, 2, 2, 2]) == 64
