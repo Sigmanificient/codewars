aebdb0414864609285ce39cd90c7ebe43c747144
sigmanificient <edhyjox@gmail.com>
2022-07-02 17:33:00 +0000

    :white_check_mark: Added test for add length

--- src/python/katas/py8kyu/add_length.py
+++ src/python/katas/py8kyu/add_length.py
@@ -5,3 +5,11 @@ from typing import List
 
 def add_length(str_: str) -> List[str]:
     return [f"{x} {len(x)}" for x in str_.split()]
+
+
+def test_add_length():
+    assert add_length('') == []
+    assert add_length('y') == ["y 1"]
+    assert add_length('you') == ["you 3"]
+    assert add_length('apple ban') == ["apple 5", "ban 3"]
+    assert add_length('you will win') == ["you 3", "will 4", "win 3"]
