4d360c6af73383c81aea8e96f91a0fec78503271
sigmanificient <edhyjox@gmail.com>
2022-07-02 17:49:00 +0000

    :white_check_mark: Added test for beginner lost without a map

--- src/python/katas/py8kyu/beginner_lost_without_a_map.py
+++ src/python/katas/py8kyu/beginner_lost_without_a_map.py
@@ -5,3 +5,11 @@ from typing import List
 
 def maps(a: List[int]) -> List[int]:
     return [x * 2 for x in a]
+
+
+def test_maps():
+    assert maps([]) == []
+    assert maps([1, 2, 3]) == [2, 4, 6]
+    assert maps([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) == [
+        0, 2, 4, 6, 8, 10, 12, 14, 16, 18
+    ]
