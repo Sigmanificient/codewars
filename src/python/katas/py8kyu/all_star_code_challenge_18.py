400b5f555a1b73b41753999cb3fa1b0a303cf81e
sigmanificient <edhyjox@gmail.com>
2022-07-02 17:35:30 +0000

    :white_check_mark: Added test for all star code challenge 18

--- src/python/katas/py8kyu/all_star_code_challenge_18.py
+++ src/python/katas/py8kyu/all_star_code_challenge_18.py
@@ -3,3 +3,12 @@
 
 def str_count(string: str, letter: str) -> int:
     return string.count(letter)
+
+
+def test_str_count():
+    assert str_count('hello', 'l') == 2
+    assert str_count('hello', 'e') == 1
+    assert str_count('codewars', 'c') == 1
+    assert str_count('ggggg', 'g') == 5
+    assert str_count('hello world', 'o') == 2
+    assert str_count('', 'z') == 0
