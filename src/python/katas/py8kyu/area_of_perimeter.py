d015a2b7427ec49064ec2e0c441b5b422f1e14b8
sigmanificient <edhyjox@gmail.com>
2022-07-02 17:40:00 +0000

    :white_check_mark: Added test for area ir perimeter

--- src/python/katas/py8kyu/area_of_perimeter.py
+++ src/python/katas/py8kyu/area_of_perimeter.py
@@ -3,3 +3,9 @@
 
 def area_or_perimeter(length: int, width: int) -> int:
     return (length * 2 + 2 * width) if length != width else length ** 2
+
+
+def test_area_or_perimeter():
+    assert area_or_perimeter(4, 4) == 16
+    assert area_or_perimeter(6, 10) == 32
+    assert area_or_perimeter(10, 10) == 100
