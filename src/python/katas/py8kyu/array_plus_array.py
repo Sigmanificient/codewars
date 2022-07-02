bfac0115d2084ad023eabcbd085155b590c175ed
sigmanificient <edhyjox@gmail.com>
2022-07-02 17:41:30 +0000

    :white_check_mark: Added test for array plus array

--- src/python/katas/py8kyu/array_plus_array.py
+++ src/python/katas/py8kyu/array_plus_array.py
@@ -6,3 +6,10 @@ from typing import List
 
 def array_plus_array(arr1: List[int], arr2: List[int]) -> int:
     return sum(arr1 + arr2)
+
+
+def test_array_plus_array():
+    assert array_plus_array([1, 2, 3], [4, 5, 6]) == 21
+    assert array_plus_array([-1, -2, -3], [-4, -5, -6]) == -21
+    assert array_plus_array([0, 0, 0], [4, 5, 6]) == 15
+    assert array_plus_array([100, 200, 300], [400, 500, 600]) == 2100
