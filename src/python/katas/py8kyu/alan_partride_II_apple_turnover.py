a3db8554721c3a13f5f6fb32c9d3390ac4830786
sigmanificient <edhyjox@gmail.com>
2022-07-02 17:35:00 +0000

    :white_check_mark: Added test for alan partride II

--- src/python/katas/py8kyu/alan_partride_II_apple_turnover.py
+++ src/python/katas/py8kyu/alan_partride_II_apple_turnover.py
@@ -8,3 +8,10 @@ not_hot: str = "Help yourself to a honeycomb Yorkie for the glovebox."
 
 def apple(x: Union[int, str]) -> str:
     return hot if int(x) ** 2 > 1000 else not_hot
+
+
+def test_apple():
+    assert apple('50') == "It's hotter than the sun!!"
+    assert apple(4) == "Help yourself to a honeycomb Yorkie for the glovebox."
+    assert apple("12") == "Help yourself to a honeycomb Yorkie for the glovebox."
+    assert apple(60) == "It's hotter than the sun!!"
