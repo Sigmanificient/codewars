817b1e9b16745465dcdc44cdb59dcbfcc9c06436
sigmanificient <edhyjox@gmail.com>
2022-07-02 16:34:00 +0000

    :white_check_mark: Added test for the 2 latest katas

--- src/python/katas/py7kyu/complementary_dna.py
+++ src/python/katas/py7kyu/complementary_dna.py
@@ -1,2 +1,11 @@
+"""Kata url: https://www.codewars.com/kata/5556282156230d0e5e000089."""
+
+
 def dna_strand(dna: str) -> str:
     return dna.translate(dna.maketrans('ACGT', 'TGCA'))
+
+
+def test_dna_strand():
+    assert dna_strand("AAAA") == "TTTT"
+    assert dna_strand("ATTGC") == "TAACG"
+    assert dna_strand("GTAT") == "CATA"
--- src/python/katas/py7kyu/maximum_multiple.py
+++ src/python/katas/py7kyu/maximum_multiple.py
@@ -1,2 +1,14 @@
+"""Kata url: https://www.codewars.com/kata/5aba780a6a176b029800041c."""
+
+
 def max_multiple(divisor: int, bound: int) -> int:
     return bound - (bound % divisor)
+
+
+def test_max_multiple():
+    assert max_multiple(2, 7) == 6
+    assert max_multiple(3, 10) == 9
+    assert max_multiple(7, 17) == 14
+    assert max_multiple(10, 50) == 50
+    assert max_multiple(37, 200) == 185
+    assert max_multiple(7, 100) == 98
