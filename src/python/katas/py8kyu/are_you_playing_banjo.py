2e9e307632c2d6df8c1539ef64e50d726d788e95
sigmanificient <edhyjox@gmail.com>
2022-07-02 17:39:00 +0000

    :white_check_mark: Added test for are you playing banjo

--- src/python/katas/py8kyu/are_you_playing_banjo.py
+++ src/python/katas/py8kyu/are_you_playing_banjo.py
@@ -7,3 +7,11 @@ def are_you_playing_banjo(name: str) -> str:
         if name.lower().startswith('r')
         else f"{name} does not play banjo"
     )
+
+
+def test_are_you_playing_banjo():
+    assert are_you_playing_banjo("John") == "John does not play banjo"
+    assert are_you_playing_banjo("martin") == "martin does not play banjo"
+    assert are_you_playing_banjo("Rikke") == "Rikke plays banjo"
+    assert are_you_playing_banjo("bravo") == "bravo does not play banjo"
+    assert are_you_playing_banjo("rolf") == "rolf plays banjo"
