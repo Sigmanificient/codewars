4c24cee6003b94d9e9b23574ac6723e0eeded91b
sigmanificient <edhyjox@gmail.com>
2022-07-02 17:51:00 +0000

    :white_check_mark: Added test for clock

--- src/python/katas/py8kyu/beginner_series_2_clock.py
+++ src/python/katas/py8kyu/beginner_series_2_clock.py
@@ -3,3 +3,12 @@
 
 def past(h: int, m: int, s: int) -> int:
     return (h * 3600 + m * 60 + s) * 1000
+
+
+def test_past():
+    assert past(0, 1, 1) == 61000
+    assert past(1, 1, 1) == 3661000
+    assert past(0, 0, 0) == 0
+    assert past(1, 0, 1) == 3601000
+    assert past(1, 0, 0) == 3600000
+    assert past(0, 1, 0) == 60000
