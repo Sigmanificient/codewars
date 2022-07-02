7a143e6211efa1d36707e5984a65060f9b64ee0c
sigmanificient <edhyjox@gmail.com>
2022-07-02 17:37:00 +0000

    :white_check_mark: Added test for are arrow function odd

--- src/python/katas/py8kyu/are_arrow_functions_odd.py
+++ src/python/katas/py8kyu/are_arrow_functions_odd.py
@@ -3,3 +3,10 @@
 from typing import Callable, List
 
 odds: Callable[[List[int]], List[int]] = lambda arr: [x for x in arr if x % 2]
+
+
+def test_solution():
+    assert odds([]) == []
+    assert odds([2, 4, 6]) == []
+    assert odds([1, 3, 5]) == [1, 3, 5]
+    assert odds([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
