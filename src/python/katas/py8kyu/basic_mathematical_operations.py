0befcc4d544896edbf78e7c321a9878400d62fa2
sigmanificient <edhyjox@gmail.com>
2022-07-02 17:42:00 +0000

    :white_check_mark: Added test for basic math ops

--- src/python/katas/py8kyu/basic_mathematical_operations.py
+++ src/python/katas/py8kyu/basic_mathematical_operations.py
@@ -12,3 +12,15 @@ ops: Dict[str, Callable[[int, int], Union[int, float]]] = {
 
 def basic_op(operator, value1, value2) -> Union[int, float]:
     return ops[operator](value1, value2)
+
+
+def test_basic_op():
+    assert basic_op('+', 4, 7) == 11
+    assert basic_op('-', 15, 18) == -3
+    assert basic_op('*', 5, 5) == 25
+    assert basic_op('/', 49, 7) == 7
+
+    assert basic_op('+', 1, 5) == 5
+    assert basic_op('-', 5, 1) == 4
+    assert basic_op('*', 2, 2) == 4
+    assert basic_op('/', 4, 2) == 2
