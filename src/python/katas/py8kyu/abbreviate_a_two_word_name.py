dfb6a54f10d35e4743afebdb5c9f3ba0619c6fcd
sigmanificient <edhyjox@gmail.com>
2022-07-02 17:31:00 +0000

    :white_check_mark: Added test for a two word name

--- src/python/katas/py8kyu/abbreviate_a_two_word_name.py
+++ src/python/katas/py8kyu/abbreviate_a_two_word_name.py
@@ -3,3 +3,11 @@
 
 def abbrev_name(name: str) -> str:
     return '.'.join(word[0].upper() for word in name.split())
+
+
+def test_abbrev_name():
+    assert abbrev_name("Sam Harris") == "S.H"
+    assert abbrev_name("patrick feenan") == "P.F"
+    assert abbrev_name("Evan C") == "E.C"
+    assert abbrev_name("P Favuzzi") == "P.F"
+    assert abbrev_name("David Mendieta") == "D.M"
