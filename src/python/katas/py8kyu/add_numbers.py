efba7302d58ed8c89c8589f68c02a37d853d9c99
sigmanificient <edhyjox@gmail.com>
2022-07-02 17:34:00 +0000

    :white_check_mark: Added test for add numbers

--- src/python/katas/py8kyu/add_numbers.py
+++ src/python/katas/py8kyu/add_numbers.py
@@ -1,6 +1,13 @@
 # Kata url: https://www.codewars.com/kata/5926d7494b2b1843780001e6.
-from typing import Tuple
 
 
 def add(*n: int) -> int:
     return sum(n)
+
+
+def test_add():
+    assert add() == 0
+    assert add(2) == 2
+    assert add(1, 1) == 2
+    assert add(1, 2, 3) == 6
+    assert add(-3, -2, -1, 1, 2, 3, 4) == 4
