d32cbd842bef868954dd1f209347c21b60c27028
sigmanificient <edhyjox@gmail.com>
2022-07-03 17:52:26 +0200

    :white_check_mark: Added test for school paperwork

--- src/python/katas/py8kyu/beginner_series_1_school_paperwork.py
+++ src/python/katas/py8kyu/beginner_series_1_school_paperwork.py
@@ -3,3 +3,11 @@
 
 def paperwork(n: int, m: int) -> int:
     return 0 if n < 0 or m < 0 else n * m
+
+
+def test_paperwork():
+    assert paperwork(5, 5) == 25
+    assert paperwork(-5, 5) == 0
+    assert paperwork(5, -5) == 0
+    assert paperwork(-5, -5) == 0
+    assert paperwork(5, 0) == 0
