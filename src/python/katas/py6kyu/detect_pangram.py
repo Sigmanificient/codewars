ee22937d87f145bfc6d54691cf3aa80777b3555f
sigmanificient <edhyjox@gmail.com>
2022-07-03 18:55:07 +0200

    :sparkles: Added solution for pangram

---
+++ src/python/katas/py6kyu/detect_pangram.py
@@ -0,0 +1,12 @@
+"""Kata url: https://www.codewars.com/kata/545cedaa9943f7fe7b000048."""
+
+
+def is_pangram(s: str) -> str:
+    return ''.join(
+        filter(str.isalpha, sorted(set(s.lower())))
+    ) == 'abcdefghijklmnopqrstuvwxyz'
+
+
+def test_is_pangram():
+    assert is_pangram("The quick, brown fox jumps over the lazy dog!")
+    assert not is_pangram("This is not a pangram.")
