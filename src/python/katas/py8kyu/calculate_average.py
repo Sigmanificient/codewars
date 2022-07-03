d7613bc2bfe52c24e80fe1dc73c05bcff01f1439
sigmanificient <edhyjox@gmail.com>
2022-07-03 18:03:22 +0200

    :white_check_mark: Added test for calculating avg

--- src/python/katas/py8kyu/calculate_average.py
+++ src/python/katas/py8kyu/calculate_average.py
@@ -5,3 +5,12 @@ from typing import List
 
 def find_average(numbers: List[int]) -> float:
     return sum(numbers) / len(numbers)
+
+
+def test_find_average():
+    assert find_average([0, 100]) == 50
+    assert find_average([0, 0, 0, 0, 5]) == 1
+    assert find_average([1, 1, 1, 1]) == 1
+
+    assert find_average([1, 2, 3]) == 2
+    assert find_average([1, 5, 87, 45, 8, 4]) == 25
