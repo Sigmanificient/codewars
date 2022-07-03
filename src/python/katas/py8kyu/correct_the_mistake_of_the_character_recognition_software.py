6abedcc5a62a69a274edbd076bd81cad25bc645d
sigmanificient <edhyjox@gmail.com>
2022-07-03 22:40:57 +0200

    :white_check_mark: Added test for correct the mistake

--- src/python/katas/py8kyu/correct_the_mistake_of_the_character_recognition_software.py
+++ src/python/katas/py8kyu/correct_the_mistake_of_the_character_recognition_software.py
@@ -3,3 +3,11 @@
 
 def correct(s: str) -> str:
     return s.replace('5', 'S').replace('0', 'O').replace('1', 'I')
+
+
+def test_correct():
+    assert correct("L0ND0N") == "LONDON"
+    assert correct("DUBL1N") == "DUBLIN"
+    assert correct("51NGAP0RE") == "SINGAPORE"
+    assert correct("BUDAPE5T") == "BUDAPEST"
+    assert correct("PAR15") == "PARIS"
