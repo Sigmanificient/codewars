a23c77d0a564c1663aaf6154bb28dc745296b07b
sigmanificient <edhyjox@gmail.com>
2022-07-03 22:39:54 +0200

    :white_check_mark: Added test for int -> int bin

--- src/python/katas/py8kyu/convert_to_binary.py
+++ src/python/katas/py8kyu/convert_to_binary.py
@@ -1,4 +1,12 @@
 """Kata url: https://www.codewars.com/kata/59fca81a5712f9fa4700159a."""
 
+
 def to_binary(n: int) -> int:
     return int(f'{n:b}')
+
+
+def test_to_binary():
+    assert to_binary(1) == 1
+    assert to_binary(2) == 10
+    assert to_binary(3) == 11
+    assert to_binary(5) == 101
