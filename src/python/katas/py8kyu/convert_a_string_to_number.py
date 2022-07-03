2ba9e90b4d1d6abc654cb3af13abac785893d028
sigmanificient <edhyjox@gmail.com>
2022-07-03 22:35:37 +0200

    :white_check_mark: Added test for string -> int

--- src/python/katas/py8kyu/convert_a_string_to_number.py
+++ src/python/katas/py8kyu/convert_a_string_to_number.py
@@ -1,4 +1,12 @@
 """Kata url: https://www.codewars.com/kata/544675c6f971f7399a000e79."""
 
+
 def string_to_number(s: str) -> int:
     return int(s)
+
+
+def test_string_to_number():
+    assert string_to_number("-7") == -7
+    assert string_to_number("1234") == 1234
+    assert string_to_number("605") == 605
+    assert string_to_number("1405") == 1405
