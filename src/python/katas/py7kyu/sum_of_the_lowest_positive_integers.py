a2f67b9b4f8781c59d3a54c988d1cc245ed9a8b3
sigmanificient <edhyjox@gmail.com>
2022-07-03 22:10:54 +0200

    :sparkles: Added solution for sum of 2 smallest int

---
+++ src/python/katas/py7kyu/sum_of_the_lowest_positive_integers.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+def sum_two_smallest_numbers(numbers: List[int]) -> int:
+    a, b, *_ = sorted(numbers)
+    return a + b
+
+
+def test_sum_tow_smallest():
+    assert sum_two_smallest_numbers([5, 8, 12, 18, 22]) == 13
+    assert sum_two_smallest_numbers([7, 15, 12, 18, 22]) == 19
+    assert sum_two_smallest_numbers([25, 42, 12, 18, 22]) == 30
