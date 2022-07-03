e476d2de2f4f2e32717705995c0f9c2a2a2f2a4c
sigmanificient <edhyjox@gmail.com>
2022-07-03 17:56:43 +0200

    :white_check_mark: Added test for cockroach

--- src/python/katas/py8kyu/beginner_series_4_cockroack.py
+++ src/python/katas/py8kyu/beginner_series_4_cockroack.py
@@ -5,3 +5,11 @@ from typing import Union
 
 def cockroach_speed(s: Union[int, float]) -> int:
     return int((s * 1000) // 36)
+
+
+def test_cockroach_speed():
+    assert cockroach_speed(0) == 0
+    assert cockroach_speed(1.08) == 30
+    assert cockroach_speed(1.09) == 30
+    assert cockroach_speed(2) == 55
+    assert cockroach_speed(3) == 83
