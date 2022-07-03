7b706b973a75a28011e158b1565789788e9d4a36
sigmanificient <edhyjox@gmail.com>
2022-07-03 22:37:04 +0200

    :white_check_mark: Added test for bool -> word

--- src/python/katas/py8kyu/convert_boolean_values_to_string_yes_and_no.py
+++ src/python/katas/py8kyu/convert_boolean_values_to_string_yes_and_no.py
@@ -3,3 +3,8 @@
 
 def bool_to_word(boolean: bool) -> str:
     return 'Yes' if boolean else 'No'
+
+
+def test_bool_to_word():
+    assert bool_to_word(True) == 'Yes'
+    assert bool_to_word(False) == 'No'
