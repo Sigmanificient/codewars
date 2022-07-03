56c990e25ffd9528879bba94254f2c31d995a32a
sigmanificient <edhyjox@gmail.com>
2022-07-03 22:30:07 +0200

    :white_check_mark: Added test for classic hello world

--- src/python/katas/py8kyu/classic_hello_world.py
+++ src/python/katas/py8kyu/classic_hello_world.py
@@ -1,7 +1,13 @@
 """Kata url: https://www.codewars.com/kata/57036f007fd72e3b77000023."""
 
+
 class Solution:
 
     @staticmethod
-    def main():
+    def main(*_args):
         print("Hello World!")
+
+
+def test_solution():
+    assert Solution.main() == "Hello World!"
+    assert Solution.main("Hola mundo!") == "Hello World!"
