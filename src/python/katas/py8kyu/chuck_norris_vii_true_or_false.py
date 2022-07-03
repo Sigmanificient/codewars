65e659756105cf5f32606d0c6f30369f1725a3b0
sigmanificient <edhyjox@gmail.com>
2022-07-03 22:25:16 +0200

    :white_check_mark: Added test for if chuck says so

--- src/python/katas/py8kyu/chuck_norris_vii_true_or_false.py
+++ src/python/katas/py8kyu/chuck_norris_vii_true_or_false.py
@@ -1,2 +1,9 @@
+"""Kata url: https://www.codewars.com/kata/570669d8cb7293a2d1001473."""
+
+
 def if_chuck_says_so() -> bool:
     return not True
+
+
+def test_if_chuck_says_so():
+    assert not if_chuck_says_so()
