82a6b57d99191fa1a551509ed2048c496607d621
sigmanificient <edhyjox@gmail.com>
2022-07-03 22:33:18 +0200

    :white_check_mark: Added test for concurrency currency

--- src/python/katas/py8kyu/concurency_convercy_i.py
+++
@@ -1,4 +0,0 @@
-# Kata url: https://www.codewars.com/kata/5c707567c447bd46c6bafd68.
-
-def usd(eur: int) -> str:
-    return f'${eur * 1.1363636:.2f}'
---
+++ src/python/katas/py8kyu/concurrency_currency_i.py
@@ -0,0 +1,13 @@
+"""Kata url: https://www.codewars.com/kata/5c707567c447bd46c6bafd68."""
+
+
+def usd(eur: int) -> str:
+    return f'${eur * 1.1363636:.2f}'
+
+
+def test_usd():
+    assert usd(1) == '$1.14'
+    assert usd(2) == '$2.27'
+    assert usd(5) == '$5.68'
+    assert usd(20) == '$22.73'
+    assert usd(50) == '$56.82'
