a7b652f8d609633ddefddd3b4faec0509ccb4003
sigmanificient <edhyjox@gmail.com>
2022-07-03 17:57:28 +0200

    :white_check_mark: Added test for bin to decimal

--- src/python/katas/py8kyu/bin_to_decimal.py
+++ src/python/katas/py8kyu/bin_to_decimal.py
@@ -3,3 +3,12 @@
 
 def bin_to_decimal(inp: str) -> int:
     return int(inp, 2)
+
+
+def test_bin_to_decimal():
+    assert bin_to_decimal("0") == 0
+    assert bin_to_decimal("1") == 1
+    assert bin_to_decimal("10") == 2
+    assert bin_to_decimal("11") == 3
+    assert bin_to_decimal("101010") == 42
+    assert bin_to_decimal("1001001") == 73
