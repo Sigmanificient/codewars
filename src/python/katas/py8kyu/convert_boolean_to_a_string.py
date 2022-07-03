99d1760a1947f2eea55b91186df7c7d77dcf7a20
sigmanificient <edhyjox@gmail.com>
2022-07-03 22:36:20 +0200

    :white_check_mark: Added test for bool -> string

--- src/python/katas/py8kyu/convert_boolean_to_a_string.py
+++ src/python/katas/py8kyu/convert_boolean_to_a_string.py
@@ -1,4 +1,10 @@
 """Kata url: https://www.codewars.com/kata/551b4501ac0447318f0009cd."""
 
+
 def boolean_to_string(b: bool) -> str:
     return str(b)
+
+
+def test_bool_to_string():
+    assert boolean_to_string(True) == "True"
+    assert boolean_to_string(False) == "False"
