e58ec8559de95a4b685315be191911af684036ab
sigmanificient <edhyjox@gmail.com>
2022-07-03 18:07:34 +0200

    :white_check_mark: Added test for capitalize word

--- src/python/katas/py8kyu/capacity_and_mutability.py
+++ src/python/katas/py8kyu/capacity_and_mutability.py
@@ -1,4 +1,11 @@
 """Kata url: https://www.codewars.com/kata/595970246c9b8fa0a8000086."""
 
+
 def capitalize_word(word: str) -> str:
     return word.capitalize()
+
+
+def test_capitalize_word():
+    assert capitalize_word('word') == 'Word'
+    assert capitalize_word('i') == 'I'
+    assert capitalize_word('glasswear') == 'Glasswear'
