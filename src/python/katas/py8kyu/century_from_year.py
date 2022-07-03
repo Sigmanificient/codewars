855dbeddfe9964c8783e4acc5f938fc045442f91
sigmanificient <edhyjox@gmail.com>
2022-07-03 18:08:32 +0200

    :white_check_mark: Added test for century from year

--- src/python/katas/py8kyu/century_from_year.py
+++ src/python/katas/py8kyu/century_from_year.py
@@ -5,3 +5,12 @@ from math import ceil
 
 def century(year: int) -> int:
     return ceil(year / 100)
+
+
+def test_century():
+    assert century(89) == 1
+    assert century(1705) == 18
+    assert century(1900) == 19
+    assert century(1601) == 17
+    assert century(2000) == 20
+    assert century(356) == 4
