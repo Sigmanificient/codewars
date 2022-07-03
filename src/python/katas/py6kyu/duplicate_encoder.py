26c9e283c9e885c8d045df15a9728feaf4092979
sigmanificient <edhyjox@gmail.com>
2022-07-03 21:48:14 +0200

    :sparkles: Added solution for duplicate encoder

---
+++ src/python/katas/py6kyu/duplicate_encoder.py
@@ -0,0 +1,13 @@
+"""Kata url: https://www.codewars.com/kata/54b42f9314d9229fd6000d9c."""
+
+
+def duplicate_encode(word: str) -> str:
+    word = word.lower()
+    return ''.join('(' if word.count(c) == 1 else ')' for c in word)
+
+
+def test_duplicate_encoder():
+    assert duplicate_encode("din") == "((("
+    assert duplicate_encode("recede") == "()()()"
+    assert duplicate_encode("Success") == ")())())"
+    assert duplicate_encode("(( @") == "))(("
