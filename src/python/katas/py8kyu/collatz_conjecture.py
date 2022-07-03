140caf0e7d6074ea482581ca5a30e363f3300688
sigmanificient <edhyjox@gmail.com>
2022-07-03 22:30:55 +0200

    :white_check_mark: Added test for hotpo

--- src/python/katas/py8kyu/collatz_conjecture.py
+++ src/python/katas/py8kyu/collatz_conjecture.py
@@ -8,3 +8,10 @@ def hotpo(n: int) -> int:
         c += 1
 
     return c
+
+
+def test_hotpo():
+    assert hotpo(1) == 0
+    assert hotpo(5) == 5
+    assert hotpo(6) == 8
+    assert hotpo(23) == 15
