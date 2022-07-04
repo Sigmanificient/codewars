a800f058f749568062885324e6d7ad0ad5bdb27f
sigmanificient <edhyjox@gmail.com>
2022-07-04 22:53:28 +0200

    :white_check_mark: Added test for vowel count

--- src/python/katas/py7kyu/vowel_count.py
+++ src/python/katas/py7kyu/vowel_count.py
@@ -1,5 +1,13 @@
 """Kata url: https://www.codewars.com/kata/54ff3102c1bad923760001f3."""
 
 
-def getCount(sentence: str) -> int:
+def get_count(sentence: str) -> int:
     return sum(sentence.count(v) for v in "aeiou")
+
+
+def test_get_count():
+    assert get_count("") == 0
+    assert get_count("y") == 0
+    assert get_count("aeiou") == 5
+    assert get_count("bcdfghjklmnpqrstvwxz y") == 0
+    assert get_count("abracadabra") == 5
