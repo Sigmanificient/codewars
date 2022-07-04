3308a2c5546ded8b009778e34db1e55968ccf627
sigmanificient <edhyjox@gmail.com>
2022-07-04 22:55:35 +0200

    :white_check_mark: Added test for vowel changer

--- src/python/katas/py7kyu/vowel_changer.py
+++ src/python/katas/py7kyu/vowel_changer.py
@@ -3,3 +3,10 @@
 
 def vowel_change(txt: str, vow: str) -> str:
     return txt.translate(str.maketrans('aeiou', vow * 5))
+
+
+def test_vowel_change():
+    assert vowel_change(
+        "hannah hannah bo-bannah banana fanna fo-fannah fee, fy, mo-mannah. hannah!", 'i'
+    ) == 'hinnih hinnih bi-binnih binini finni fi-finnih fii, fy, mi-minnih. hinnih!'
+    assert vowel_change('adira wants to go to the park', 'o') == 'odoro wonts to go to tho pork'
