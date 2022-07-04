6faf820e621a51b8e1ee62d17a0dc678e3d5e1c7
sigmanificient <edhyjox@gmail.com>
2022-07-04 22:41:41 +0200

    :white_check_mark: Added test for count of monkeys

--- src/python/katas/py8kyu/count_of_monkeys.py
+++ src/python/katas/py8kyu/count_of_monkeys.py
@@ -6,3 +6,12 @@ from typing import List
 
 def monkey_count(n: int) -> List[int]:
     return list(range(1, n + 1))
+
+
+def test_monkey_count():
+    assert monkey_count(5) == [1, 2, 3, 4, 5]
+    assert monkey_count(3) == [1, 2, 3]
+    assert monkey_count(9) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    assert monkey_count(10) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert monkey_count(20) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    assert monkey_count(1) == [1]
