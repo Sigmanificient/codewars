53cf84dca7b7d3683c138c57faf515a961380feb
sigmanificient <edhyjox@gmail.com>
2022-07-04 22:46:03 +0200

    :white_check_mark: Added test for 0 & 1

--- src/python/katas/py7kyu/zeros_and_ones.py
+++ src/python/katas/py7kyu/zeros_and_ones.py
@@ -5,3 +5,10 @@ from typing import List
 
 def binary_array_to_number(arr: List[int]) -> int:
     return int(''.join(map(str, arr)), 2)
+
+
+def test_binary_array_to_number():
+    assert binary_array_to_number([0, 0, 0, 1]) == 1
+    assert binary_array_to_number([1, 1, 0, 1]) == 13
+    assert binary_array_to_number([1, 0, 0, 1, 0, 0, 1, 1, 1, 0]) == 590
+    assert binary_array_to_number([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) == 0
