d63b128d9a06bffb905e87e16c433a5ddb7ad0bd
sigmanificient <edhyjox@gmail.com>
2022-07-04 23:11:53 +0200

    :white_check_mark: Added test for sorted but not all

--- src/python/katas/py7kyu/some_but_not_all.py
+++ src/python/katas/py7kyu/some_but_not_all.py
@@ -5,3 +5,12 @@ from typing import Any, Callable, Union, List
 
 def some_but_not_all(seq: Union[str, List[int]], pred: Callable[[Any], Any]):
     return 0 < sum(pred(x) for x in seq) < len(seq)
+
+
+def test_some_but_not_all():
+    assert some_but_not_all('abcdefg&%$', str.isalpha)
+    assert not some_but_not_all('&%$=', str.isalpha)
+    assert not some_but_not_all('abcdefg', str.isalpha)
+    assert some_but_not_all([4, 1], lambda x: x > 3)
+    assert not some_but_not_all([1, 1], lambda x: x > 3)
+    assert not some_but_not_all([4, 4], lambda x: x > 3)
