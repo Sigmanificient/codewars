fe4dd55ca915be68432e665a2d0cb3b63f1d42fc
sigmanificient <edhyjox@gmail.com>
2022-07-04 23:10:19 +0200

    :white_check_mark: Added test for sorted numbers

--- src/python/katas/py7kyu/sorted_numbers.py
+++ src/python/katas/py7kyu/sorted_numbers.py
@@ -5,3 +5,11 @@ from typing import Optional, List
 
 def solution(nums: Optional[List[int]]) -> List[int]:
     return sorted(nums) if nums is not None else []
+
+
+def test_solution():
+    assert solution([]) == []
+    assert solution(None) == []
+    assert solution([1, 2, 3, 10, 5]) == [1, 2, 3, 5, 10]
+    assert solution([20, 2, 10]) == [2, 10, 20]
+    assert solution([2, 20, 10]) == [2, 10, 20]
