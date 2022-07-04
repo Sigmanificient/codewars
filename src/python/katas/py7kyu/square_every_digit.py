e6a25b9803ef034a1efbf7562c80be1b5b3cb6bc
sigmanificient <edhyjox@gmail.com>
2022-07-04 23:09:08 +0200

    :white_check_mark: Added test for square every digits

--- src/python/katas/py7kyu/square_every_digit.py
+++ src/python/katas/py7kyu/square_every_digit.py
@@ -1,5 +1,11 @@
-"""Kata url: https://www.codewars.com/kata/546e2562b03326a88e000020/. """
+"""Kata url: https://www.codewars.com/kata/546e2562b03326a88e000020. """
 
 
 def square_digits(num: int) -> int:
     return int(''.join(str(int(digit) ** 2) for digit in str(num)))
+
+
+def test_square_digits():
+    assert square_digits(0) == 0
+    assert square_digits(2) == 4
+    assert square_digits(9119) == 811181
