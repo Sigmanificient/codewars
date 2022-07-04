8640e230751457d3d78b2119ebf05dbdacac9dd8
sigmanificient <edhyjox@gmail.com>
2022-07-04 23:08:06 +0200

    :white_check_mark: Added test for string swap vowel case

--- src/python/katas/py7kyu/string_swap_vowel_case.py
+++ src/python/katas/py7kyu/string_swap_vowel_case.py
@@ -1,6 +1,15 @@
 """Kata url: https://www.codewars.com/kata/5803c0c6ab6c20a06f000026."""
 
+
 def swap_vowel_case(st: str) -> str:
     return ''.join(
         x.swapcase() if x.lower() in 'aeoui' else x for x in st
     )
+
+
+def test_swap_vowel_case():
+    assert swap_vowel_case(" ") == " "
+    assert swap_vowel_case("C Is AlIvE!") == "C is alive!"
+    assert swap_vowel_case("to") == "tO"
+    assert swap_vowel_case("The") == "ThE"
+    assert swap_vowel_case("a") == "A"
