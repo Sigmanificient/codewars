3ad3e7fa80cb89c4a874ca472236149115c59dc8
sigmanificient <edhyjox@gmail.com>
2022-07-04 23:04:42 +0200

    :white_check_mark: Added test for sum diff

--- src/python/katas/py7kyu/sum_or_difference.py
+++ src/python/katas/py7kyu/sum_or_difference.py
@@ -3,3 +3,8 @@
 
 def sum_diff(a: int, b: int, c: int) -> int:
     return (b + c) if a % 2 else max(b, c) - min(b, c)
+
+
+def test_sum_diff():
+    assert sum_diff(3, 2, 5) == 7
+    assert sum_diff(3, 2, 3) == 5
