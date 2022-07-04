2379e2d2059e9e792b75cf591305daf78ee21246
sigmanificient <edhyjox@gmail.com>
2022-07-04 22:43:20 +0200

    :white_check_mark: Added test for count number of cubes paint on

--- src/python/katas/py8kyu/count_the_number_of_cubes_with_paint_on.py
+++ src/python/katas/py8kyu/count_the_number_of_cubes_with_paint_on.py
@@ -4,3 +4,9 @@
 def count_squares(cuts: int) -> int:
     cuts += 1
     return (cuts ** 3) - ((cuts - 2) ** 3)
+
+
+def test_count_squares():
+    assert count_squares(5) == 152
+    assert count_squares(16) == 1538
+    assert count_squares(23) == 3176
