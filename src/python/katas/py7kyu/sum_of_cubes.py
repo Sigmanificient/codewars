c7ee1d2b5373c1f2a5b15b8e012c001fdb6ef368
sigmanificient <edhyjox@gmail.com>
2022-07-04 23:07:19 +0200

    :white_check_mark: Added test for sum of cubes

--- src/python/katas/py7kyu/sum_of_cubes.py
+++ src/python/katas/py7kyu/sum_of_cubes.py
@@ -3,3 +3,13 @@
 
 def sum_cubes(n: int) -> int:
     return sum(x ** 3 for x in range(1, n + 1))
+
+
+def test_sum_cubes():
+    assert sum_cubes(1) == 1
+    assert sum_cubes(2) == 9
+    assert sum_cubes(3) == 36
+    assert sum_cubes(4) == 100
+    assert sum_cubes(10) == 3025
+    assert sum_cubes(123) == 58155876
+    assert sum_cubes(0) == 0
