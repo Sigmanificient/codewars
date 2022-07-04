096b1eb9b4cb34de46516251c073adff7a1d2ddf
sigmanificient <edhyjox@gmail.com>
2022-07-04 23:02:41 +0200

    :white_check_mark: Added test for format poem

--- src/python/katas/py7kyu/thinkful_string_drills_poem_formatter.py
+++ src/python/katas/py7kyu/thinkful_string_drills_poem_formatter.py
@@ -3,3 +3,31 @@
 
 def format_poem(poem: str) -> str:
     return '.\n'.join(poem.split('. '))
+
+
+def test_format_poem():
+    assert format_poem(
+        'Beautiful is better than ugly. Explicit is better than implicit.'
+        ' Simple is better than complex. Complex is better than complicated.'
+    ) == (
+        'Beautiful is better than ugly.\nExplicit is better than implicit.'
+        '\nSimple is better than complex.\nComplex is better than complicated.'
+    )
+
+    assert format_poem(
+        "Flat is better than nested. Sparse is better than dense. Readability "
+        "counts. Special cases aren't special enough to break the rules."
+    ) == (
+        "Flat is better than nested.\nSparse is better than dense.\nReadability "
+        "counts.\nSpecial cases aren't special enough to break the rules."
+    )
+
+    assert format_poem(
+        "Although practicality beats purity. Errors should never pass silently."
+        " Unless explicitly silenced. In the face of ambiguity, refuse the "
+        "temptation to guess."
+    ) == (
+        "Although practicality beats purity.\nErrors should never pass "
+        "silently.\nUnless explicitly silenced.\nIn the face of ambiguity, "
+        "refuse the temptation to guess."
+    )
