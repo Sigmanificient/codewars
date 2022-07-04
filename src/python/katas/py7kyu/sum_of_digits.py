d1af17323f7467632aa6075dd21e68f1ef135a0b
sigmanificient <edhyjox@gmail.com>
2022-07-04 23:06:27 +0200

    :white_check_mark: Added test for sum of digits

--- src/python/katas/py7kyu/sum_of_digits.py
+++ src/python/katas/py7kyu/sum_of_digits.py
@@ -9,3 +9,13 @@ def sum_of_digits(digits: Optional[Union[int, str]]) -> str:
 
     digits = str(digits)
     return ' + '.join(digits) + f" = {sum(map(int, digits))}"
+
+
+def test_sum_of_digits():
+    assert sum_of_digits("3433") == "3 + 4 + 3 + 3 = 13"
+    assert sum_of_digits("64323") == "6 + 4 + 3 + 2 + 3 = 18"
+    assert sum_of_digits("8") == "8 = 8"
+    assert sum_of_digits(3433) == "3 + 4 + 3 + 3 = 13"
+    assert sum_of_digits(64323) == "6 + 4 + 3 + 2 + 3 = 18"
+    assert sum_of_digits(8) == "8 = 8"
+    assert sum_of_digits(None) == ""
