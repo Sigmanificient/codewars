e10b0778b98d1ac992e8a6791912f4d55836a4e2
sigmanificient <edhyjox@gmail.com>
2022-07-04 23:03:18 +0200

    :white_check_mark: Added test for switcheroo

--- src/python/katas/py7kyu/switcheroo.py
+++ src/python/katas/py7kyu/switcheroo.py
@@ -3,3 +3,11 @@
 
 def switcheroo(s: str) -> str:
     return s.translate(str.maketrans('ab', 'ba'))
+
+
+def test_switcheroo():
+    assert switcheroo("abc") == "bac"
+    assert switcheroo('aaabcccbaaa') == 'bbbacccabbb'
+    assert switcheroo('ccccc') == 'ccccc'
+    assert switcheroo('abababababababab') == 'babababababababa'
+    assert switcheroo('aaaaa') == 'bbbbb'
