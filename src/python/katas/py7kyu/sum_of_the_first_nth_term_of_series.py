079fa40eaa43bd04e228b744f12d7170327147c9
sigmanificient <edhyjox@gmail.com>
2022-07-04 23:05:33 +0200

    :white_check_mark: Added test 1st nth tern of series

--- src/python/katas/py7kyu/sum_of_the_first_nth_term_of_series.py
+++ src/python/katas/py7kyu/sum_of_the_first_nth_term_of_series.py
@@ -3,3 +3,9 @@
 
 def series_sum(n: int) -> str:
     return f"{sum(1/(1 + 3 * x) for x in range(n)):.2f}"
+
+
+def test_series():
+    assert series_sum(1) == "1.00"
+    assert series_sum(2) == "1.25"
+    assert series_sum(3) == "1.39"
