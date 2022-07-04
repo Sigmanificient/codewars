7a956712c7958e31e87d791b55ff1a4b48d51e5c
sigmanificient <edhyjox@gmail.com>
2022-07-04 22:01:06 +0200

    :sparkles: Added solution for sum dig pow

---
+++ src/python/katas/py6kyu/take_a_number_and_sum_its_digits_raised_to_the_consectuve_powers_and_eureka.py
@@ -0,0 +1,14 @@
+def sum_dig_pow(a: int, b: int) -> int:
+    return [
+        x for x in range(a, b + 1)
+        if sum(int(d) ** c for c, d in enumerate(str(x), start=1)) == x
+    ]
+
+
+def test_sum_dig_pow():
+    assert sum_dig_pow(1, 10) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    assert sum_dig_pow(1, 100) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+    assert sum_dig_pow(10, 89) == [89]
+    assert sum_dig_pow(10, 100) == [89]
+    assert sum_dig_pow(90, 100) == []
+    assert sum_dig_pow(89, 135) == [89, 135]
