4c43cd8159cee3e390f8001a55ea77b5d86627ac
sigmanificient <edhyjox@gmail.com>
2022-07-04 22:42:35 +0200

    :white_check_mark: Added test for count of >1 sum ?-

--- src/python/katas/py8kyu/count_of_positive_sum_of_negative.py
+++ src/python/katas/py8kyu/count_of_positive_sum_of_negative.py
@@ -18,3 +18,12 @@ def count_positives_sum_negatives(arr: List[int]) -> List[int]:
             c += 1
 
     return [c, r]
+
+
+def test_count_positives_sum_negatives():
+    assert count_positives_sum_negatives([]) == []
+    assert count_positives_sum_negatives([1]) == [1, 0]
+    assert count_positives_sum_negatives([-1]) == [0, -1]
+    assert count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]) == [10, -65]
+    assert count_positives_sum_negatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]) == [8, -50]
+    assert count_positives_sum_negatives([0, 0, 0, 0, 0, 0, 0, 0, 0]) == [0, 0]
