adf26708845ebbc8eeaf98b082bb15da2b7e3a3b
sigmanificient <edhyjox@gmail.com>
2022-07-04 22:48:58 +0200

    :white_check_mark: Added test for highest value

--- src/python/katas/py7kyu/which_string_is_worth_more.py
+++ src/python/katas/py7kyu/which_string_is_worth_more.py
@@ -3,3 +3,9 @@
 
 def highest_value(a: str, b: str) -> str:
     return a if sum(map(ord, a)) >= sum(map(ord, b)) else b
+
+
+def test_highest_value():
+    assert highest_value(
+        "AaBbCcXxYyZz0189", "KkLlMmNnOoPp4567"
+    ) == "KkLlMmNnOoPp4567"
