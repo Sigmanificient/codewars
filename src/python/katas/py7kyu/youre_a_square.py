7868e2331b3fed0f893011b4b9730a79ebedf2ae
sigmanificient <edhyjox@gmail.com>
2022-07-04 22:47:15 +0200

    :white_check_mark: Added test for is square

--- src/python/katas/py7kyu/youre_a_square.py
+++ src/python/katas/py7kyu/youre_a_square.py
@@ -5,3 +5,12 @@ from math import sqrt
 
 def is_square(n: int) -> bool:
     return sqrt(n).is_integer() if n >= 0 else False
+
+
+def test_is_square():
+    assert is_square(0)
+    assert is_square(4)
+    assert is_square(25)
+    assert not is_square(3)
+    assert not is_square(-1)
+    assert not is_square(26)
