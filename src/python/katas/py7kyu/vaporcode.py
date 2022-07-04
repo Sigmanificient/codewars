4ee958a63a0ca36d45688888ffcb391bb51142f8
sigmanificient <edhyjox@gmail.com>
2022-07-04 22:56:28 +0200

    :white_check_mark: Added test for vaporcode

--- src/python/katas/py7kyu/vaporcode.py
+++ src/python/katas/py7kyu/vaporcode.py
@@ -3,3 +3,13 @@
 
 def vaporcode(s: str) -> str:
     return '  '.join(s.upper().replace(' ', ''))
+
+
+def test_vapor_code():
+    assert vaporcode(
+        "Lets go to the movies"
+    ) == "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
+
+    assert vaporcode(
+        "Why isn't my code working?"
+    ) == "W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?"
