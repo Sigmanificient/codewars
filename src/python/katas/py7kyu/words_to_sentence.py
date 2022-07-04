0438177225b893dc8313a40643c6a03cb1b2d1fd
sigmanificient <edhyjox@gmail.com>
2022-07-04 22:48:06 +0200

    :white_check_mark: Added test for word to sentence

--- src/python/katas/py7kyu/words_to_sentence.py
+++ src/python/katas/py7kyu/words_to_sentence.py
@@ -5,3 +5,8 @@ from typing import List
 
 def words_to_sentence(words: List[str]) -> str:
     return ' '.join(words)
+
+
+def test_words_to_sentence():
+    assert words_to_sentence(['bacon', 'is', 'delicious']) == 'bacon is delicious'
+    assert words_to_sentence(['hello', 'world']) == 'hello world'
