2bef467d94aa7a16630c2785f889fd9315592225
sigmanificient <edhyjox@gmail.com>
2022-07-04 23:09:41 +0200

    :white_check_mark: Added test for split in parts

--- src/python/katas/py7kyu/split_in_parts.py
+++ src/python/katas/py7kyu/split_in_parts.py
@@ -3,3 +3,11 @@
 
 def split_in_parts(s: str, part_length: int) -> str:
     return ' '.join(s[i:i + part_length] for i in range(0, len(s), part_length))
+
+
+def test_split_in_parts():
+    assert split_in_parts(
+        "supercalifragilisticexpialidocious", 3
+    ) == "sup erc ali fra gil ist ice xpi ali doc iou s"
+    assert split_in_parts("HelloKata", 1) == "H e l l o K a t a"
+    assert split_in_parts("HelloKata", 9) == "HelloKata"
