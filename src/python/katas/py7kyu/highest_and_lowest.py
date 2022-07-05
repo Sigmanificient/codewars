a5f992895306e95e752c8f6370100acde73ca426
sigmanificient <edhyjox@gmail.com>
2022-07-05 22:57:05 +0200

    :white_check_mark: Added test for high & low

--- src/python/katas/py7kyu/highest_and_lowest.py
+++ src/python/katas/py7kyu/highest_and_lowest.py
@@ -6,3 +6,8 @@ from typing import Tuple
 def high_and_low(numbers_string: str) -> str:
     numbers: Tuple[int, ...] = tuple(map(int, numbers_string.split(' ')))
     return f"{max(numbers)} {min(numbers)}"
+
+
+def test_high_and_low():
+    assert high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4") == "42 -9"
+    assert high_and_low("1 2 3") == "3 1"
