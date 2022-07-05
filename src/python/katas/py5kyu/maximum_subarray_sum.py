5e38b6f315d5099aeb874bbf39dc32708ba0ae8f
sigmanificient <edhyjox@gmail.com>
2022-07-05 23:08:44 +0200

    :white_check_mark: Added test for max subarray sequence

--- src/python/katas/py5kyu/maximum_subarray_sum.py
+++ src/python/katas/py5kyu/maximum_subarray_sum.py
@@ -17,3 +17,8 @@ def max_sequence(arr: List[int]) -> int:
                 sub_max = s
 
     return sub_max
+
+
+def test_max_sequence():
+    assert max_sequence([]) == 0
+    assert max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6
