f54f0185d27abde2488d51a0974b16b116fae583
sigmanificient <edhyjox@gmail.com>
2022-07-05 22:58:22 +0200

    :white_check_mark: Added test for heron formula

--- src/python/katas/py7kyu/heron_formula.py
+++ src/python/katas/py7kyu/heron_formula.py
@@ -6,3 +6,8 @@ from math import sqrt
 def heron(a: int, b: int, c: int) -> float:
     s: float = (a + b + c) / 2
     return round(sqrt(s * (s - a) * (s - b) * (s - c)), 2)
+
+
+def test_heron():
+    assert heron(3, 4, 5) == 6
+    assert heron(5, 12, 13) == 30
