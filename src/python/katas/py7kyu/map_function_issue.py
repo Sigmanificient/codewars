5643a470b4e44b93b869b8883ba175bf427bfa9d
sigmanificient <edhyjox@gmail.com>
2022-07-05 22:50:17 +0200

    :white_check_mark: Added test for map some func

--- src/python/katas/py7kyu/map_function_issue.py
+++ src/python/katas/py7kyu/map_function_issue.py
@@ -15,3 +15,12 @@ def map(arr, somefunction):
         return 'array should contain only numbers'
 
     return [somefunction(item) for item in arr]
+
+
+def test_map():
+    assert map([], func) == []
+    assert map([1, 2, 3, '8'], func) == [False, True, False, True]
+    assert map([77, 84, 18, 43, 16, 70, 53], func) == [False, True, True, False, True, True, False]
+    assert map([1, 96, 37, 60, 7], 'test') == 'given argument is not a function'
+    assert map([72, 12, 30, 'q'], func) == 'array should contain only numbers'
+    assert map([9, 53], func) == [False, False]
