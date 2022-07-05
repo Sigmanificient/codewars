88bda0db3edfed4e793732f84cc10f6cf562397e
sigmanificient <edhyjox@gmail.com>
2022-07-05 22:54:46 +0200

    :white_check_mark: Added test for prev mul of 3

--- src/python/katas/py7kyu/previous_multiple_of_three.py
+++ src/python/katas/py7kyu/previous_multiple_of_three.py
@@ -8,3 +8,11 @@ def prev_mult_of_three(n: int) -> Optional[int]:
         n //= 10
 
     return n or None
+
+
+def test_prev_mult_of_three():
+    assert prev_mult_of_three(1) is None
+    assert prev_mult_of_three(25) is None
+    assert prev_mult_of_three(36) == 36
+    assert prev_mult_of_three(1244) == 12
+    assert prev_mult_of_three(952406) == 9
