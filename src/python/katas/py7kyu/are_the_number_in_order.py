36a246d000808a7c2f8ee6394192e8d22610c0f9
sigmanificient <edhyjox@gmail.com>
2022-07-05 11:55:57 +0200

    :heavy_plus_sign: Added solution for 'is asc order'

---
+++ src/python/katas/py7kyu/are_the_number_in_order.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def in_asc_order(arr: List[int]) -> bool:
+    return arr == sorted(arr)
+
+
+assert in_asc_order([1, 2])
+assert not in_asc_order([2, 1])
+assert in_asc_order([1, 2, 3])
+assert not in_asc_order([1, 3, 2])
+assert in_asc_order([1, 4, 13, 97, 508, 1047, 20058])
+assert not in_asc_order([56, 98, 123, 67, 742, 1024, 32, 90969])
