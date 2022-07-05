020d80492b9ba85d516204100f1d5d2ac7bd826d
sigmanificient <edhyjox@gmail.com>
2022-07-05 22:11:02 +0200

    :sparkles: Added solution for 'int -> ip'

---
+++ src/python/katas/py5kyu/int32_to_ipv4.py
@@ -0,0 +1,11 @@
+def int32_to_ip(int32: int) -> str:
+    r, d = divmod(int32, 256)
+    r, c = divmod(r, 256)
+    a, b = divmod(r, 256)
+    return f'{a}.{b}.{c}.{d}'
+
+
+def test_int32_to_ip():
+    assert int32_to_ip(0) == "0.0.0.0"
+    assert int32_to_ip(2154959208) == "128.114.17.104"
+    assert int32_to_ip(2149583361) == "128.32.10.1"
