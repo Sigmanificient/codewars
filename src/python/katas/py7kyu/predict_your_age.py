c85fc4d5a7c69c513d5c0c18650190f384aa205f
sigmanificient <edhyjox@gmail.com>
2022-07-05 22:56:13 +0200

    :white_check_mark: Added test for predict your age

--- src/python/katas/py7kyu/predict_your_age.py
+++ src/python/katas/py7kyu/predict_your_age.py
@@ -4,3 +4,9 @@ import math
 
 def predict_age(*ages: int) -> int:
     return math.sqrt(sum(age * age for age in ages)) // 2
+
+
+def test_predict_age():
+    assert predict_age(65, 60, 75, 55, 60, 63, 64, 45) == 86
+    assert predict_age(45, 55, 29, 34, 43, 65, 45, 62) == 68
+    assert predict_age(45, 55, 29, 34, 43, 65, 45, 62, 45) == 72
