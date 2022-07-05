291020c699779fdce145f9b2a61e5ff0864f6016
sigmanificient <edhyjox@gmail.com>
2022-07-05 22:39:53 +0200

    :sparkles: Added solution for 'luck check'

---
+++ src/python/katas/py5kyu/luck_check.py
@@ -0,0 +1,16 @@
+"""Kata url: https://www.codewars.com/kata/5314b3c6bb244a48ab00076c."""
+import pytest
+
+
+def luck_check(string: str) -> bool:
+    x = tuple(map(int, string))
+    h = len(string) // 2
+    return sum(x[:h]) == sum(x[::-1][:h])
+
+
+def test_luck_check():
+    assert luck_check('683179')
+    assert not luck_check('683000')
+
+    with pytest.raises(ValueError):
+        luck_check('6F43E8')
