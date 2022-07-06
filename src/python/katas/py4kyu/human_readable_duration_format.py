7949e086a150544d0f164d9e04d2a644fcd15468
sigmanificient <edhyjox@gmail.com>
2022-07-06 23:24:20 +0200

    :sparkles: Added solution for human_readable_time

---
+++ src/python/katas/py4kyu/human_readable_duration_format.py
@@ -0,0 +1,42 @@
+def format_duration(seconds: int) -> str:
+    if not seconds:
+        return 'now'
+
+    dvr = []
+    r = seconds
+    for k in (60, 60, 24, 365):
+        r, v = divmod(r, k)
+        dvr.append(v)
+
+    y, (d, h, m, s) = r, dvr[::-1]
+
+    display = [
+        (v, unit) for v, unit in zip(
+            (y, d, h, m, s),
+            ('year', 'day', 'hour', 'minute', 'second')
+        ) if v
+    ]
+
+    last, last_unit = display.pop()
+    suffix = f"{last} {last_unit}{'s' * (last > 1)}"
+
+    if not display:
+        return suffix
+
+    return ', '.join(
+        f'{v} {d}{"s" * (v > 1)}' for v, d in display
+    ) + ' and ' + suffix
+
+
+def test_format_duration():
+    assert format_duration(0) == 'now'
+    assert format_duration(1) == "1 second"
+    assert format_duration(62) == "1 minute and 2 seconds"
+    assert format_duration(120) == "2 minutes"
+    assert format_duration(3600) == "1 hour"
+    assert format_duration(3662), "1 hour == 1 minute and 2 seconds"
+
+    assert format_duration(99999) == "1 day, 3 hours, 46 minutes and 39 seconds"
+    assert format_duration(
+        1_000_000_000
+    ) == "31 years, 259 days, 1 hour, 46 minutes and 40 seconds"
