6149f19bfd94a3c200b38b7a051b813e893db815
sigmanificient <edhyjox@gmail.com>
2022-07-08 00:02:21 +0200

    :sparkles: Added solution for card suit

---
+++ src/python/katas/py8kyu/define_a_card_suit.py
@@ -0,0 +1,17 @@
+"""Kata url: https://www.codewars.com/kata/5a360620f28b82a711000047."""
+
+
+def define_suit(card):
+    return {
+        'C': 'clubs',
+        'S': 'spades',
+        'D': 'diamonds',
+        'H': 'hearts'
+    }[card[-1]]
+
+
+def test_define_suit():
+    assert define_suit('3C') == 'clubs'
+    assert define_suit('QS') == 'spades'
+    assert define_suit('9D') == 'diamonds'
+    assert define_suit('JH') == 'hearts'
