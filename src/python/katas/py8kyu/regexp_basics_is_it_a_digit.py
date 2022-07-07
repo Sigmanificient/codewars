67ff8e3d4b37329ffa1348eb31b444b53068b92f
sigmanificient <edhyjox@gmail.com>
2022-07-07 23:54:07 +0200

    :sparkles: Added solution for regexo basics

---
+++ src/python/katas/py8kyu/regexp_basics_is_it_a_digit.py
@@ -0,0 +1,14 @@
+"""Kata url: https://www.codewars.com/kata/567bf4f7ee34510f69000032."""
+import re
+
+
+def is_digit(n):
+    return bool(re.fullmatch(r'(\d)', n))
+
+
+def test_is_digit():
+    assert not is_digit("")
+    assert not is_digit(" ")
+    assert not is_digit("a")
+    assert not is_digit("a5")
+    assert is_digit("7")
