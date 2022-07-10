59d0334eceb440aa8a6530a6941c0dd6afb37ede
sigmanificient <edhyjox@gmail.com>
2022-07-10 22:57:08 +0000

    :sparkles: Added solution for binary tree compare

---
+++ src/python/katas/py6kyu/binary_tree_compare.py
@@ -0,0 +1,32 @@
+"""Kata url: https://www.codewars.com/kata/55847fcd3e7dadc9f800005f."""
+from typing import Optional
+
+
+class Node:
+    def __init__(self, val, left, right):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def compare(a: Optional[Node], b: Optional[Node]) -> bool:
+    if a is None:
+        return b is None
+
+    if b is None:
+        return False
+
+    return (
+        a.val == b.val
+        and compare(a.left, b.left)
+        and compare(a.right, b.right)
+    )
+
+
+def test_compare():
+    a_node = Node(1, None, None)
+    b_node = Node(1, None, None)
+    c_node = Node(2, None, None)
+
+    assert compare(a_node, b_node)
+    assert not compare(a_node, c_node)
