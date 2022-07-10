f949fe813086b7b37c2627b33e1964a2959a319f
sigmanificient <edhyjox@gmail.com>
2022-07-10 23:04:31 +0000

    :sparkles: Added solution for char code calculation

---
+++ src/python/katas/py7kyu/char_code_calculation.py
@@ -0,0 +1,14 @@
+"""Kata ur: https://www.codewars.com/kata/55847fcd3e7dadc9f800005f."""
+
+
+def calc(x: str) -> int:
+    t = ''.join(str(ord(c)) for c in x)
+    return sum(map(int, t)) - sum(map(int, t.replace('7', '1')))
+
+
+def test_calc():
+    assert calc('abcdef') == 6
+    assert calc('ifkhchlhfd') == 6
+    assert calc('aaaaaddddr') == 30
+    assert calc('jfmgklf8hglbe') == 6
+    assert calc('jaam') == 12
