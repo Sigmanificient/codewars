928f56c0071d73c3ee0ce2cfec71d7e56d125c51
sigmanificient <edhyjox@gmail.com>
2022-07-11 17:30:50 +0200

    :sparkles: Added solution for weird string case

---
+++ src/python/katas/py6kyu/weird_string_case.py
@@ -0,0 +1,13 @@
+def to_weird_case(string):
+    return ' '.join(
+        ''.join(
+            char.lower() if c % 2 else char.upper()
+            for c, char in enumerate(w)
+        ) for w in string.split(' ')
+    )
+
+
+def test_to_weird_case():
+    assert to_weird_case('This') == 'ThIs'
+    assert to_weird_case('is') == 'Is'
+    assert to_weird_case('This is a test') == 'ThIs Is A TeSt'
