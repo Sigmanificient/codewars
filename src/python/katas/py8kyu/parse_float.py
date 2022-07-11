621781319c2e79b1af487ad30edba25a48b26379
sigmanificient <edhyjox@gmail.com>
2022-07-11 23:07:07 +0200

    :bug: Fixed parse_float function

--- src/python/katas/py8kyu/parse_float.py
+++ src/python/katas/py8kyu/parse_float.py
@@ -1,17 +1,21 @@
 """Kata url: https://www.codewars.com/kata/57a386117cb1f31890000039."""
 
 
-from typing import Optional
+from typing import Optional, Union
 
 
-def parse_float(string: str) -> Optional[float]:
+def parse_float(string: Union[list, str]) -> Optional[float]:
     if isinstance(string, list):
         return None
 
     if '.' not in string:
         return None
 
-    int_part, dec_part = string.split('.')
+    int_part, dec_part, *left_over = string.split('.')
+
+    if left_over:
+        return None
+
     if int_part.isdigit() and dec_part.isdigit():
         return float(string)
 
