26c3a12c504a1c28bd71c0a390132415580cc250
sigmanificient <edhyjox@gmail.com>
2022-07-12 21:01:55 +0200

    :sparkles: Added solution for 'stop gninnips my sdrow'

---
+++ src/python/katas/py6kyu/stop_gninnips_my_sdrow.py
@@ -0,0 +1,17 @@
+"""Kata url: https://www.codewars.com/kata/5264d2b162488dc400000001."""
+
+
+def spin_words(sentence: str) -> str:
+    return ' '.join(
+        w if len(w) < 5 else w[::-1]
+        for w in sentence.split(' ')
+    )
+
+
+def test_spin_words():
+    assert spin_words("Welcome") == "emocleW"
+    assert spin_words("to") == "to"
+    assert spin_words("CodeWars") == "sraWedoC"
+    assert spin_words(
+        "This sentence is a sentence"
+    ) == "This ecnetnes is a ecnetnes"
