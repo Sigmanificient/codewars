73a076bc56f54be8d543d2290caab43ed389b468
sigmanificient <edhyjox@gmail.com>
2022-07-14 14:37:14 +0200

    :white_check_mark: Added unittest for previous solution

--- src/python/katas/beta_is_this_a_rectangle.py
+++ src/python/katas/beta_is_this_a_rectangle.py
@@ -44,3 +44,90 @@ def is_this_a_rectangle(p, t=0.05):
         abs(ab - cd) <= t
         and abs(bc - ad) <= t
     )
+
+def test_is_this_a_rectangle():
+    assert is_this_a_rectangle([[0, 0], [1, 0], [1, 1], [0, 1], [0, 0]])
+    assert is_this_a_rectangle([[0, 0], [2, 0], [2, 2], [0, 2], [0, 0]])
+    assert is_this_a_rectangle([[0, 0], [-1, 0], [-1, -1], [0, -1], [0, 0]])
+
+    assert not is_this_a_rectangle([[0, 0], [1, 0], [1, 1], [0, 1], [1, 0]])
+    assert not is_this_a_rectangle([[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])
+    assert not is_this_a_rectangle([[0, 0], [0, 0], [0, 1], [0, 1], [0, 0]])
+    assert not is_this_a_rectangle([[0, 1], [1, 1], [1, 1], [0, 1], [0, 1]])
+    assert not is_this_a_rectangle([[1, 1], [1, 1], [1, 1], [1, 1], [1, 1]])
+
+    assert not is_this_a_rectangle([[0, 0], [2, 0], [1, 1], [0, 1], [0, 0]])
+    assert not is_this_a_rectangle([[0, 0], [2, 0], [1, 1], [0, 2], [0, 0]])
+    assert not is_this_a_rectangle([[-1, 0], [1, 0], [1, 1], [0, 1], [-1, 0]])
+    assert not is_this_a_rectangle([[0, 0], [1, 0], [2, 2], [0, 1], [0, 0]])
+    assert not is_this_a_rectangle([[0, 0], [1, 0], [1, 1], [0, 2], [0, 0]])
+
+    assert is_this_a_rectangle([[0, 0], [1000000, 0], [1000000, 1], [0, 1], [0, 0]])
+    assert is_this_a_rectangle([[0, 0], [1000000, 0], [1000000, -1], [0, -1], [0, 0]])
+    assert is_this_a_rectangle([[0, 0], [1, 0], [1, 1000000], [0, 1000000], [0, 0]])
+    assert is_this_a_rectangle([[0, 0], [1, 0], [1, -1000000], [0, -1000000], [0, 0]])
+    assert is_this_a_rectangle([[1, -1000000], [2, -1000000], [2, 1], [1, 1], [1, -1000000]])
+
+    assert is_this_a_rectangle([[-1, -1], [1, -1], [1, 1], [-1, 1], [-1, -1]])
+    assert is_this_a_rectangle([[-1, -100], [1, -100], [1, 1], [-1, 1], [-1, -100]])
+    assert is_this_a_rectangle([[-2, -2], [2, -2], [2, 2], [-2, 2], [-2, -2]])
+    assert is_this_a_rectangle([[-1000, -1], [1, -1], [1, 1], [-1000, 1], [-1000, -1]])
+    assert is_this_a_rectangle([[-12345, -12345], [12345, -12345], [12345, 12345], [-12345, 12345], [-12345, -12345]])
+
+    assert is_this_a_rectangle([[0, 0], [1, 0], [1, 1], [0, 1], [0, 0]])
+    assert is_this_a_rectangle([[0, 0], [1, 0], [1, -1], [0, -1], [0, 0]])
+    assert is_this_a_rectangle([[0, 0], [-1, 0], [-1, -1], [0, -1], [0, 0]])
+    assert is_this_a_rectangle([[0, 0], [-1, 0], [-1, 1], [0, 1], [0, 0]])
+    assert is_this_a_rectangle([[0, 0], [0, 1], [-1, 1], [-1, 0], [0, 0]])
+    assert is_this_a_rectangle([[0, 0], [0, 1], [1, 1], [1, 0], [0, 0]])
+    assert is_this_a_rectangle([[0, 0], [0, -1], [1, -1], [1, 0], [0, 0]])
+    assert is_this_a_rectangle([[0, 0], [0, -1], [-1, -1], [-1, 0], [0, 0]])
+
+    assert not is_this_a_rectangle([[0, 0], [2, 0], [1, 1], [0, 1], [0, 0]])
+    assert not is_this_a_rectangle([[0, 0], [1, 0], [1, 1], [0, 2], [0, 0]])
+
+    assert not is_this_a_rectangle([[0, 0], [1, 0], [2, 2], [1, 2], [0, 0]])
+    assert not is_this_a_rectangle([[2, 2], [1, 0], [-2, -2], [-1, 0], [2, 2]])
+
+    assert not is_this_a_rectangle([[0, 0], [2, 0], [0, 2], [2, 2], [0, 0]])
+    assert not is_this_a_rectangle([[2, 2], [-2, -2], [2, -2], [-2, 2], [2, 2]])
+    assert not is_this_a_rectangle([[0, 2], [2, 0], [2, 2], [0, 0], [0, 2]])
+    assert not is_this_a_rectangle([[-1, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]])
+
+    assert not is_this_a_rectangle([[0, 0], [2, 0], [2, 2], [2, 0], [0, 0]])
+    assert not is_this_a_rectangle([[0, 0], [-2, 0], [-2, -2], [-2, 0], [0, 0]])
+
+    assert not is_this_a_rectangle([[0, 0], [2, 0], [2, 0], [2, 0], [0, 0]])
+    assert not is_this_a_rectangle([[0, 0], [2, 0], [4, 0], [2, 0], [0, 0]])
+    assert not is_this_a_rectangle([[0, 0], [0, 2], [0, 2], [0, 2], [0, 0]])
+    assert not is_this_a_rectangle([[0, 0], [0, 2], [0, 4], [0, 2], [0, 0]])
+
+    assert is_this_a_rectangle([[0, 0], [2, 2], [0, 4], [-2, 2], [0, 0]])
+    assert is_this_a_rectangle([[-1, 0], [0, -1], [1, 0], [0, 1], [-1, 0]])
+
+    assert is_this_a_rectangle([[0, 0], [2, 2], [0, 4], [-2, 2], [0, 0]], 0)
+    assert is_this_a_rectangle([[-1, 0], [0, -1], [1, 0], [0, 1], [-1, 0]], 0)
+    assert not is_this_a_rectangle([[0.001, 0], [2, 2], [0, 4], [-2, 2], [0, 0]], 0)
+    assert not is_this_a_rectangle([[-1, 0], [0, -1], [1, 0.001], [0, 1], [-1, 0]], 0)
+
+    assert is_this_a_rectangle([[0, 0], [2, 2], [0, 4], [-2, 2], [0, 0]])
+    assert is_this_a_rectangle([[-1, 0], [0, -1], [1, 0], [0, 1], [-1, 0]])
+    assert is_this_a_rectangle([[0.001, 0], [2, 2], [0, 4], [-2, 2], [0, 0]])
+    assert is_this_a_rectangle([[-1, 0], [0, -1], [1, 0.001], [0, 1], [-1, 0]])
+    assert not is_this_a_rectangle([[0, 0], [1, 0], [1, 1], [0, 1], [-0.06, 0]])
+    assert not is_this_a_rectangle([[0, 0], [1, 0], [1, 1.06], [0, 1], [0, 0]])
+
+    assert is_this_a_rectangle([[0, 0], [2, 2], [0, 4], [-2, 2], [0, 0]], 0.5)
+    assert is_this_a_rectangle([[-1, 0], [0, -1], [1, 0], [0, 1], [-1, 0]], 0.5)
+    assert is_this_a_rectangle([[0.001, 0], [2, 2], [0, 4], [-2, 2], [0, 0]], 0.5)
+    assert is_this_a_rectangle([[-1, 0], [0, -1], [1, 0.001], [0, 1], [-1, 0]], 0.5)
+    assert is_this_a_rectangle([[0, 0], [1, 0], [1, 1], [0, 1], [-0.06, 0]], 0.5)
+    assert is_this_a_rectangle([[0, 0], [1, 0], [1, 1.06], [0, 1], [0, 0]], 0.5)
+    assert not is_this_a_rectangle([[0, 0], [1, 0], [1, 1], [0, 1], [-0.6, 0]], 0.5)
+    assert not is_this_a_rectangle([[0, 0], [1, 0], [1, 1.6], [0, 1], [0, 0]], 0.5)
+
+    assert is_this_a_rectangle([[0, 0], [2, 2], [0, 4], [-2, 2], [0, 0]], -1)
+    assert not is_this_a_rectangle([[-1, 0], [0, -1], [1, 0.001], [0, 1], [-1, 0]], -1)
+    assert is_this_a_rectangle([[0, 0], [2, 2], [0, 4], [-2, 2], [0, 0]], 0.7)
+    assert is_this_a_rectangle([[0, 0], [1, 0], [1, 1], [0, 1], [-0.06, 0]], 0.7)
+    assert not is_this_a_rectangle([[0, 0], [1, 0], [1, 1.6], [0, 1], [0, 0]], 0.7)
