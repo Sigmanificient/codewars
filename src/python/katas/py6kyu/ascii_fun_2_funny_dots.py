dac894c25eacc6d02922d683da33e40292358fc2
sigmanificient <edhyjox@gmail.com>
2022-07-15 14:38:07 +0200

    :memo: Added solution for funny dots

---
+++ src/python/katas/py6kyu/ascii_fun_2_funny_dots.py
@@ -0,0 +1,22 @@
+"""Kata url: https://www.codewars.com/kata/59098c39d8d24d12b6000020."""
+
+
+def dot(n: int, m: int) -> str:
+    lines = ['|' + ' o |' * n, '+' + '---+' * n]
+    return lines[-1] + '\n' + '\n'.join(lines * m)
+
+
+def test_dot():
+    assert dot(1, 1) == (
+        "+---+\n"
+        "| o |\n"
+        "+---+"
+    )
+
+    assert dot(3, 2) == (
+        "+---+---+---+\n"
+        "| o | o | o |\n"
+        "+---+---+---+\n"
+        "| o | o | o |\n"
+        "+---+---+---+"
+    )
