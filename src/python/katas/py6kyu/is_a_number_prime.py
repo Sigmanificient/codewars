ed4fc5d2ffd84cbd8c3a409a418a0a664bc0da3a
sigmanificient <edhyjox@gmail.com>
2022-07-19 00:00:35 +0200

    :white_check_mark: Added test for is_an_number_prime

--- src/python/katas/py6kyu/is_a_number_prime.py
+++ src/python/katas/py6kyu/is_a_number_prime.py
@@ -11,3 +11,27 @@ def is_prime(num):
         num % n
         for n in range(3, int(sqrt(num) + 1), 2)
     )
+
+
+def test_is_prime():
+    assert not is_prime(0)
+    assert not is_prime(1)
+    assert is_prime(2)
+    assert is_prime(73)
+    assert not is_prime(75)
+    assert not is_prime(-1)
+
+    assert is_prime(3)
+    assert is_prime(5)
+    assert is_prime(7)
+    assert is_prime(41)
+    assert is_prime(5099)
+
+    assert not is_prime(4)
+    assert not is_prime(6)
+    assert not is_prime(8)
+    assert not is_prime(9)
+    assert not is_prime(45)
+    assert not is_prime(-5)
+    assert not is_prime(-8)
+    assert not is_prime(-41)
