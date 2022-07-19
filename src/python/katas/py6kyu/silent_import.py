f4d32a3e87c93844de0947f090f6bf31a6d43b3d
sigmanificient <edhyjox@gmail.com>
2022-07-19 15:24:00 +0200

    :sparkles: Added solution for silent import

---
+++ src/python/katas/py6kyu/silent_import.py
@@ -0,0 +1,12 @@
+"""Kata url: https://www.codewars.com/kata/5bc5c064eba26ef6ed000158."""
+
+
+def silent_thief(module_name: str) -> str:
+    x = lambda o, n: getattr(o, n.center(len(n) + 4, '_'))
+    return x(x(breakpoint, 'self'), 'imp' + 'ort')(module_name)
+
+
+def test_silent_thief():
+    math = silent_thief("math")
+    import math as also_math
+    assert math == also_math
