a0d02520de5aed861d15d3404b727dd6c3748630
sigmanificient <edhyjox@gmail.com>
2022-07-20 21:56:04 +0200

    :sparkles: Added solution for count vegetables

---
+++ src/python/katas/py7kyu/help_sukui_count_his_vegetables.py
@@ -0,0 +1,19 @@
+"""Kata url: https://www.codewars.com/kata/56ff1667cc08cacf4b00171."""
+from collections import Counter
+
+
+VEGETABLES = [
+    "cabbage", "carrot", "celery", "cucumber", "mushroom",
+    "onion", "pepper", "potato", "tofu", "turnip"
+]
+
+
+def count_vegetables(string):
+    return sorted(
+        [
+            (v, k)
+            for k, v in Counter(string.split()).items()
+            if k in VEGETABLES
+        ],
+        reverse=True
+    )
