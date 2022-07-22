ab28a1f491afa9456b40228b890d350475878101
sigmanificient <edhyjox@gmail.com>
2022-07-22 22:50:42 +0200

    :sparkles: Added solution for camelcase method

---
+++ src/python/katas/py6kyu/camelcase_method.py
@@ -0,0 +1,13 @@
+"""Kata url: https://www.codewars.com/kata/587731fda577b3d1b0001196."""
+
+
+def camel_case(string: str) -> str:
+    return ''.join(w.capitalize() for w in string.split(' '))
+
+
+def test_camel_case():
+    assert camel_case("") == ""
+    assert camel_case("test case") == "TestCase"
+    assert camel_case("camel case method") == "CamelCaseMethod"
+    assert camel_case("say hello ") == "SayHello"
+    assert camel_case(" camel case word") == "CamelCaseWord"
