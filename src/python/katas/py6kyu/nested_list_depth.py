a616df58624d4fdbcddf9785e12f2307dc3e2d3e
sigmanificient <edhyjox@gmail.com>
2022-07-22 22:53:15 +0200

    :sparkles: Added solution for nested list depth

---
+++ src/python/katas/py6kyu/nested_list_depth.py
@@ -0,0 +1,15 @@
+"""Kata url: https://www.codewars.com/kata/56b3b9c7a6df24cf8c00000e."""
+from typing import List, Union
+
+
+RecursiveList = List[Union[bool, str, int, 'RecursiveList']]
+
+
+def list_depth(lst: RecursiveList, d: int = 0) -> int:
+    if not isinstance(lst, list):
+        return d
+
+    return (
+        max(list_depth(item, d + 1) for item in lst)
+        if lst else d + 1
+    )
