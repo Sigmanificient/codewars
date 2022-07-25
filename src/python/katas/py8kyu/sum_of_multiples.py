7d1fe6309f602525d0d6be85a3c50998573d7662
sigmanificient <edhyjox@gmail.com>
2022-07-25 23:25:44 +0200

    :sparkles: Added solution for sum of multiple

---
+++ src/python/katas/py8kyu/sum_of_multiples.py
@@ -0,0 +1,22 @@
+"""Kata url: https://www.codewars.com/kata/57241e0f440cd279b5000829."""
+
+
+def sum_mul(n: int, m: int) -> int:
+    if n <= 0 or m <= 0:
+        return 'INVALID'
+
+    return 0 if m < n else sum(range(n, m, n))
+
+
+def test_sum_mul():
+    assert sum_mul(4, 123) == 1860
+    assert sum_mul(123, 4567) == 86469
+    assert sum_mul(2, 10) == 20
+    assert sum_mul(2, 2) == 0
+    assert sum_mul(7, 7) == 0
+    assert sum_mul(7, 2) == 0
+    assert sum_mul(21, 3) == 0
+    assert sum_mul(0, 2) == 'INVALID'
+    assert sum_mul(2, 0) == 'INVALID'
+    assert sum_mul(4, -7) == 'INVALID'
+    assert sum_mul(-7, 4) == 'INVALID'
