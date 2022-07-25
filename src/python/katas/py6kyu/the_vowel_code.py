cc83b3133360539c72453e4721202d29b9469226
sigmanificient <edhyjox@gmail.com>
2022-07-25 22:40:20 +0200

    :sparkles: Added solution for the vowel code

---
+++ src/python/katas/py6kyu/the_vowel_code.py
@@ -0,0 +1,17 @@
+ENCODE = str.maketrans('aeiou', '12345')
+DECODE = str.maketrans('12345', 'aeiou')
+
+
+def encode(st: str) -> str:
+    return st.translate(ENCODE)
+
+
+def decode(st: str) -> str:
+    return st.translate(DECODE)
+
+
+def test_code():
+    assert encode('hello') == 'h2ll4'
+    assert encode('How are you today?') == 'H4w 1r2 y45 t4d1y?'
+    assert encode('This is an encoding test.') == 'Th3s 3s 1n 2nc4d3ng t2st.'
+    assert decode('h2ll4') == 'hello'
