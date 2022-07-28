566cf2fc10278f319283e0d01c75e2b474712d3f
sigmanificient <edhyjox@gmail.com>
2022-07-29 00:34:49 +0200

    :sparkles: Added solution for async requests

---
+++ src/python/katas/beta/async_requests.py
@@ -0,0 +1,31 @@
+"""Kata url: https://www.codewars.com/kata/62c702489012c30017ded374"""
+import asyncio
+from copy import copy
+from itertools import cycle
+from random import randint
+from string import ascii_letters, digits
+
+_cycler = cycle(ascii_letters)
+
+
+async def _send_request():
+    return (res := next(_cycler)) and await asyncio.sleep(0.2) or res
+
+
+async def request_manager(n: int) -> str:
+    return ''.join(await asyncio.gather(*(_send_request() for _ in range(n))))
+
+
+def test_request_manager():
+    _cycler_copy = copy(_cycler)
+
+    def run_it(n: int) -> str:
+        try:
+            return asyncio.run(asyncio.wait_for(request_manager(n), 0.3))
+        except asyncio.TimeoutError:
+            return "Execution took longer than 1.5 sec"
+
+    for _ in range(5):
+        rand = randint(14, 300)
+        s = ''.join(next(_cycler_copy) for i in range(rand))
+        assert run_it(rand) == s
