8429c1a5ab31e96133974e282b1d7b9021f1dc55
sigmanificient <edhyjox@gmail.com>
2022-07-28 19:46:16 +0200

    :sparkles: A3d s6n for w2d a a10n :D

---
+++ src/python/katas/py6kyu/word_a_a10n.py
@@ -0,0 +1,29 @@
+"""Kata url: https://www.codewars.com/kata/5375f921003bf62192000746."""
+
+
+def abbreviate(s: str) -> str:
+    out = word = ''
+    m = len(s)
+
+    for c, char in enumerate(f'{s}.'):
+        if char.isalpha():
+            word += char
+            continue
+
+        if len(word) > 3:
+            word = f'{word[0]}{len(word) - 2}{word[-1]}'
+
+        out += word
+        if c != m:
+            out += char
+
+        word = ''
+
+    return out
+
+
+def test_abbreviate():
+    assert abbreviate("internationalization") == "i18n"
+    assert abbreviate("accessibility") == "a11y"
+    assert abbreviate("Accessibility") == "A11y"
+    assert abbreviate("elephant-ride") == "e6t-r2e"
