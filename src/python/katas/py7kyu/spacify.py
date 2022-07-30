87d9e3d6740cea7ae0d8647917677c08673a7661
sigmanificient <edhyjox@gmail.com>
2022-07-30 15:46:38 +0200

    :sparkles: Added solution for spacify

---
+++ src/python/katas/py7kyu/spacify.py
@@ -0,0 +1,14 @@
+"""Kata url: https://www.codewars.com/kata/62dcbe87f4ac96005f052962."""
+
+
+def spacify(string: str) -> str:
+    return ' '.join(string)
+
+
+def test_spacify():
+    assert spacify("") == ""
+    assert spacify("a") == "a"
+
+    assert spacify("hello world") == "h e l l o   w o r l d"
+    assert spacify("12345") == "1 2 3 4 5"
+    assert spacify("Pippi") == "P i p p i"
