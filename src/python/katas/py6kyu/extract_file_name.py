a7c7cbabaea563c94754a67c163beb19f7c0425a
sigmanificient <edhyjox@gmail.com>
2022-07-30 16:55:28 +0200

    :sparkles: Added solution for extract file name

---
+++ src/python/katas/py6kyu/extract_file_name.py
@@ -0,0 +1,21 @@
+"""Kata url: https://www.codewars.com/kata/597770e98b4b340e5b000071."""
+import re
+
+FILE_PATTERN = r'(\d+)_(?P<F>([^\.]+.[^\.]+))'
+
+
+class FileNameExtractor:
+
+    @staticmethod
+    def extract_file_name(dirty_file_name: str) -> str:
+        return re.match(FILE_PATTERN, dirty_file_name)['F']
+
+
+def test_filename_extractor():
+    assert FileNameExtractor.extract_file_name(
+        "1231231223123131_FILE_NAME.EXTENSION.OTHEREXTENSION"
+    ) == "FILE_NAME.EXTENSION"
+
+    assert FileNameExtractor.extract_file_name(
+        "1_FILE_NAME.EXTENSION.OTHEREXTENSIONadasdassdassds34"
+    ) == "FILE_NAME.EXTENSION"
