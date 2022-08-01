91e6054b2b1c8e83568215b0e12e0278d275ede1
sigmanificient <edhyjox@gmail.com>
2022-08-01 23:30:36 +0200

    :sparkles: Added solution for hellphabet

---
+++ src/python/katas/py6kyu/hellphabet_can_you_give_the_alphabet.py
@@ -0,0 +1,6 @@
+"""Kata url: https://www.codewars.com/kata/62dcbe87f4ac96005f052962."""
+ALPHABET = str().join(x for x in sorted(set(str().join(dir(int)))) if x.isalpha())
+
+
+def test_alphabet():
+    assert ALPHABET == 'abcdefghijklmnopqrstuvwxyz'
