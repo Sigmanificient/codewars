30c8eb88cd437f9e8bc523d3848620413a91137b
sigmanificient <edhyjox@gmail.com>
2022-08-01 23:23:58 +0200

    :sparkles: Added solution for contamination string

---
+++ src/python/katas/py8kyu/contamination_1_string.py
@@ -0,0 +1,13 @@
+"""Kata url: https://www.codewars.com/kata/596fba44963025c878000039."""
+
+
+def contamination(text: str, char: str) -> str:
+    return char * len(text) if char else ''
+
+
+def test_contamination():
+    assert contamination("abc", "z") == "zzz"
+    assert contamination("", "z") == ""
+    assert contamination("abc", "") == ""
+    assert contamination("_3ebzgh4", "&") == "&&&&&&&&"
+    assert contamination("//case", " ") == "      "
