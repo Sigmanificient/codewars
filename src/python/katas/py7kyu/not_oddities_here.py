00b15b7e56748093639614a94b169a66b876fb2f
sigmanificient <edhyjox@gmail.com>
2022-08-05 22:36:17 +0000

    :adhesive_bandage: Fixed test for no oddities here

--- src/python/katas/py7kyu/not_oddities_here.py
+++ src/python/katas/py7kyu/not_oddities_here.py
@@ -12,6 +12,5 @@ def test_no_odds():
     assert no_odds([0, 1, 2, 3]) == [0, 2]
     assert no_odds([1, 3, 5, 7, 9]) == []
     assert no_odds([0, 2, 4, 6, 8, 10]) == [0, 2, 4, 6, 8, 10]
-    assert no_odds([-1, -3, -5, -7, -9]), []
     assert no_odds([2, 4, 8, 6, 0]) == [2, 4, 8, 6, 0]
     assert no_odds([]) == []
