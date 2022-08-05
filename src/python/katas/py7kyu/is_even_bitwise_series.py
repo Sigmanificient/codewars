936e6a97292665738daaecc37ad9bcc1825c2398
sigmanificient <edhyjox@gmail.com>
2022-08-05 22:18:06 +0000

    :white_check_mark: Added 2 missing tests

--- src/python/katas/py7kyu/is_even_bitwise_series.py
+++ src/python/katas/py7kyu/is_even_bitwise_series.py
@@ -3,3 +3,12 @@
 
 def is_even(n: int) -> bool:
     return n == ((n >> 1) * 2)
+
+
+def test_is_even():
+    assert is_even(2)
+    assert not is_even(3)
+    assert is_even(14)
+    assert not is_even(15)
+    assert is_even(26)
+    assert not is_even(27)
--- src/python/katas/py7kyu/remove_duplicate_words.py
+++ src/python/katas/py7kyu/remove_duplicate_words.py
@@ -5,3 +5,12 @@ from typing import List
 def remove_duplicate_words(s: str) -> str:
     words: List[str] = s.split()
     return " ".join(sorted(set(words), key=words.index))
+
+
+def test_remove_duplicate_words():
+    assert remove_duplicate_words(
+        "alpha beta beta gamma gamma gamma delta "
+        "alpha beta beta gamma gamma gamma delta"
+    ) == "alpha beta gamma delta"
+
+    assert remove_duplicate_words("my cat is my cat fat") == "my cat is fat"
