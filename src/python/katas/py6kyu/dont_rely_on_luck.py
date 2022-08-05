957ec9c1ff54221e4edf98e6b827b830af149297
sigmanificient <edhyjox@gmail.com>
2022-08-05 21:09:33 +0000

    :bug: Fix don't rely on luck seed bug

--- src/python/katas/py6kyu/dont_rely_on_luck.py
+++ src/python/katas/py6kyu/dont_rely_on_luck.py
@@ -5,8 +5,8 @@ from random import randint, seed
 seed(0)
 guess = randint(1, 100)
 seed(0)
+lucky_number = randint(1, 100)
 
 
 def test_solution():
-    lucky_number = randint(1, 100)
     assert guess == lucky_number
