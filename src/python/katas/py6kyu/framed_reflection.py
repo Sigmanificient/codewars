5c85d228de402210a0795812e5042d6e0f97a020
sigmanificient <edhyjox@gmail.com>
2022-08-06 08:13:55 +0200

    :sparkles: Added solution for framed reflection

---
+++ src/python/katas/py6kyu/framed_reflection.py
@@ -0,0 +1,21 @@
+def mirror(text: str) -> str:
+    lines = text[::-1].split(' ')[::-1]
+    length = len(max(lines, key=len))
+
+    content = '\n'.join(
+        f"* {line:<{length}} *"
+        for line in lines if line
+    )
+
+    deco = '*' * (length + 4)
+    return f'{deco}\n{content}\n{deco}'
+
+
+def test_mirror():
+    assert mirror("Hello World") == "*********\n* olleH *\n* dlroW *\n*********"
+    assert mirror("Codewars") == "************\n* srawedoC *\n************"
+    assert mirror("a  b    c") == '*****\n* a *\n* b *\n* c *\n*****'
+    assert mirror("emosewA !ataK") == (
+        "***********\n* Awesome *\n* Kata!   *\n***********"
+    )
+
