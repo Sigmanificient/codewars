8e84ef22205cba8f3308126724e5dc8d75cfc423
sigmanificient <edhyjox@gmail.com>
2022-08-12 19:41:33 +0200

    :sparkles: Added solution for permute a palindrome

---
+++ src/python/katas/py6kyu/permute_a_palindrome.py
@@ -0,0 +1,30 @@
+"""Kata url: https://www.codewars.com/kata/58ae6ae22c3aaafc58000079"""
+
+
+def permute_a_palindrome(word: str) -> bool:
+    counts = [word.count(c) for c in set(word)]
+    max_odd = len(word) % 2
+
+    for count in counts:
+        odd = count % 2
+        if not odd:
+            continue
+
+        if not max_odd:
+            return False
+
+        max_odd -= 1
+
+    return True
+
+
+def test_permute_a_palindrome():
+    assert permute_a_palindrome("a")
+    assert permute_a_palindrome("aa")
+    assert permute_a_palindrome("baa")
+    assert permute_a_palindrome("aab")
+    assert not permute_a_palindrome("baabcd")
+    assert not permute_a_palindrome("racecars")
+
+    assert not permute_a_palindrome("abcdefghba")
+    assert permute_a_palindrome("")
