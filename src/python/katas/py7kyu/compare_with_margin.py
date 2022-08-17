59e655fe5ed72762bc89f78bb3095d4d991b7343
sigmanificient <edhyjox@gmail.com>
2022-08-17 23:13:16 +0200

    :sparkles: Added solution for compare with margin

---
+++ src/python/katas/py7kyu/compare_with_margin.py
@@ -0,0 +1,17 @@
+"""Kata url: https://www.codewars.com/kata/56453a12fcee9a6c4700009c."""
+
+
+def close_compare(a: int, b: int, margin: float = 0):
+    return (a - b - margin > 0) - (b - a - margin > 0)
+
+
+def test_close_compare():
+    assert close_compare(4, 5) == -1
+    assert close_compare(5, 5) == 0
+    assert close_compare(6, 5) == 1
+
+    assert close_compare(2, 5, 3) == 0
+    assert close_compare(5, 5, 3) == 0
+    assert close_compare(8, 5, 3) == 0
+    assert close_compare(8.1, 5, 3) == 1
+    assert close_compare(1.99, 5, 3) == -1
