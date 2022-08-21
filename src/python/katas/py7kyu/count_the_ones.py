c1f3762c6cd09851232dad5448a033adfcc60e01
sigmanificient <edhyjox@gmail.com>
2022-08-21 23:23:04 +0200

    :sparkles: Added solution for count 1s

---
+++ src/python/katas/py7kyu/count_the_ones.py
@@ -0,0 +1,14 @@
+"""Kata url: https://www.codewars.com/kata/5519e930cd82ff8a9a000216."""
+
+
+def hamming_weight(x: str) -> int:
+    return f'{x:b}'.count('1')
+
+
+def test_hamming_weight():
+    assert hamming_weight(0) == 0
+    assert hamming_weight(1) == 1
+    assert hamming_weight(2) == 1
+    assert hamming_weight(10) == 2
+    assert hamming_weight(21) == 3
+    assert hamming_weight(2048) == 1
