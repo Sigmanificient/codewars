e9735d4a7bd6d7530b5499cd92e54a3bef559b1f
sigmanificient <edhyjox@gmail.com>
2022-08-30 15:37:39 +0200

    :sparkles: Added solution for hanoi record

---
+++ src/python/katas/py6kyu/hanoi_record.py
@@ -0,0 +1,14 @@
+"""https://www.codewars.com/kata/534eb5ad704a49dcfa000ba6."""
+
+
+def hanoi(disks: int) -> int:
+    return 2 ** disks - 1
+
+
+def test_hanoi():
+    assert hanoi(1) == 1
+    assert hanoi(2) == 3
+    assert hanoi(3) == 7
+    assert hanoi(5) == 31
+    assert hanoi(10) == 1023
+    assert hanoi(20) == 1048575
