58430ab54755694cdf82bfd2c0a423b220418080
sigmanificient <edhyjox@gmail.com>
2022-09-09 09:25:20 +0200

    :sparkles: Added solution for js? filter

---
+++ src/python/katas/py7kyu/javascript_filter.py
@@ -0,0 +1,20 @@
+def search_names(logins):
+    return list(filter(lambda x: x[0].endswith('_'), logins))
+
+
+def test_search_names():
+    a = [["foo", "foo@foo.com"], ["bar_", "bar@bar.com"]]
+    b = [["bar_", "bar@bar.com"]]
+    assert search_names(a) == b
+
+    a = [["foobar_", "foo@foo.com"], ["bar_", "bar@bar.com"]]
+    b = [["foobar_", "foo@foo.com"], ["bar_", "bar@bar.com"]]
+    assert search_names(a) == b
+
+    a = [["foo", "foo@foo.com"], ["bar", "bar@bar.com"]]
+    b = []
+    assert search_names(a) == b
+
+    import inspect
+
+    assert "filter" in inspect.getsource(search_names)
