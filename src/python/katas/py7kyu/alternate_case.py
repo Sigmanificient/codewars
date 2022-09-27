72940a10184f4976f396c04f5cf92ddacfde92dc
sigmanificient <edhyjox@gmail.com>
2022-09-27 17:54:00 +0100

    :white_check_mark: Fix unittests

--- src/python/katas/py7kyu/alternate_case.py
+++ src/python/katas/py7kyu/alternate_case.py
@@ -3,11 +3,11 @@ alternate_case = str.swapcase
 
 
 def test_alternate_case():
-    assert alternate_case("ABC"), "abc"
-    assert alternate_case(""), ""
-    assert alternate_case(" "), " "
-    assert alternate_case("Hello World"), "hELLO wORLD"
-    assert alternate_case("cODEwARS"), "CodeWars"
+    assert alternate_case("ABC") == "abc"
+    assert alternate_case("") == ""
+    assert alternate_case(" ") == " "
+    assert alternate_case("Hello World") == "hELLO wORLD"
+    assert alternate_case("cODEwARS") == "CodeWars"
     assert alternate_case(
         "i LIKE MAKING KATAS VERY MUCH"
     ) == "I like making katas very much"
