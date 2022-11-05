b91ccef2ca8c1d7a60698aeae83d1155da1eeba8
Sigmanificient <edhyjox@gmail.com>
2022-11-06 00:27:09 +0100

    :sparkles: Add solution for find fib last digit (hard)

---
+++ src/python/katas/py6kyu/find_last_fib_digit_hard.py
@@ -0,0 +1,26 @@
+"""Kata url: https://www.codewars.com/kata/56b7771481290cc283000f28."""
+
+
+def last_fib_digit(n):
+    if not n % 60:
+        return 0
+
+    a, b = 1, 1
+
+    for i in range((n % 60) - 1):
+        a, b = b, (a + b) % 10
+
+    return a
+
+
+def test_last_fib_digit():
+    assert last_fib_digit(1) == 1
+    assert last_fib_digit(21) == 6
+    assert last_fib_digit(302) == 1
+    assert last_fib_digit(4003) == 7
+    assert last_fib_digit(50004) == 8
+    assert last_fib_digit(600005) == 5
+    assert last_fib_digit(7000006) == 3
+    assert last_fib_digit(80000007) == 8
+    assert last_fib_digit(900000008) == 1
+    assert last_fib_digit(1000000009) == 9
