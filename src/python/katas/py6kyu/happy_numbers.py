9619ecf9747729b6ce438fa05d2d678d87d9fcca
Sigmanificient <edhyjox@gmail.com>
2022-11-10 00:23:46 +0100

    :sparkles: Add solution for happy numbers

---
+++ src/python/katas/py6kyu/happy_numbers.py
@@ -0,0 +1,21 @@
+"""Kata url: https://www.codewars.com/kata/5464cbfb1e0c08e9b3000b3e."""
+
+
+def is_happy(n):
+    cache = set()
+    while True:
+        n = sum(d ** 2 for d in map(int, str(n)))
+        if n in cache:
+            return False
+
+        if n == 1:
+            return True
+
+
+def test_is_happy():
+    assert is_happy(1)
+    assert is_happy(7)
+    assert is_happy(901)
+
+    assert not is_happy(16)
+    assert not is_happy(37)
