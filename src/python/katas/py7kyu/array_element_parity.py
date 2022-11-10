270ce909d5f30b6adf56e4344b313e7ce17f5f3a
Sigmanificient <edhyjox@gmail.com>
2022-11-10 09:41:34 +0100

    :sparkles: Add solution for array element parity

--- src/python/katas/py7kyu/array_element_parity.py
+++ src/python/katas/py7kyu/array_element_parity.py
@@ -5,3 +5,11 @@ def solve(arr):
     for x in arr:
         if -x not in arr:
             return x
+
+
+def test_solve():
+    assert solve([1, -1, 2, -2, 3]) == 3
+    assert solve([-3, 1, 2, 3, -1, -4, -2]) == -4
+    assert solve([1, -1, 2, -2, 3, 3]) == 3
+    assert solve([-110, 110, -38, -38, -62, 62, -38, -38, -38]) == -38
+    assert solve([-9, -105, -9, -9, -9, -9, 105]) == -9
