6443d7ded7cc5c6f0f6ee48c033a6380b9db3b99
Sigmanificient <edhyjox@gmail.com>
2022-11-10 09:37:32 +0100

    :sparkles: Add solution for leap year

---
+++ src/python/katas/py7kyu/leap_year.py
@@ -0,0 +1,16 @@
+"""Kata: https://www.codewars.com/kata/526c7363236867513f0005ca."""
+
+
+def is_leap_year(year):
+    return not (year % 4) - (not year % 100) + (not year % 400)
+
+
+def test_is_leap_year():
+    assert is_leap_year(1984)
+    assert is_leap_year(2000)
+    assert is_leap_year(2004)
+    assert is_leap_year(8)
+
+    assert not is_leap_year(1234)
+    assert not is_leap_year(1100)
+    assert not is_leap_year(1194)
