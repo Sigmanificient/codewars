061ed27da3c13718725b03eec282a26400ad2f80
Sigmanificient <edhyjox@gmail.com>
2022-11-12 16:34:47 +0100

    :sparkles: Add solution for digitize

---
+++ src/python/katas/py7kyu/digitize.py
@@ -0,0 +1,13 @@
+"""Kara url: https://www.codewars.com/kata/5417423f9e2e6c2f040002ae."""
+
+
+def digitize(n):
+    return list(map(int, str(n)))
+
+
+def test_digitize():
+    assert digitize(123) == [1, 2, 3]
+    assert digitize(1) == [1]
+    assert digitize(0) == [0]
+    assert digitize(1230) == [1, 2, 3, 0]
+    assert digitize(8675309) == [8, 6, 7, 5, 3, 0, 9]
