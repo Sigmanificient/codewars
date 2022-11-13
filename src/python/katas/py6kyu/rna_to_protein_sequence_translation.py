a5c4ddc20ae74070f46010e14a298c555a1e68a9
Sigmanificient <edhyjox@gmail.com>
2022-11-13 13:18:41 +0100

    :sparkles: Add solution for dna to rna

---
+++ src/python/katas/py6kyu/rna_to_protein_sequence_translation.py
@@ -0,0 +1,32 @@
+"""Kata url: https://www.codewars.com/kata/555a03f259e2d1788c000077."""
+
+PROTEIN_DICT = {
+    'UUC': 'F', 'UUU': 'F',
+    'UUA': 'L', 'UUG': 'L', 'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+    'AUU': 'I', 'AUC': 'I', 'AUA': 'I',
+    'AUG': 'M',
+    'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+    'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S', 'AGU': 'S', 'AGC': 'S',
+    'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+    'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+    'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+    'UAU': 'Y', 'UAC': 'Y',
+    'CAU': 'H', 'CAC': 'H',
+    'CAA': 'Q', 'CAG': 'Q',
+    'AAU': 'N', 'AAC': 'N',
+    'AAA': 'K', 'AAG': 'K',
+    'GAU': 'D', 'GAC': 'D',
+    'GAA': 'E', 'GAG': 'E',
+    'UGU': 'C', 'UGC': 'C',
+    'UGG': 'W',
+    'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R', 'AGA': 'R', 'AGG': 'R',
+    'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G',
+    'UAA': 'Stop', 'UGA': 'Stop', 'UAG': 'Stop'
+}
+
+
+def protein(rna):
+    return ''.join(
+        PROTEIN_DICT[rna[i: i + 3]]
+        for i in range(0, len(rna), 3)
+    )
