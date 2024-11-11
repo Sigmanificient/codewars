0213b8feb6ba3e94f4e1699040ea5d229d7b6c63
Sigmanificient <edhyjox@gmail.com>
2024-11-11 04:17:39 +0100

    Add solution for switch/case bug fixing

---
+++ src/python/katas/py8kyu/switch_case_bug_fixing.py
@@ -0,0 +1,18 @@
+"""Kata url: https://www.codewars.com/kata/55c933c115a8c426ac000082."""
+
+def eval_object(v):
+    match v["operation"]:
+        case "+":
+            return v["a"] + v["b"]
+        case "-":
+            return v["a"] - v["b"]
+        case "/":
+            return v["a"] / v["b"]
+        case "*":
+            return v["a"] * v["b"]
+        case "%":
+            return v["a"] % v["b"]
+        case "**":
+            return v["a"] ** v["b"]
+        case _:
+            return 1
